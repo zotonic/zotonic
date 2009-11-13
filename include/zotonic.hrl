4f2ef2b1fbf782ea3c729f9213b871e5a963337f
Arjan Scherpenisse <arjan@scherpenisse.net>
2009-11-13 19:48:50 +0100

    Pushed version nr of default branch to 0.2-dev; added RELEASE file to default branch.

--- include/zotonic.hrl
+++ include/zotonic.hrl
@@ -16,7 +16,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--define(ZOTONIC_VERSION, "0.1.0").
+-define(ZOTONIC_VERSION, "0.2-dev").
 
 -include_lib("deps/webmachine/include/wm_reqdata.hrl").
 
