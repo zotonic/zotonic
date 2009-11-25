1588764d34a48c52936cfdf491797cd3c1a93a20
Marc Worrell <marc@worrell.nl>
2009-11-25 20:56:15 +0100

    Added the search {match_objects_cats id=id} with optional cat parameter. Same as match_objects but also matches the category ids.

--- modules/mod_search/mod_search.erl
+++ modules/mod_search/mod_search.erl
@@ -123,7 +123,6 @@ search({match_objects, [{id,Id}]}, _OffsetLimit, Context) ->
 	ObjectIds = m_edge:objects(Id, Context),
 	MatchTerms = [ ["zpo",integer_to_list(ObjId)] || ObjId <- ObjectIds ],
 	TsQuery = lists:flatten(z_utils:combine("|", MatchTerms)),
-	?DEBUG(TsQuery),
 	case TsQuery of
 		[] ->
 			#search_result{};
@@ -139,6 +138,32 @@ search({match_objects, [{id,Id}]}, _OffsetLimit, Context) ->
 	end;
 search({match_objects, [{cat,Cat},{id,Id}]}, OffsetLimit, Context) ->
 	case search({match_objects, [{id,Id}]}, OffsetLimit, Context) of
+		#search_sql{} = Search -> Search#search_sql{cats=[{"r", Cat}]};
+		Result -> Result
+	end;
+
+%% @doc Return the rsc records that have similar objects
+search({match_objects_cats, [{id,Id}]}, _OffsetLimit, Context) ->
+	IsCats = m_rsc:is_a_id(Id, Context),
+	CatTerms = [ ["zpc",integer_to_list(CatId)] || CatId <- IsCats ],
+	ObjectIds = m_edge:objects(Id, Context),
+	ObjectTerms = [ ["zpo",integer_to_list(ObjId)] || ObjId <- ObjectIds ],
+	TsQuery = lists:flatten(z_utils:combine("|", CatTerms++ObjectTerms)),
+	case TsQuery of
+		[] ->
+			#search_result{};
+		_ ->
+		    #search_sql{
+		        select="r.id, ts_rank(pivot_rtsv, query) AS rank",
+		        from="rsc r, to_tsquery($1) query",
+		        where=" query @@ pivot_rtsv and id <> $2",
+		        order="rank desc",
+		        args=[TsQuery, Id],
+		        tables=[{rsc,"r"}]
+		    }
+	end;
+search({match_objects_cats, [{cat,Cat},{id,Id}]}, OffsetLimit, Context) ->
+	case search({match_objects_cats, [{id,Id}]}, OffsetLimit, Context) of
 		#search_sql{} = Search -> Search#search_sql{cats=[{"r", Cat}]};
 		Result -> Result
 	end;
