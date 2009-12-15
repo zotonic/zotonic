fbc805ee6f08b68c4dd2f7a75f7a9ef243d38a45
Arjan Scherpenisse <arjan@scherpenisse.net>
2009-12-15 21:45:40 +0100

    Moved blog site to default site.

--- priv/sites/default/default.erl
+++ priv/sites/default/default.erl
@@ -15,7 +15,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(blog).
+-module(default).
 -author("Arjan Scherpenisse <arjan@scherpenisse.net>").
 -behaviour(gen_server).
 
