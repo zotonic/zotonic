c13c5c2115458afff1104005e813e469f51e314e
Arjan Scherpenisse <arjan@scherpenisse.net>
2009-12-15 21:53:52 +0100

    Moved 'zotonic' site to 'zotonicwww' to prevent a name clash.
    --HG--
    rename : priv/sites/zotonic/config.in => priv/sites/zotonicwww/config.in
    rename : priv/sites/zotonic/demodata/learnmore.html => priv/sites/zotonicwww/demodata/learnmore.html
    rename : priv/sites/zotonic/demodata/welcome.html => priv/sites/zotonicwww/demodata/welcome.html
    rename : priv/sites/zotonic/dispatch/dispatch => priv/sites/zotonicwww/dispatch/dispatch
    rename : priv/sites/zotonic/files/archive/koe.jpg => priv/sites/zotonicwww/files/archive/koe.jpg
    rename : priv/sites/zotonic/files/dropbox/.empty => priv/sites/zotonicwww/files/dropbox/.empty
    rename : priv/sites/zotonic/files/preview/.empty => priv/sites/zotonicwww/files/preview/.empty
    rename : priv/sites/zotonic/files/processing/.empty => priv/sites/zotonicwww/files/processing/.empty
    rename : priv/sites/zotonic/files/unhandled/.empty => priv/sites/zotonicwww/files/unhandled/.empty
    rename : priv/sites/zotonic/lib/css/zp-project.css => priv/sites/zotonicwww/lib/css/zp-project.css
    rename : priv/sites/zotonic/lib/images/docs.png => priv/sites/zotonicwww/lib/images/docs.png
    rename : priv/sites/zotonic/lib/images/download_zotonic.png => priv/sites/zotonicwww/lib/images/download_zotonic.png
    rename : priv/sites/zotonic/lib/images/favicon.ico => priv/sites/zotonicwww/lib/images/favicon.ico
    rename : priv/sites/zotonic/lib/images/home_banner.jpg => priv/sites/zotonicwww/lib/images/home_banner.jpg
    rename : priv/sites/zotonic/lib/images/home_bg.jpg => priv/sites/zotonicwww/lib/images/home_bg.jpg
    rename : priv/sites/zotonic/lib/images/logo.jpg => priv/sites/zotonicwww/lib/images/logo.jpg
    rename : priv/sites/zotonic/lib/images/page_banner.jpg => priv/sites/zotonicwww/lib/images/page_banner.jpg
    rename : priv/sites/zotonic/lib/images/page_bg.jpg => priv/sites/zotonicwww/lib/images/page_bg.jpg
    rename : priv/sites/zotonic/lib/images/page_footer.jpg => priv/sites/zotonicwww/lib/images/page_footer.jpg
    rename : priv/sites/zotonic/lib/images/page_top.gif => priv/sites/zotonicwww/lib/images/page_top.gif
    rename : priv/sites/zotonic/lib/js/cufon.anja.js => priv/sites/zotonicwww/lib/js/cufon.anja.js
    rename : priv/sites/zotonic/modules/.emtpty => priv/sites/zotonicwww/modules/.emtpty
    rename : priv/sites/zotonic/resources/.emtpty => priv/sites/zotonicwww/resources/.emtpty
    rename : priv/sites/zotonic/templates/_documents.tpl => priv/sites/zotonicwww/templates/_documents.tpl
    rename : priv/sites/zotonic/templates/_footer.tpl => priv/sites/zotonicwww/templates/_footer.tpl
    rename : priv/sites/zotonic/templates/_js_include.tpl => priv/sites/zotonicwww/templates/_js_include.tpl
    rename : priv/sites/zotonic/templates/_searchfield.tpl => priv/sites/zotonicwww/templates/_searchfield.tpl
    rename : priv/sites/zotonic/templates/base.tpl => priv/sites/zotonicwww/templates/base.tpl
    rename : priv/sites/zotonic/templates/category.tpl => priv/sites/zotonicwww/templates/category.tpl
    rename : priv/sites/zotonic/templates/collection.tpl => priv/sites/zotonicwww/templates/collection.tpl
    rename : priv/sites/zotonic/templates/documentation.tpl => priv/sites/zotonicwww/templates/documentation.tpl
    rename : priv/sites/zotonic/templates/error.tpl => priv/sites/zotonicwww/templates/error.tpl
    rename : priv/sites/zotonic/templates/features.tpl => priv/sites/zotonicwww/templates/features.tpl
    rename : priv/sites/zotonic/templates/gallery.tpl => priv/sites/zotonicwww/templates/gallery.tpl
    rename : priv/sites/zotonic/templates/home.tpl => priv/sites/zotonicwww/templates/home.tpl
    rename : priv/sites/zotonic/templates/page.tpl => priv/sites/zotonicwww/templates/page.tpl
    rename : priv/sites/zotonic/templates/zotonic_team.tpl => priv/sites/zotonicwww/templates/zotonic_team.tpl
    rename : priv/sites/zotonic/zotonic.erl => priv/sites/zotonicwww/zotonicwww.erl

--- priv/sites/zotonicwww/zotonicwww.erl
+++ priv/sites/zotonicwww/zotonicwww.erl
@@ -17,7 +17,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(zotonic).
+-module(zotonicwww).
 -author("Tim Benniks <tim@timbenniks.nl>").
 -behaviour(gen_server).
 
