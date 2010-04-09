1af911af4b76ed5005ba23e031241be47ebc9cbd
Marc Worrell <marc@worrell.nl>
2010-04-09 12:08:18 +0200

    Added support for the xor operator.

--- src/erlydtl/erlydtl_scanner.erl
+++ src/erlydtl/erlydtl_scanner.erl
@@ -63,7 +63,7 @@ identifier_to_keyword({identifier, Pos, String}, {PrevToken, Acc}) when PrevToke
     RevString = lists:reverse(String),
     Keywords = ["for", "empty", "endfor", "in", "include", "catinclude", "block", "endblock",
         "extends", "autoescape", "endautoescape", "if", "else", "endif",
-        "not", "or", "and", "comment", "endcomment", "cycle", "firstof",
+        "not", "or", "and", "xor", "comment", "endcomment", "cycle", "firstof",
         "ifchanged", "ifequal", "endifequal", "ifnotequal", "endifnotequal",
         "now", "regroup", "rsc", "spaceless", "endspaceless", "ssi", "templatetag",
         "load", "call", "with", "url", "print", "image", "image_url", "media", "_", "with", "endwith", 
