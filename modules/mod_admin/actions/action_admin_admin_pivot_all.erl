7deadc9134985747af4b975deafb521ef9abe178
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-04-11 16:59:52 +0200

    Added admin "System" section, which contains buttons for flushing the cache and rebuilding the search index.
    See issue 18.

---
+++ modules/mod_admin/actions/action_admin_admin_pivot_all.erl
@@ -0,0 +1,31 @@
+%% @author Arjan Scherpenisse <arjan@scherpenisse.net>
+%% @copyright 2010 Arjan Scherpenisse
+%% @date 2010-04-11
+%% @doc Rebuild the search index
+
+-module(action_admin_admin_pivot_all).
+-author("Arjan Scherpenisse <arjan@scherpenisse.net>").
+-include("zotonic.hrl").
+
+%% interface functions
+-export([
+    render_action/4,
+    event/2
+]).
+
+render_action(TriggerId, TargetId, _Args, Context) ->
+    Postback = {admin_flush},
+	{PostbackMsgJS, _PickledPostback} = z_render:make_postback(Postback, click, TriggerId, TargetId, ?MODULE, Context),
+	{PostbackMsgJS, Context}.
+
+%% @doc Unlink the edge, on success show an undo message in the element with id "unlink-message"
+%% @spec event(Event, Context1) -> Context2
+event({postback, {admin_flush}, _TriggerId, _TargetId}, Context) ->
+    case z_acl:has_role(admin, Context) of
+        true ->
+            z_pivot_rsc:queue_all(Context),
+            z_render:growl("The search index is rebuilding. Depending on the database size, this can take a long time.", Context);
+        false ->
+            z_render:growl_error("Sorry, you have no permission to rebuild the search index.", Context)
+    end.
+ 
---
+++ modules/mod_admin/templates/admin_status.tpl
@@ -0,0 +1,40 @@
+{% extends "admin_base.tpl" %}
+
+{% block title %} system status {% endblock %}
+
+{% block content %}
+
+<style>
+    span.expl { float: left;margin-top: 3px; }
+</style>
+
+<div id="content" class="zp-85">
+    <div class="block clearfix">
+{#
+		<h2>System status</h2>
+
+            <p>This should show some counters, stats, etc.</p>
+#}
+
+		<div class="clearfix">
+
+            <h2>System administration</h2>
+			<div class="clearfix">
+                {% button class="" text="Flush system caches" action={admin_flush} %} 
+                <span class="expl">Flush all URL dispatch rules, template- and library caches and other in-memory cached data.</span>
+            </div>
+
+			<div class="clearfix">
+                {% button class="" text="Rebuild search indices" action={admin_pivot_all} %}
+                <span class="expl">Rebuild all search-indices by putting all data from the Zotonic database in the indexer queue. This can take a long time!</span>
+
+            </div>
+
+		</div>
+		
+		<hr />
+		
+    </div>
+</div>
+
+{% endblock %}
