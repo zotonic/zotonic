fe419e19316bde1adf753954f97d96777079dc35
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-04-18 18:50:39 +0200

    Added Windows build and start scripts. Thanks Pierrick <pgourlain@gmail.com>

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set erl="C:\Program Files\erl5.7.4\bin\erl.exe"
-set erlc="C:\Program Files\erl5.7.4\bin\erlc.exe"
-set ebin=ebin deps\mochiweb\ebin  deps\erlang-oauth\ebin deps\webmachine\ebin
-
-%erl% +P 10000000 -pa %ebin% -boot start_sasl -s zotonic
