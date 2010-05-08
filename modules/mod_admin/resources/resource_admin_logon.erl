3078c1254cc8438c8e786bf8dbc168375f14a595
Marc Worrell <marc@worrell.nl>
2010-05-08 16:24:45 +0200

    Fixed the /admin/logon resource, for when the admin logon should be shown.

--- modules/mod_admin/resources/resource_admin_logon.erl
+++ modules/mod_admin/resources/resource_admin_logon.erl
@@ -37,7 +37,20 @@ html(Context) ->
     html(Context, []).
 
 html(Context, Vars) ->
-	ContextLogon = z_auth:output_logon(Context, Vars),
+    RD1 = z_context:get_reqdata(Context),
+    Redirect = case z_context:get_q("redirect", Context) of
+        undefined -> wrq:raw_path(RD1);
+		Redir -> Redir
+	end,
+	Vars1 = [
+		{zp_username, z_context:get_q("zp-username", Context)},
+		{redirect, Redirect} | Vars
+	],
+    Html = z_template:render("admin_logon.tpl", Vars1, Context),
+    {Data, ContextOut} = z_context:output(Html, Context),
+    RD2 = wrq:append_to_resp_body(Data, RD1),
+    RD3 = wrq:set_resp_header("Content-Type", "text/html; charset=utf-8", RD2),
+	ContextLogon = z_context:set_reqdata(RD3, ContextOut),
     {{halt, 401}, ContextLogon}.
 
 
