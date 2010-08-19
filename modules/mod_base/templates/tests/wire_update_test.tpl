2dbb88e95895c55f5ecdd472fc8040b8be6bad01
Marc Worrell <marc@worrell.nl>
2010-08-19 11:50:54 +0200

    Fixed problem where actions inside an update action where not rendered properly.  Also added regression tests.

---
+++ modules/mod_base/templates/tests/wire_update_test.tpl
@@ -0,0 +1,2 @@
+<div id="summary">Bla bla bla</div>
+{% wire action={update selector="#summary" template="tests/wire_update_test_1.tpl"} %}
---
+++ modules/mod_base/tests/mod_admin_tests.erl
@@ -0,0 +1,21 @@
+-module(mod_admin_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("zotonic.hrl").
+
+wire_update_test() ->
+    Context = z_context:new(testsandbox),
+    z_ids:fix_seed(),
+    {Html, ContextHtml} = z_template:render_to_iolist("tests/wire_update_test.tpl", [], Context),
+    Script = z_script:get_script(ContextHtml),
+    <<"\n\n$(\"#summary\").html(\"\\x3cbutton id\\x3d\\x27mtbuaa\\x27\\x3eClick Me!\\x3c/button\\x3e\\n\").widgetManager();\n$(\"#mtbuaa\").bind('click', function(event) { alert(\"Thank you\"); return z_opt_cancel(this); } );\n">>
+            = iolist_to_binary(Script),
+    <<"<div id=\"summary\">Bla bla bla</div>\n\n">> = iolist_to_binary(Html),
+    ok.
+
+%     <div id="summary">Bla bla bla</div>
+%     {% wire action={update selector="#summary" template="_test.tpl"} %}
+% en in _test.tpl dan dit zet:
+% Een button:
+%    {% button text="Click Me!" action={alert text="Thank you"} %}
+   
