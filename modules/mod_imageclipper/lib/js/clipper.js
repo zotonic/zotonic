cce6758e3c96e4af4a629abe7e533e9bcebfef58
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-08-19 22:43:34 +0200

    Added mod_imageclipper module: A module which lets you "borrow" images from another site through a nice javascript bookmarklet. The images get imported in your Zotonic site.

---
+++ modules/mod_imageclipper/lib/js/clipper.js
@@ -0,0 +1,153 @@
+window.ZotonicImageClipper =
+(function()
+ {
+     var self = this;
+
+     self.buildPopup = function()
+     {
+         self.popup = $("<div>")
+             .appendTo(document.body)
+             .hide()
+             .css({position: "fixed", width: "400px", top: 5, right: 5, "background-color": "white", border: "1px solid #666"})
+             .fadeIn();
+         self.popup.append($("<h2>")
+                           .text("Click an image to import it in Zotonic")
+                           .css({padding:20,"font": "16px Verdana",color:"#666"})
+                          );
+
+
+         self.imageContainer = $("<div>").css({"padding-left":20, "padding-right":20})
+             .hide()
+             .appendTo(self.popup);
+
+         self.buttonContainer = $("<div>").css({padding:20, "padding-top": 15})
+             .hide()
+             .appendTo(self.popup);
+
+         self.form = $("<form>")
+             .attr("method", "post")
+             .attr("action", window._clipper)
+             .append($("<button>").text("Upload to Zotonic"))
+             .appendTo(self.buttonContainer);
+
+         $("<span>")
+             .html($.browser.msie ? "x" : "&#10799;")
+             .css({cursor: "pointer", "font-weight": "bold", color: "red", position: "absolute", top: 0, right: 5})
+             .attr("title", "Close image clipper")
+             .click(function(){self.destroy();})
+             .appendTo(self.popup);
+
+     };
+
+     // Show the popup
+     self.run = function()
+     {
+         self.images = [];
+         if (self.popup)
+         {
+             self.destroy();
+         }
+         self.buildPopup();
+
+         $(document).bind("keyup.clipper", function(e) { if (e.keyCode == 27 && self.popup) self.destroy(); });
+
+         if (!self.selection)
+         {
+             self.selection = $("<div>")
+                 .hide()
+                 .click(function(){self.addImage(self.currentImage);return false;})
+                 .appendTo(document.body)
+                 .css({position: "absolute", border: "5px solid red", "z-index": 1000000, cursor: "pointer"});
+         }
+
+
+         $("img").bind("mouseover.clipper", function() {
+                           var im = $(this);var o=im.offset();
+                           var w=im.outerWidth(true);var h=im.outerHeight(true);
+                           if (w<100&&h<100) return;
+                           self.selection.css({top:o.top-5,left:o.left-5,width:w,height:h});
+                           self.selection.show();
+                           self.currentImage = im;
+                       });
+     };
+
+     self.destroy = function()
+     {
+         self.popup.remove();
+         self.popup = null;
+         if (self.selection) {
+             self.selection.remove();
+             self.selection = null;
+         }
+         $("img").unbind("mouseover.clipper");
+         $("img").unbind("click.clipper");
+         $(document).unbind("keyup.clipper");
+     };
+
+
+     // Add an image to the clipper
+     self.addImage = function (im)
+     {
+         self.buttonContainer.show();
+         self.imageContainer.show();
+
+         var src = null;
+         // First check if image links to a big picture
+         if (im.parents("a:first").length) {
+             var u = im.parents("a:first").attr("href");
+             if (u.match(/\.(jpe?g|png|bmp|gif)$/i)) {
+                 src = u;
+             }
+         }
+         if (!src) src = im.attr("src");
+         if (!src.match(/^https?:\/\//))
+         {
+             var l = document.location.href;
+             if (src.match(/^\//)) {
+                 // absolute URL; prepend hostname
+                 src = l.replace(/^(https?:\/\/.*?)\/.*$/, "$1") + src;
+             } else {
+                 // relative url
+                 src = l.replace(/^(.*\/).*$/, "$1") + src;
+             }
+         }
+         if ($.inArray(src, self.images)!=-1) {
+             return;
+         }
+
+         self.images.push(src);
+         self.form.append($("<input type=\"hidden\">").attr("name", "url").val(src));
+
+         var h = 60;
+
+         $("<img>")
+             .attr("src", im.attr("src"))
+             .css({padding: 1, border: "1px solid #e0e0e0", "margin-right": 10, "margin-bottom": 10, height: h, width: h * (im.width()/im.height())})
+             .appendTo(self.imageContainer);
+
+     };
+
+     // Check jQuery and then start
+     if (typeof window.jQuery == 'undefined')
+     {
+         // Add jQuery script
+         var script =document.createElement("script");
+         script.src="http://code.jquery.com/jquery-1.4.2.min.js";
+         document.body.appendChild(script);
+         var tryRun = function() {
+             if (typeof window.jQuery == 'undefined') {
+                 setTimeout(tryRun, 100);
+             } else {
+                 self.run();
+             }
+         };
+         setTimeout(tryRun, 100);
+     }
+     else
+     {
+         // First run
+         self.run();
+     }
+
+     return self;
+ })();
---
+++ modules/mod_imageclipper/templates/_clipper_bookmarklet.tpl
@@ -0,0 +1,2 @@
+javascript:(function(){window._clipper="http://{{ m.site.hostname }}{% url imageclipper_post %}";function%20b(g){var%20e=window;if(e.ZotonicImageClipper){e.ZotonicImageClipper.run()}else{var%20f=g.createElement("script");f.src="http://{{ m.site.hostname }}/lib/js/clipper.js?"+Math.floor((new%20Date()).getTime()/86400000);g.body.appendChild(f)}}b(document);for(var%20a=0;a<frames.length;++a){var%20d=frames[a];if(d.frameElement.tagName=="IFRAME"){continue}if(d.innerWidth<400||d.innerHeight<400){continue}b(d.document)}})();
+
