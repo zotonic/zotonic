a2bdda0c19788a8e1f2fa647005f632dcadd50cb
Marc Worrell <marc@worrell.nl>
2010-09-30 14:54:02 +0200

    Added 'words' argument to loremipsum scomp.  Added z_string:truncatewords/2 (and /3), added z_string:trim_left_func/2.

--- modules/mod_base/scomps/scomp_base_loremipsum.erl
+++ modules/mod_base/scomps/scomp_base_loremipsum.erl
@@ -22,10 +22,21 @@
 
 -include("zotonic.hrl").
 
--define(LOREM_IPSUM, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis, lacus id molestie suscipit, enim leo pharetra velit, ac cursus felis mauris at velit. Cras ultrices, massa pharetra faucibus cursus, neque leo bibendum mauris, eu vulputate leo magna vitae lacus. Etiam pharetra gravida elementum. Maecenas lacinia, sem sed eleifend euismod, risus velit interdum nulla, convallis sagittis orci dui a metus. Aliquam tristique orci a ipsum dapibus viverra. Donec vestibulum varius ante, vitae placerat magna ultrices ac. Donec nec ante magna. Donec porttitor, arcu a condimentum aliquam, lectus nunc rhoncus quam, id ornare neque ligula quis dui. Pellentesque sit amet lectus augue, ut ullamcorper nisi. Donec et rutrum sem. In porta ultricies nibh, in sagittis lacus euismod at. Etiam consectetur tristique tellus, quis tristique dui vulputate vel. Curabitur sagittis gravida dui, vel sagittis lectus suscipit ac.").
+-define(LOREM_IPSUM, 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis, lacus id molestie suscipit, "
+    "enim leo pharetra velit, ac cursus felis mauris at velit. Cras ultrices, massa pharetra faucibus cursus, "
+    "neque leo bibendum mauris, eu vulputate leo magna vitae lacus. Etiam pharetra gravida elementum. Maecenas "
+    "lacinia, sem sed eleifend euismod, risus velit interdum nulla, convallis sagittis orci dui a metus. Aliquam "
+    "tristique orci a ipsum dapibus viverra. Donec vestibulum varius ante, vitae placerat magna ultrices ac. "
+    "Donec nec ante magna. Donec porttitor, arcu a condimentum aliquam, lectus nunc rhoncus quam, id ornare neque "
+    "ligula quis dui. Pellentesque sit amet lectus augue, ut ullamcorper nisi. Donec et rutrum sem. In porta "
+    "ultricies nibh, in sagittis lacus euismod at. Etiam consectetur tristique tellus, quis tristique dui vulputate "
+    "vel. Curabitur sagittis gravida dui, vel sagittis lectus suscipit ac.").
 
 vary(_Params, _Context) -> nocache.
-render(_Params, _Vars, Context) ->
-    Context1 = z_render:render(?LOREM_IPSUM, Context),
-    {ok, Context1}.
+render(Params, _Vars, _Context) ->
+    case proplists:get_value(words, Params) of
+        undefined -> {ok, ?LOREM_IPSUM};
+        Words -> {ok, z_string:truncatewords(?LOREM_IPSUM, z_convert:to_integer(Words), ".")}
+    end.
 
