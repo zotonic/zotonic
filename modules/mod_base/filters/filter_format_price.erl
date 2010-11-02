06f698a4862b5cb34076bd1dadfcb405a3e4a303
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-11-02 22:33:14 +0100

    Updated the format_price filter to add custom thousand and decimal separators. Thanks to Michael Connors.

--- modules/mod_base/filters/filter_format_price.erl
+++ modules/mod_base/filters/filter_format_price.erl
@@ -1,8 +1,8 @@
-%% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2010 Marc Worrell
+%% @author Michael Connors <michael@bring42.net>
+%% @copyright 2010 Michael Connors
 %% @doc 'format_price' filter, show a price with two digits. Accepts a price in cents.
 
-%% Copyright 2010 Marc Worrell
+%% Copyright 2010 Michael Connors
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -17,28 +17,68 @@
 %% limitations under the License.
 
 -module(filter_format_price).
--export([format_price/2]).
+-export([format_price/4, format_price/3, format_price/2]).
 
+insert_thousands_separator(_Sep, Output, []) ->
+    Output;
 
-format_price(Input, _Context) when is_integer(Input) ->
+insert_thousands_separator(Sep, Output, Input) when is_list(Input) ->
+    case length(Input) > 3 of
+        true ->
+            case length(Input) rem 3 of
+                0 -> 
+                     [Head | Input2] = Input,
+                     insert_thousands_separator(Sep, lists:append(Output, [Head]), Input2);
+                1 -> 
+                     [Head | Input2] = Input,
+                     Head1 = lists:append([Head], [Sep]),
+                     insert_thousands_separator(Sep, lists:append(Output, Head1), Input2);
+                2 ->
+                     [Head | Input2] = Input,
+                     insert_thousands_separator(Sep, lists:append(Output, [Head]), Input2)
+            end;
+        false -> lists:append(Output, Input)
+    end.
+
+insert_thousands_separator(Sep, Input) when is_integer(Input) ->
+    insert_thousands_separator(Sep, [], integer_to_list(Input)).
+
+format_price(Input, DSep, TSep, _Context) when is_integer(Input) ->
     case Input rem 100 of
         0 -> 
             integer_to_list(Input div 100);
         Cents when Cents < 10 -> 
-            [integer_to_list(Input div 100), $,, $0, Cents + $0 ];
+            [insert_thousands_separator(TSep, Input div 100), DSep, $0, Cents + $0 ];
         Cents -> 
-            [integer_to_list(Input div 100), $,, integer_to_list(Cents) ]
+            [insert_thousands_separator(TSep, Input div 100), DSep, integer_to_list(Cents) ]
     end;
-format_price(Input, Context) when is_float(Input) ->
-    format_price(round(Input * 100), Context);
-format_price(Input, Context) when is_function(Input, 0) ->
-    format_price(Input(), Context);
-format_price(Input, Context) when is_function(Input, 1) ->
-    format_price(Input(Context), Context);
-format_price(Input, Context) when is_list(Input) ->
+format_price(Input, DSep, TSep, Context) when is_float(Input) ->
+    format_price(round(Input * 100), DSep, TSep, Context);
+format_price(Input, DSep, TSep, Context) when is_function(Input, 0) ->
+    format_price(Input(), DSep, TSep, Context);
+format_price(Input, DSep, TSep, Context) when is_function(Input, 1) ->
+    format_price(Input(Context), DSep, TSep, Context);
+format_price(Input, DSep, TSep, Context) when is_list(Input) ->
     case string:to_integer(Input) of
         {error, _} -> Input;
-        {N, _Rest} -> format_price(N, Context)
+        {N, _Rest} -> format_price(N, DSep, TSep, Context)
     end;
-format_price(undefined, _Context) ->
+format_price(Input, DSep, TSep, Context) when is_binary(Input) ->
+    case string:to_integer(binary_to_list(Input)) of
+        {error, _} -> Input;
+        {N, _Rest} -> format_price(N, DSep, TSep, Context)
+    end;
+format_price(undefined, _Dsep, _Tsep, _Context) ->
     "-".
+
+format_price(Input, Args, Context) ->
+    case length(Args) of
+        0 -> format_price(Input, $., $,, Context);
+        1 -> format_price(Input, Args, $,, Context);
+        2 -> [DSep, TSep] = Args,
+             format_price(Input, DSep, TSep, Context);
+        _ -> format_price(Input, $., $,, Context)
+    end.
+
+format_price(Input, Context) ->
+    format_price(Input, $., $,, Context).
