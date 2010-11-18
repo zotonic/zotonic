40d79b61a9117a10f22fa08707f5c85c68123091
Marc Worrell <marc@worrell.nl>
2010-11-18 16:12:38 +0100

    Fix problem where one of the e-mail addresses for sendmail is a binary.

--- modules/mod_emailer/mod_emailer.erl
+++ modules/mod_emailer/mod_emailer.erl
@@ -258,8 +258,9 @@ mark_retry(Id, Retry, Context) ->
 sendemail(Msg = #mime_multipart{}, #state{sendmail=Sendmail}) when Sendmail /= [] ->
 	Message = esmtp_mime:encode(Msg),
 	{_FromName, FromEmail} = z_email:split_name_email(esmtp_mime:from(Msg)),
-	SendmailFrom = Sendmail ++ " -f" ++ z_utils:os_escape(FromEmail) ++ " " ++ z_utils:os_escape(esmtp_mime:to(Msg)),
-	P = open_port({spawn, SendmailFrom}, [use_stdio]),
+	SendmailFrom = [ Sendmail, " -f", z_utils:os_escape(FromEmail), " ", z_utils:os_escape(esmtp_mime:to(Msg)) ],
+	SendmailFromAsList = binary_to_list(iolist_to_binary(SendmailFrom)),
+	P = open_port({spawn, SendmailFromAsList}, [use_stdio]),
 	port_command(P, Message),
 	port_close(P);
 
