9768ac6bdadf06bdb67be8e1d5e5db5f2b46e140
Marc Worrell <marc@worrell.nl>
2011-01-13 13:49:23 +0100

    Select language using the Accept-Language header. Store selected language in the persistent state. Added extra session init/context foldl for initializing the correct language with a session.

--- modules/mod_translation/mod_translation.erl
+++ modules/mod_translation/mod_translation.erl
@@ -25,9 +25,14 @@
 -mod_prio(500).
 
 -export([
+    observe_session_init_fold/3,
+    observe_session_context/3,
+    
     init/1, 
     event/2,
-    generate/1
+    generate/1,
+    
+    do_choose/2
 ]).
 
 -include("zotonic.hrl").
@@ -53,6 +58,41 @@ init(Context) ->
     end.
 
 
+%% @doc Check if the user has a prefered language (in the user's persistent data). If not
+%%      then check the accept-language header (if any) against the available languages.
+observe_session_init_fold(session_init_fold, Context, _Context) ->
+    case z_context:get_persistent(language, Context) of
+        undefined ->
+            case z_context:get_req_header("accept-language", Context) of
+                undefined ->
+                    Context;
+                AcceptLanguage ->
+                    LanguagesAvailable = [ atom_to_list(Lang)
+                                            || {Lang, Props} <- get_language_config(Context), 
+                                               proplists:get_value(is_enabled, Props) =:= true 
+                                         ],
+                    case catch do_choose(LanguagesAvailable, AcceptLanguage) of
+                        Lang when is_list(Lang) ->
+                            do_set_language(list_to_atom(Lang), Context);
+                        none ->
+                            Context;
+                        _Error ->
+                            % @todo log the error, might be a problem in the accept-language header
+                            Context
+                    end
+            end;
+        Language ->
+            do_set_language(Language, Context)
+    end.
+
+
+observe_session_context(session_context, Context, _Context) ->
+    case z_context:get_session(language, Context) of
+        undefined -> Context;
+        Language -> Context#context{language=Language}
+    end.
+
+
 %% @doc Set the current session language, reload the user agent's page.
 event({postback, {set_language, Args}, _TriggerId, _TargetId}, Context) ->
     Code = case proplists:get_value(code, Args) of
@@ -61,6 +101,7 @@ event({postback, {set_language, Args}, _TriggerId, _TargetId}, Context) ->
             end,
     List = get_language_config(Context),
     Context1 = set_language(z_convert:to_list(Code), List, Context),
+    z_context:set_persistent(language, z_context:language(Context1), Context1),
     z_render:wire({reload, []}, Context1);
 
 %% @doc Set the default language.
@@ -129,14 +170,15 @@ event({postback, {language_enable, Args}, _TriggerId, _TargetId}, Context) ->
 %% @doc Set the language of the current user/session
 set_language(Code, [{CodeAtom, _Language}|Other], Context) ->
     case z_convert:to_list(CodeAtom) of
-        Code ->
-            Context1 = z_context:set_language(CodeAtom, Context),
-            z_context:set_session(language, CodeAtom, Context1),
-            z_notifier:notify({language, CodeAtom}, Context1),
-            Context1;
-        _Other ->
-            set_language(Code, Other, Context)
+        Code ->   do_set_language(CodeAtom, Context);
+        _Other -> set_language(Code, Other, Context)
     end.
+
+do_set_language(Code, Context) when is_atom(Code) ->
+    Context1 = z_context:set_language(Code, Context),
+    z_context:set_session(language, Code, Context1),
+    z_notifier:notify({language, Code}, Context1),
+    Context1.
 
 
 %% @doc Add a language to the i18n configuration
@@ -184,4 +226,65 @@ generate(#context{} = Context) ->
 generate(Host) when is_atom(Host) ->
     translation_po:generate(translation_scan:scan(z_context:new(Host))).
 
+
+
+%% do_choose/2 is adapted from webmachine_util:do_choose/3
+%% 
+%% @author Justin Sheehy <justin@basho.com>
+%% @author Andy Gross <andy@basho.com>
+%% @copyright 2007-2008 Basho Technologies
+do_choose(Choices, Header) ->
+    Accepted = build_conneg_list(string:tokens(Header, ",")),
+    StarPrio = [P || {P,C} <- Accepted, C =:= "*"],
+    AnyOkay = case StarPrio of
+        [] -> no;
+        [0.0] -> no;
+        _ -> yes
+    end,
+    do_choose(AnyOkay, Choices, Accepted).
+do_choose(_AnyOkay, [], []) ->
+    none;
+do_choose(_AnyOkay, [], _Accepted) ->
+    none;
+do_choose(yes, Choices, []) ->
+    hd(Choices);
+do_choose(no, _Choices, []) ->
+    none;
+do_choose(AnyOkay, Choices, [AccPair|AccRest]) ->
+    {Prio, Acc} = AccPair,
+    case Prio of
+        0.0 ->
+            do_choose(AnyOkay, lists:delete(Acc, Choices), AccRest);
+        _ ->
+            LAcc = string:to_lower(Acc),
+            LChoices = [string:to_lower(X) || X <- Choices],
+            % doing this a little more work than needed in
+            % order to be easily insensitive but preserving
+            case lists:member(LAcc, LChoices) of
+                true -> 
+                    hd([X || X <- Choices,
+                             string:to_lower(X) =:= LAcc]);
+                false -> do_choose(AnyOkay, Choices, AccRest)
+            end
+    end.
+
+build_conneg_list(AccList) ->
+    build_conneg_list(AccList, []).
+build_conneg_list([], Result) -> lists:reverse(lists:sort(Result));
+build_conneg_list([Acc|AccRest], Result) ->
+    XPair = list_to_tuple([string:strip(X) || X <- string:tokens(Acc, ";")]),
+    Pair = case XPair of
+        {Choice, "q=" ++ PrioStr} ->
+            % Simplify "en-us" to "en"
+            [Choice1|_] = string:tokens(Choice, "-"), 
+            case PrioStr of
+                "0" -> {0.0, Choice1};
+                "1" -> {1.0, Choice1};
+                [$.|_] -> {list_to_float([$0|PrioStr]), Choice1};
+                _ -> {list_to_float(PrioStr), Choice1}
+            end;
+        {Choice} ->
+            {1.0, hd(string:tokens(Choice, "-"))}
+    end,
+    build_conneg_list(AccRest,[Pair|Result]).
 
