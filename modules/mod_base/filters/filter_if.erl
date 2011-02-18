4bd3976e1dd5e2c43b99bf35833c4a5a08b8196e
Marc Worrell <marc@worrell.nl>
2011-02-18 20:36:07 +0100

    Added 'if' filter.

---
+++ modules/mod_base/filters/filter_if.erl
@@ -0,0 +1,27 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2010 Marc Worrell
+%% @doc 'if' filter, depending on input select first or second argument
+
+%% Copyright 2011 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(filter_if).
+-export(['if'/4]).
+
+
+'if'(Input, A, B, _Context) ->
+    case z_convert:to_bool(Input) of
+        true -> A;
+        false -> B
+    end.
