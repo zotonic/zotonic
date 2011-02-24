aca27fc7fddc871357a59cd232ff71f7f6551ceb
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-02-24 12:05:22 +0100

    skel site support for 'zotonic addsite'.
     - moved default site to priv/skel/blog
     - simplified addsite script
     - created install mechanism for default data
     - moved demo data from default site to src/install
     - created mechanism to add default menu structure
     - added "-s <skel>" option to addsite
     - updated documentation ZotonicCommands.txt
     - added options to addsite to specify database crdentials
     - added defaults mechanism in $HOME/.zotonic-defaults
     - updated INSTALL instructions
     - added 'empty' skeleton

--- src/install/z_install_defaultdata.erl
+++ src/install/z_install_defaultdata.erl
@@ -1,8 +1,10 @@
 %% @author Arjan Scherpenisse <arjan@scherpenisse.net>
-%% @copyright 2009 Arjan Scherpenisse
-%% @date 2009-12-12
-%% @doc Module implementing a basic blog.
+%% @copyright 2011 Arjan Scherpenisse
+%%
+%% @doc Installs default data in a Zotonic site.
 
+%% Copyright 2011 Arjan Scherpenisse
+%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -15,34 +17,27 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(default).
+-module(z_install_defaultdata).
 -author("Arjan Scherpenisse <arjan@scherpenisse.net>").
 
--mod_title("Default Zotonic site").
--mod_description("A simple weblog, used as an example of how to create a Zotonic site.").
--mod_prio(10).
+-include("zotonic.hrl").
 
-%% gen_server exports
--export([init/1]).
+%% interface functions
+-export([
+    install/2,
+    default_menu/1
+]).
 
--include_lib("zotonic.hrl").
 
-%% @doc Initialize the datamodel
-init(Context) ->
-    z_datamodel:manage(?MODULE, datamodel(), Context).
-
-%%====================================================================
-%% support functions
-%%====================================================================
-
-datamodel() ->
+install(blog, Context) ->
     Now = {{2010,04,03},{9,12,0}},
-    [
-     {resources,
+    Datamodel = 
+        #datamodel{
+      resources =
       [
-
+       
        %% MENU ENTRIES
-
+       
        {page_home,
         text,
         [{title, <<"Home">>},
@@ -63,7 +58,6 @@ datamodel() ->
          {page_path, <<"/contact">>}]
        },
 
-
        %% BLOG ENTRIES
 
        {blog_article_welcome,
@@ -71,7 +65,7 @@ datamodel() ->
         [{title, <<"Welcome to Zotonic " ?ZOTONIC_VERSION "!">>},
          {publication_start, Now},
          {summary, <<"Zotonic is the content management system for people that want a fast, extensible, flexible and complete system for dynamic web sites. It is built from the ground up with rich internet applications ánd web publishing in mind.">>},
-         {body, {file, filename:join([z_utils:lib_dir(priv), "sites", ?MODULE, "demodata", "welcome.html"])}}
+         {body, {file, datafile(blog, "welcome.html")}}
         ]
        },
        {blog_article_learnmore,
@@ -79,14 +73,15 @@ datamodel() ->
         [{title, <<"Want to learn more?">>},
          {publication_start, z_datetime:prev_day(Now)},
          {summary, <<"This blog website you're looking demonstrates only a small part of what you can do with a Zotonic site. For instance, did you know that sending mass-mailings is a builtin module? That it does OAuth out of the box? That Zotonic sites are SEO optimized by default?">>},
-         {body, {file, filename:join([z_utils:lib_dir(priv), "sites", ?MODULE, "demodata", "learnmore.html"])}}]
+         {body, {file, datafile(blog, "learnmore.html")}}
+        ]
        },
        {blog_article_demo,
         article,
         [{title, <<"Zotonic's Typography">>},
          {publication_start, z_datetime:prev_month(Now)},
          {summary, <<"This article demonstrates the typographic features that Zotonic has. It shows creating ordered and unordered lists, blockquotes, and different methods of embedding media, even even showing an embedded video from Vimeo.com.">>},
-         {body, {file, filename:join([z_utils:lib_dir(priv), "sites", ?MODULE, "demodata", "demo.html"])}}
+         {body, {file, datafile(blog, "demo.html")}}
         ]
        },
 
@@ -104,44 +99,59 @@ datamodel() ->
         keyword,
         [{title, <<"Support">>}]
        }
+      ],
 
-      ]
-     },
-
-     {media,
+      media =
       [
        {media_learning,
-        filename:join([z_utils:lib_dir(priv), "sites", ?MODULE, "demodata", "learning.jpg"]),
+        datafile(blog, "learning.jpg"),
         [{title, <<"A bunch of computer books">>},
          {summary, <<"Taken by Sibi from Flickr, licensed Attribution-Noncommercial-No Derivative Works 2.0.">>}]
        },
        {media_welcome,
-        filename:join([z_utils:lib_dir(priv), "sites", ?MODULE, "demodata", "welcome.jpg"]),
+        datafile(blog, "welcome.jpg"),
         [{title, <<"Rocky sunrise">>},
          {summary, <<"Taken by Grant MacDonald from Flickr, CC licensed Attribution-Noncommercial 2.0.">>}]
        },
        {media_video,
         {<<"vimeo">>, <<"<object width=\"400\" height=\"225\"><param name=\"allowfullscreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><param name=\"movie\" value=\"http://vimeo.com/moogaloop.swf?clip_id=7630916&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=&amp;fullscreen=1\" /><embed src=\"http://vimeo.com/moogaloop.swf?clip_id=7630916&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=&amp;fullscreen=1\" type=\"application/x-shockwave-flash\" allowfullscreen=\"true\" allowscriptaccess=\"always\" width=\"400\" height=\"225\"></embed></object>">>},
-        [{title, <<"Zotonic introduction video">>}]
+          [{title, <<"Zotonic introduction video">>}]
        }
-      ]
-     },
+      ],
 
-     {edges,
+      edges = 
       [
        {blog_article_learnmore, author, administrator},
        {blog_article_welcome, author, administrator},
        {blog_article_demo, author, administrator},
-
+       
        {blog_article_learnmore, subject, kw_support},
        {blog_article_demo, subject, kw_technical},
        {blog_article_welcome, subject, kw_support},
        {blog_article_welcome, subject, kw_announcement},
-
+       
        {blog_article_welcome, depiction, media_welcome},
        {blog_article_learnmore, depiction, media_learning},
        {blog_article_demo, depiction, media_welcome}
-
+       
       ]
-     }
-    ].
+     },
+
+    ?DEBUG("Install blog"),
+    z_datamodel:manage(?MODULE, Datamodel, Context);
+
+
+install(_, _) ->
+    %% no/unknown skeleton = no default data
+    ok.
+
+%% @doc Retrieve the default menu structure for a given skeleton site. Used by mod_menu to create the menu.
+default_menu(blog) ->
+    [{page_home, []}, {page_about, []}, {page_contact, []}];
+
+default_menu(undefined) ->
+    [].
+
+
+datafile(Skeleton, Filename) ->
+    filename:join([z_utils:lib_dir(src), "install", "defaultdata", Skeleton, Filename]).
