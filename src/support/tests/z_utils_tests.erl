996bab5ed3244d931bacabba98498dec43477fe0
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-03-02 19:27:55 +0100

    Make z_utils:url_decode/1 work and add unit tests.
    This fixes issue 151

---
+++ src/support/tests/z_utils_tests.erl
@@ -0,0 +1,20 @@
+%% @author Arjan Scherpenisse <arjan@scherpenisse.net>
+%% @hidden
+
+-module(z_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("zotonic.hrl").
+
+
+url_encode_decode_test() ->
+    Url = "index.html?x=y&z=1",
+    ?assertEqual(Url, z_utils:url_decode(z_utils:url_encode(Url))).
+
+
+url_encode_test() ->
+    ?assertEqual("foo%26bar", z_utils:url_encode("foo&bar")).
+
+
+url_decode_test() ->
+    ?assertEqual("foo&bar", z_utils:url_decode("foo%26bar")).
