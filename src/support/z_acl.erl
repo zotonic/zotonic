3e59013c5a71741243ee353e0e4547f580770f5c
Marc Worrell <marc@worrell.nl>
2011-03-16 20:47:29 +0100

    Added license text.

--- src/support/z_acl.erl
+++ src/support/z_acl.erl
@@ -3,6 +3,20 @@
 %% Date: 2010-04-27
 %% @doc Access control for Zotonic.  Interfaces to modules implementing the ACL events.
 
+%% Copyright 2010 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(z_acl).
 -author("Marc Worrell <marc@worrell.nl>").
 
