6bcabda1914b4efb0b2c27e75ea6270dfe458d14
Marc Worrell <marc@worrell.nl>
2011-03-22 13:45:26 +0100

    Fixes for pivoting country names.  Expands iso code to country names in languages as defined in the page.  Only use the english postgresql indexing to prevent problems when searching in other languages.

--- modules/mod_l10n/mod_l10n.erl
+++ modules/mod_l10n/mod_l10n.erl
@@ -23,3 +23,74 @@
 -mod_title("Localization").
 -mod_description("Localization support and translations for country names etc.").
 
+-export([
+    observe_pivot_rsc_data/3
+]).
+
+-include("zotonic.hrl").
+
+%% @doc Expand the two letter iso code country depending on the languages in the resource.
+observe_pivot_rsc_data(pivot_rsc_data, Rsc, Context) ->
+    Languages = [ default_language(Context) | resource_languages(Rsc) ],
+    Rsc2 = expand_country(address_country, Rsc, Languages, Context),
+    expand_country(mail_country, Rsc2, Languages, Context).
+
+expand_country(Prop, Rsc, Languages, Context) ->
+    Rsc1 = map_country(Prop, Rsc),
+    case proplists:get_value(Prop, Rsc1) of
+        <<>> -> Rsc1;
+        undefined -> Rsc1;
+        <<_,_>> = Iso ->
+            Countries = lists:map(
+                            fun(Lang) -> 
+                                Context1 = z_context:set_language(Lang, Context),
+                                m_l10n:country_name(Iso, Context1)
+                            end,
+                            Languages),
+            add_alias(Iso, Rsc1 ++ [ {extra_pivot_data, C} || C <- lists:usort(Countries)]);
+        _Other -> Rsc1
+    end.
+
+    add_alias(<<"us">>, R) ->
+        [{extra_pivot_data, <<"USA">>} | R];
+    add_alias(<<"uk">>, R) ->
+        [{extra_pivot_data, <<"England">>} | R];
+    add_alias(<<"nl">>, R) ->
+        [{extra_pivot_data, <<"Holland">>} | R];
+    add_alias(<<"be">>, R) ->
+        [{extra_pivot_data, <<"België">>}, {extra_pivot_data, "Belgique"} | R];
+    add_alias(_, R) ->
+        R.
+
+%% @doc Map a country name to its iso code.  This very crude and should be more comprehensive.
+map_country(Prop, Rsc) ->
+    case proplists:get_value(Prop, Rsc) of
+        <<>> -> Rsc;
+        undefined -> Rsc;
+        Country ->
+            case z_string:to_lower(Country) of
+                "usa" -> [{Prop, <<"us">>} | Rsc];
+                "nederland" -> [{Prop, <<"nl">>} | Rsc];
+                "netherlands" -> [{Prop, <<"nl">>} | Rsc];
+                "the netherlands" -> [{Prop, <<"nl">>} | Rsc];
+                "belgië" -> [{Prop, <<"be">>} | Rsc];
+                % typos
+                "nethlerlands" -> [{Prop, <<"nl">>} | Rsc];
+                % just keep as-is
+                _ -> Rsc
+            end
+    end.
+
+
+default_language(Context) ->
+    case m_config:get_value(i18n, language, Context) of
+        undefined -> en;
+        <<>> -> en;
+        Lang -> z_convert:to_atom(Lang)
+    end.
+
+resource_languages(Rsc) ->
+    case proplists:get_value(language, Rsc) of
+        undefined -> [];
+        Langs -> [ z_convert:to_atom(Lang) || Lang <- Langs ]
+    end.
