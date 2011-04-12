2807bb4e7aca1183a0be2323c574978719707e28
Marc Worrell <marc@worrell.nl>
2011-04-12 16:59:44 +0200

    Fix for a problem where widgetManager is only called for the first element in the jquery context.

--- modules/mod_base/lib/js/apps/z.widgetmanager.js
+++ modules/mod_base/lib/js/apps/z.widgetmanager.js
@@ -172,7 +172,8 @@ limitations under the License.
     
 	$.fn.widgetManager = function()
 	{
-	    $.widgetManager(this[0]);
+		this.each(function() { $.widgetManager(this); });
+		return this;
 	};
 
 })(jQuery);
