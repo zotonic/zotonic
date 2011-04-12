4165c26adcf9cf958f6a9302c05e0c5195d33727
Marc Worrell <marc@worrell.nl>
2011-04-12 13:19:33 +0200

    Added copyright/license.

--- modules/mod_base/lib/js/modules/z.popupwindow.js
+++ modules/mod_base/lib/js/modules/z.popupwindow.js
@@ -1,3 +1,28 @@
+/* z.popupwindow.js
+----------------------------------------------------------
+
+@author Cody Lindley
+@author Marc Worrell <marc@worrell.nl>
+
+http://code.google.com/p/jquery-swip/
+
+Copyright 2009 Cody Lindley
+Copyright 2011 Marc Worrell
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+---------------------------------------------------------- */
+
 $.widget("ui.popupwindow", {
 	_init: function() {
 		self = this;
@@ -16,7 +41,7 @@ $.widget("ui.popupwindow", {
 			var centeredY, centeredX;
 	
 			if (self.options.centerBrowser) {
-				if ($.browser.msie) {//hacked together for IE browsers
+				if ($.browser.msie) {
 					centeredY = (window.screenTop - 120) + ((((document.documentElement.clientHeight + 120)/2) - (self.options.height/2)));
 					centeredX = window.screenLeft + ((((document.body.offsetWidth + 20)/2) - (self.options.width/2)));
 				} else {
