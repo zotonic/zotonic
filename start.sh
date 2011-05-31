a82c126cc0dbb3e731138ed6731de0ecf44bacca
Marc Worrell <marc@worrell.nl>
2011-05-31 11:52:31 +0200

    Simple debug startup script, useful for testing.

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# This starts Zotonic in debug mode.  
+#
+# Zotonic will use the settings in priv/config
+# Make your own by following the instructions in priv/config.in
+#
+# After it started you will be left in the Erlang shell.  
+# Leave the shell with: ctrl-C crtl-C
+#  
+./bin/zotonic debug
