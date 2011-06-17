0e33dead445d85538997de99b2fc01e9989036b9
Marc Worrell <marc@worrell.nl>
2011-06-17 12:35:36 +0200

    Allow the <address> elt in html.

--- src/support/z_html.erl
+++ src/support/z_html.erl
@@ -336,6 +336,7 @@ allow_once(_) -> false.
 
 %% @doc Allowed elements (see also allow_once/1 above)
 allow_elt(<<"audio">>) -> true;
+allow_elt(<<"address">>) -> true;
 allow_elt(<<"blockquote">>) -> true;
 allow_elt(<<"caption">>) -> true;
 allow_elt(<<"col">>) -> true;
