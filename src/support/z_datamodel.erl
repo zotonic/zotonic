923dd2c8d852dea0bc6c44b04859b956e6a59e4f
Marc Worrell <marc@worrell.nl>
2011-06-27 16:34:24 +0200

    Allow 'undefined' for the object/subject of a predicate validfor relation.

--- src/support/z_datamodel.erl
+++ src/support/z_datamodel.erl
@@ -205,7 +205,9 @@ manage_predicate_validfor(Id, [{SubjectCat, ObjectCat} | Rest], Context) ->
     SubjectId = m_rsc:name_to_id_check(SubjectCat, Context),
     ObjectId  = m_rsc:name_to_id_check(ObjectCat, Context),
 
-    F = fun(S, I, C) ->
+    F = fun(_S, _I, undefined) ->
+                ok;
+           (S, I, C) ->
                 case z_db:q("SELECT 1 FROM predicate_category WHERE predicate_id = $1 AND is_subject = $2 AND category_id = $3", [S, I, C], Context) of
                     [{1}] ->
                         ok;
