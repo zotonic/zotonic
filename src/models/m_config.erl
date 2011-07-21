551914cbd2618e53030240aca50e09574a7ca52c
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-07-21 15:25:11 +0200

    Config changes now trigger events. Fixes issue 183

--- src/models/m_config.erl
+++ src/models/m_config.erl
@@ -148,6 +148,7 @@ set_value(Module, Key, Value, Context) ->
         1 -> ok
     end,
     z_depcache:flush(config, Context),
+    z_notifier:notify({m_config_update, Module, Key, Value}, Context),
     ok.
 
 
@@ -159,6 +160,7 @@ set_prop(Module, Key, Prop, PropValue, Context) ->
         Id -> z_db:update(config, Id, [{Prop,PropValue}], Context)
     end,
     z_depcache:flush(config, Context),
+    z_notifier:notify({m_config_update_prop, Module, Key, Prop, PropValue}, Context),
     ok.
 
 
@@ -167,6 +169,7 @@ set_prop(Module, Key, Prop, PropValue, Context) ->
 delete(Module, Key, Context) ->
     z_db:q("delete from config where module = $1 and key = $2", [Module, Key], Context),
     z_depcache:flush(config, Context),
+    z_notifier:notify({m_config_update, Module, Key, undefined}, Context),
     ok.
 
 
