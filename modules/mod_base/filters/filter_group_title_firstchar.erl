dc608a858209c8932d5c1a340be33f55a4b499b2
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-09-12 15:56:27 +0200

    Generalized group_title_firstchar filter into group_firstchar.
    It can now filter on any given rsc property, e.g. name_surname for
    lists of persons. The assumption is that the input rsc list is already
    sorted on the property.

--- modules/mod_base/filters/filter_group_title_firstchar.erl
+++ modules/mod_base/filters/filter_group_title_firstchar.erl
@@ -1,8 +1,8 @@
 %% @author Arjan Scherpenisse <marc@worrell.nl>
-%% @copyright 2010 Arjan Scherpenisse
+%% @copyright 2010,2011 Arjan Scherpenisse
 %% @doc group a list of sorted rsc ids on their first letter of the title. Then, split this list in #Cols more-or-less even columns.
 
-%% Copyright 2010 Arjan Scherpenisse
+%% Copyright 2010,2011 Arjan Scherpenisse
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -24,42 +24,4 @@
 group_title_firstchar(undefined, _Cols, _Context) ->
     undefined;
 group_title_firstchar(In, Cols, Context) ->
-    Ids = erlydtl_runtime:to_list(In, Context),
-    Max = round(length(Ids)/Cols)+1,
-    First = [ [{first, firstchar_title(Id, Context)}, {id, Id}] || Id <- Ids],
-    F = z_utils:group_by(First, first, Context),
-    
-    X = [ [{first, [proplists:get_value(first, hd(P))]}, 
-           {result, [proplists:get_value(id, Pl) || Pl <- P]}]
-          || P <- F],
-    Grouped = grouped(X, Max),
-    Grouped.
-
-
-
-firstchar_title(Id, Context) ->
-    case z_convert:to_list(z_trans:lookup_fallback(m_rsc:p(Id, title, Context), Context)) of
-        [] -> undefined;
-        L -> z_string:first_char(L)
-    end.
-    
-
-
-grouped(GroupedList, Max) ->
-    grouped(GroupedList, [], 0, [], Max).
-
-grouped([], Cur, _CurLen, Acc, _Max) ->
-    lists:reverse([lists:reverse(Cur)|Acc]);
-
-grouped([Part | Rest], Cur, CurLen, Acc, Max) ->
-    L = length(proplists:get_value(result, Part)),
-    New = [Part|Cur],
-    case CurLen + L >= Max of
-        true ->
-            %% begin new group
-            grouped(Rest, [], 0, [lists:reverse(New)|Acc], Max);
-        false ->
-            %% Add to current group
-            grouped(Rest, New, CurLen+L, Acc, Max)
-    end.
-
+    filter_group_firstchar:group_firstchar(In, title, Cols, Context).
