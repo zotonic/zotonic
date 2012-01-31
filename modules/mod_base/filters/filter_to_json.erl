b7985d19e9eba68bc836f1f08b483d1a84912362
François Cardinaux <fcardinaux@gmail.com>
2012-01-31 01:00:56 +0100

    Modified the to_json filter to allow UTF8-encoded character strings in the input term

--- modules/mod_base/filters/filter_to_json.erl
+++ modules/mod_base/filters/filter_to_json.erl
@@ -16,8 +16,43 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(filter_to_json).
--export([to_json/2]).
+%% Modified by François Cardinaux in order to convert UTF8 strings correctly
+%% Usage: 
+%%    * 8-bit character encoding (ASCII, ISO8859-x, etc.): 
+%%          {{ value|to_json }}
+%%    * UTF-8 character encoding: 
+%%          {{ value|to_json:"utf8" }}
+%%    * Unicode character encoding, as defined in the mochijson module: 
+%%          {{ value|to_json:"unicode" }}
 
+-module(filter_to_json).
+-export([to_json/2, to_json/3]).
+
+%% @doc Convert an Erlang list or tuple to JSON
+%% @spec to_json(ErlangTerm, Context) -> Json
+%% Where: 
+%%    * ErlangTerm = list() | tuple()
+%%    * Context = Zotonic context record
+%%    * Json = JSON content
 to_json(Value, _Context) ->
     mochijson:encode(z_convert:to_json(Value)).
+    
+%% @doc Convert an Erlang list or tuple to JSON
+%% @spec to_json(ErlangTerm, Encoding, Context) -> Json
+%% Where: 
+%%    * ErlangTerm, Context and Json: like in to_json/2
+%%    * Encoding = 
+%%        * "utf8": ErlangTerm contains UTF8 strings
+%%        * "unicode": ErlangTerm contains unicode strings, as defined in the mochijson module
+to_json(Value, "utf8", _Context) ->
+    Encoder = mochijson:encoder([{input_encoding, utf8}]),
+    Encoder(z_convert:to_json(Value));
+    
+to_json(Value, "unicode", _Context) ->
+    Encoder = mochijson:encoder([{input_encoding, unicode}]),
+    Encoder(z_convert:to_json(Value));
+
+to_json(Value, _Encoding, Context) ->
+    to_json(Value, Context).
+    
+
