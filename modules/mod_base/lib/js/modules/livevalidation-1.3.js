ea7b16b330f7c56953bf814e3c873cbf3f100194
Michael Connors <mconnors@kavanagh.local>
2012-02-10 13:57:14 +0100

    move entire validation code into livevalidation-1.3.js

--- modules/mod_base/lib/js/modules/livevalidation-1.3.js
+++ modules/mod_base/lib/js/modules/livevalidation-1.3.js
@@ -908,11 +908,51 @@ var Validate = {
      **/
 
     Date: function(value, paramsObj){
+      function to_integer(value) {
+          if (parseInt(value) == value) {
+              return parseInt(value);
+          } else {
+              return parseInt("NaN");
+          }
+      }
+
       var paramsObj = paramsObj || {};
       var message = paramsObj.failureMessage || "Incorrect Date";
       var format = paramsObj.format || "l";
       var separator = paramsObj.separator || "-";
       value = $.trim(value);
+
+      var date_components = my_date.split(separator);
+      
+      if (date_components.length != 3) {
+          return false;
+      } else {
+          not_a_number = to_integer(separator);
+          if (!isNaN(not_a_number)) {
+              throw "Seperator cannot be a number!";
+          }
+          if (format == 'l') {
+              var day = to_integer(date_components[0]);
+              var month = to_integer(date_components[1]);
+              var year = to_integer(date_components[2]);
+          } else if (format == 'b') {
+              var day = to_integer(date_components[2]);
+              var month = to_integer(date_components[1]);
+              var year = to_integer(date_components[0]);
+          } else if (format == 'm') {
+              var day = to_integer(date_components[1]);
+              var month = to_integer(date_components[0]);
+              var year = to_integer(date_components[2]);
+          } else {
+              throw "Bad date format error!";
+          }
+          var date_object = new Date(year, month-1, day);
+          if (!((date_object.getDate() == day) && (date_object.getMonth()+1 == month) && (date_object.getFullYear() == year))) {
+              Validate.fail(message);
+          }
+      }
+
+
       if (!valid_date(value, format, separator)) {
           Validate.fail(message);
       }
