2e3f08467e6666d46c6dbe8c55375cc4c2a792ff
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-03-09 22:00:08 +0100

    mod_import_wordpress tweaks
    - Fix paragraphs in body text
    - Better feedback on crash
    - Add support for version 1.1
    (cherry picked from commit 29c8f0632791dcd13c6893c6e8fdd92e2f5b066d)

--- modules/mod_import_wordpress/support/import_wordpress.erl
+++ modules/mod_import_wordpress/support/import_wordpress.erl
@@ -18,7 +18,6 @@
 %% limitations under the License.
 -module(import_wordpress).
 
-
 -export([
          wxr_import/2,
          wxr_import/3,
@@ -26,10 +25,8 @@
          test/0
         ]).
 
-
 -include_lib("xmerl/include/xmerl.hrl").
 -include("zotonic.hrl").
-
 
 -define(RSS_NS, 'http://purl.org/rss/1.0/modules/content/').
 -define(WFW_NS, 'http://wellformedweb.org/CommentAPI/').
@@ -59,7 +56,11 @@ wxr_to_datamodel(Filename, Context) ->
 
     %% Make sure we support the right version. FIXME - make more flexible.
     [Version] = xmerl_xpath:string("wp:wxr_version", Channel),
-    <<"1.0">> = get_xmltext(Version),
+    case get_xmltext(Version) of
+        <<"1.0">> -> supported;
+        <<"1.1">> -> supported;
+        V -> throw({error, {unsupported_wxr_version, binary_to_list(V)}})
+    end,
 
     [Link] = xmerl_xpath:string("link", Channel),
     Base = z_convert:to_list(get_xmltext(Link)),
@@ -250,7 +251,8 @@ xml_attrib(Name, #xmlElement{attributes=Attrs}) ->
 wp_embed_tags(B) when is_binary(B) ->
     list_to_binary(wp_embed_tags(binary_to_list(B)));
 
-wp_embed_tags(BodyText) ->
+wp_embed_tags(BodyText0) ->
+    BodyText = "<p>" ++ re:replace(BodyText0, "\r\n\r\n", "</p><p>", [{return, list}, global]) ++ "</p>",
     Re = "(<a .*?>)?<img.*?class=\"(.*?wp-image-([0-9]+).*?)\".*?/>(</a>)?",
     case re:run(BodyText, Re, [{capture, all}]) of
         nomatch ->
