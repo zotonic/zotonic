becb69fc2792a67df7ee4e2d155fac0ef01536f1
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-03-17 09:27:23 +0100

    Convert z.dialog to Bootstrap modal

--- modules/mod_base/lib/js/modules/z.dialog.js
+++ modules/mod_base/lib/js/modules/z.dialog.js
@@ -1,142 +1,104 @@
 /* dialog js
-----------------------------------------------------------
+ ----------------------------------------------------------
 
-@package:	Zotonic 2009	
-@Author:	Tim Benniks <tim@timbenniks.nl>
+ @package:	Zotonic 2009, 2012
+ @Author:	Tim Benniks <tim@timbenniks.nl>
 
-Copyright 2009 Tim Benniks
+ Copyright 2009 Tim Benniks
+ Copyright 2012 Arjan Scherpenisse
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
  
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
  
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
----------------------------------------------------------- */
+ ---------------------------------------------------------- */
 
 (function($)
-{
-	$.extend({
-		dialogAdd: function(options)
-		{	
-			$('.dialog').remove(); 
+ {
+     $.extend(
+         {
+             dialogAdd: function(options)
+	     {	
+		 $('#zmodal').remove(); 
 
-			if(!$('.dialog').length)
-			{
-				// declare varaibles
-				var options, dialogWrapper, dialogTop, dialogTitle, dialogTLC, dialogTRC, dialogClose, dialogContent, dialogInnerContent, dialogRightContent, dialogBottom, dialogBLC, dialogBRC, leftPos, topPos;
-				var width;
-				
-				options = $.extend({}, $.ui.dialog.defaults, options);
-				
-				var dialogClass = 'dialog'
-				if (typeof(options.addclass) == "string")
-					dialogClass = 'dialog '+options.addclass
-					
-				if (typeof(options.width) == "number")
-					width = options.width + "px";
-				else
-					width = options.width;
+		 options = $.extend({}, $.ui.dialog.defaults, options);
+		     		     
+		 var width = options.width;
 
-				dialogTitle			= $('<h5></h5>').addClass('dialog-title').text(options.title);
-				dialogTLC			= $('<span></span>').addClass('dialog-top-left');
-				dialogTRC			= $('<span></span>').addClass('dialog-top-right');
-				dialogClose			= $('<span></span>').addClass('dialog-close').click(function(){ $.dialogRemove(dialogWrapper); });
-				dialogInnerContent	= $('<div></div>').addClass('dialog-inner-content').html(options.text);
-				dialogRightContent	= $('<span></span>').addClass('dialog-content-right');
-				dialogBLC			= $('<span></span>').addClass('dialog-bottom-left');
-				dialogBRC			= $('<span></span>').addClass('dialog-bottom-right');
-				leftPos				= Math.floor((parseInt($(window).width()) / 2) - (parseInt(width) / 2));
-				
-				var scrollTop = $(window).scrollTop();
-				topPos				= scrollTop + 100;
-			
-				dialogTop			= $('<div></div>').addClass('dialog-top').append(dialogTitle, dialogTLC, dialogTRC, dialogClose);
-				dialogContent		= $('<div></div>').addClass('dialog-content clearfix').append(dialogInnerContent, dialogRightContent);
-				dialogBottom		= $('<div></div>').addClass('dialog-bottom').append(dialogBLC, dialogBRC);
-			
-				dialogWrapper		= $('<div></div>')
-										.addClass(dialogClass)
-										.append(dialogTop, dialogContent, dialogBottom)
-										.fadeIn(300)
-										.css({left: leftPos, top: topPos, width: width})
-										.draggable({addClasses: false, handle: dialogTop, opacity: 0.90, zIndex: 2700, iframeFix: true, scroll: true});
+		 var title = $("<div>").addClass("modal-header")
+                     .append($("<a>").addClass("close").attr("data-dismiss", "modal").html("&times;"))
+                     .append($("<h3>").text(options.title));
 
-				$(document).keypress(function(e)
-				{
-					if($.browser.msie)	{ var key = e.which }
-					else				{ key = e.keyCode } 
-					
-					if(key == $.ui.keyCode.ESCAPE)
-					{
-						dialogClose.click();
-					}
-				});
-				
-				$('body').append(dialogWrapper);
+                 var body = $("<div>").addClass("modal-body")
+                     .html(options.text);
+                 
+		 var dialogClass = 'modal';
+		 if (typeof(options.addclass) == "string")
+		     dialogClass += ' ' + options.addclass;
 
-				/* Make sure that the dialog is within the viewport */
-				var dialogHeight = dialogWrapper.height();
-				var windowHeight = $(window).height();
-				if (100 + dialogHeight > windowHeight) {
-					var newTop = scrollTop + windowHeight - dialogHeight - 20;
-					
-					$(dialogWrapper).css({top: newTop > scrollTop ? newTop : scrollTop});
-				}
+                 var dialog = $("<div>")
+                     .attr("id", "zmodal")
+                     .addClass(dialogClass)
+                     .append(title)
+                     .append(body)
+                     .appendTo($("body"));
 
-				$('input[type=text]', dialogWrapper).focus();
-				if (typeof($.widgetManager) != 'undefined') {
-					dialogWrapper.widgetManager();
-				}
-				z_tinymce_add(dialogWrapper);
-			}
-		},
+                 dialog.modal({backdrop: true});
 
-		dialogClose: function()
-		{
-			$('.dialog-close').click();
-		},
-		
-		dialogRemove: function(obj)
-		{
-			obj = obj || $('.dialog');
-			z_tinymce_remove(obj);
-			obj.draggable('destroy').resizable('destroy').fadeOut(300, function()
-			{
-				$(this).remove();
-			});
-		}
-	});
-	
-	$.widget("ui.show_dialog", 
-	{
-		_init: function() 
-		{
-			var title	= this.options.title;
-			var text	= this.options.text;
-			var width	= this.options.width;
-			
-			this.element.click(function()
-			{
-				$.dialogAdd(
-				{
-					title: title,
-					text:  text,
-					width: width
-				})
-			})
-		}
-	});
-	
-	$.ui.dialog.defaults = {
-		title: 'Title',
-		text: 'tekst',
-		width: '450px'
-	}
-})(jQuery);
+		 if (typeof($.widgetManager) != 'undefined') {
+		     dialog.widgetManager();
+		 }
+		 z_tinymce_add(dialog);
+
+	     },
+
+	     dialogClose: function()
+	     {
+		 $('#zmodal').modal('hide');
+	     },
+	     
+	     dialogRemove: function(obj)
+	     {
+		 obj = obj || $('#zmodal');
+		 z_tinymce_remove(obj);
+		 obj.draggable('destroy').resizable('destroy').fadeOut(300, function()
+			                                               {
+				                                           $(this).remove();
+			                                               });
+	     }
+	 });
+     
+     $.widget("ui.show_dialog", 
+	      {
+		  _init: function() 
+		  {
+		      var title	= this.options.title;
+		      var text	= this.options.text;
+		      var width	= this.options.width;
+		      
+		      this.element.click(function()
+			                 {
+				             $.dialogAdd(
+				                 {
+					             title: title,
+					             text:  text,
+					             width: width
+				                 });
+			                 });
+		  }
+	      });
+     
+     $.ui.dialog.defaults = {
+	 title: 'Title',
+	 text: 'tekst',
+	 width: '450px'
+     };
+ })(jQuery);
