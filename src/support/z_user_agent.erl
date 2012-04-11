132d9e183b6b3f6ba866b414b1c0671f4253a360
Marc Worrell <marc@worrell.nl>
2012-04-11 15:42:36 +0200

    Make ua vars available in m_req. Copy ua vars to z_context vars.

--- src/support/z_user_agent.erl
+++ src/support/z_user_agent.erl
@@ -22,6 +22,7 @@
 -export([
     set_class/1,
     get_class/1,
+    get_props/1,
     filename_split_class/1,
     order_class/2,
     classes/0
@@ -29,7 +30,10 @@
 
 -include_lib("zotonic.hrl").
 
+-type ua_template_class() :: ua_classifier:device_type() | generic.
+
 %% @doc Classify the user agent in the wm_reqdata.
+-spec set_class(#wm_reqdata{}) -> {ok, #wm_reqdata{}}.
 set_class(#wm_reqdata{} = ReqData) ->
     case wrq:get_req_header_lc("user-agent", ReqData) of
         undefined ->
@@ -40,10 +44,14 @@ set_class(#wm_reqdata{} = ReqData) ->
         UserAgent ->
             case ua_classifier:classify(UserAgent) of
                 {ok, Props} ->
+                    {ok, RD1} = webmachine_request:set_metadata(
+                                    ua_class,
+                                    ua_classifier:device_type(Props),
+                                    ReqData),
                     webmachine_request:set_metadata(
-                        ua_class,
-                        ua_classifier:device_type(Props),
-                        ReqData);
+                            ua_props,
+                            Props,
+                            RD1);
                 {error, _Reason} ->
                     webmachine_request:set_metadata(
                         ua_class,
@@ -53,6 +61,7 @@ set_class(#wm_reqdata{} = ReqData) ->
     end.
 
 %% @doc Get the user agent class
+-spec get_class( #context{} | #wm_reqdata{} | undefined ) -> ua_classifier:device_type().
 get_class(undefined) ->
     desktop;
 get_class(#context{} = Context) ->
@@ -63,7 +72,21 @@ get_class(#wm_reqdata{} = ReqData) ->
         UAClass -> UAClass
     end.
 
+%% @doc Get the user agent class
+-spec get_props( #context{} | #wm_reqdata{} | undefined ) -> [ {atom(), term()} ].
+get_props(undefined) ->
+    [{id, <<"desktop">>}];
+get_props(#context{} = Context) ->
+    get_props(z_context:get_reqdata(Context));
+get_props(#wm_reqdata{} = ReqData) ->
+    case webmachine_request:get_metadata(ua_props, ReqData) of
+        undefined -> [{id, <<"desktop">>}];
+        Props -> Props
+    end.
+
+
 %% @doc Split a filename (with subdir) to a device class and a relative path.
+-spec filename_split_class(file:filename()) -> { ua_template_class(), file:filename() }.
 filename_split_class(F) ->
     case filename:split(F) of
         ["desktop"|T] -> {desktop, string:join(T, "/")};
@@ -75,6 +98,7 @@ filename_split_class(F) ->
 
 
 %% @doc Ordering function for lists:sort/2. Orders by genericity. Most specific first.
+-spec order_class( ua_classifier:device_type(), ua_classifier:device_type() ) -> boolean().
 order_class(A,A) -> true;
 order_class(_, generic) -> false;
 order_class(desktop, _) -> true;
@@ -85,5 +109,6 @@ order_class(_, _) -> false.
 
 
 %% @doc Return all possible UA classes
+-spec classes() -> [ ua_classifier:device_type() ].
 classes() ->
     [ text, phone, tablet, desktop ].
