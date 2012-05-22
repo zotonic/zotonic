42b33cc9bdc7a058a0ade03cfdb948a05767f27d
Marc Worrell <marc@worrell.nl>
2012-05-22 13:10:25 +0200

    Show simpe popups for features. Might need tpls from server instead.

--- modules/mod_geomap/lib/js/geomap.js
+++ modules/mod_geomap/lib/js/geomap.js
@@ -24,17 +24,12 @@
         }
     });
     
-    /* Just show where we clicked */
-    var display = function(event) {
-        console.log(event);
-        for(var i=0; i < event.feature.cluster.length; ++i) {
-            console.log(event.feature.cluster[i].data);
-        }
-    }
-
     var GeoMap = {
         _map: null,
-        _base: null
+        _base: null,
+        _selectControl: null,
+        _selectedFeature: null,
+        _popup : null
     };
     
     GeoMap.init = function() {
@@ -63,100 +58,115 @@
         var center = new OpenLayers.LonLat(0, 30).transform(new OpenLayers.Projection("EPSG:4326"), GeoMap.map().getProjectionObject());
         this._map.setCenter(center, 3);
     };
+
     GeoMap.map = function() {
         return this._map;
     };
-    GeoMap.addCollection = function(name, searchParams) {
-        var style = new OpenLayers.Style({
-            pointRadius: "${radius}",
-            fillColor: "#ffcc66",
-            fillOpacity: 0.8,
-            strokeColor: "#cc6633",
-            strokeWidth: 2,
-            strokeOpacity: 0.8
-        }, {
-            context: {
-                radius: function(feature) {
-                    return Math.min(feature.attributes.count, 7) + 3;
+    GeoMap.selectControl = function() {
+        return this._selectControl;
+    }
+
+    GeoMap.addCollections = function(list) {
+        var self = this;
+        var default_style = new OpenLayers.Style({
+                pointRadius: "${radius}",
+                fillColor: "#ffcc66",
+                fillOpacity: 0.8,
+                strokeColor: "#cc6633",
+                strokeWidth: 2,
+                strokeOpacity: 0.8
+            }, {
+                context: {
+                    radius: function(feature) {
+                        return Math.min(feature.attributes.count, 7) + 5;
+                    }
                 }
-            }
-        });
-        searchParams.count = searchParams.count || 1000;
-        var searchLayer = new OpenLayers.Layer.Vector(name, {
-            projection: "EPSG:4326",
-            strategies: [
-                new OpenLayers.Strategy.Fixed(),
-                new OpenLayers.Strategy.Cluster()
-            ],
-            protocol: new OpenLayers.Protocol.Script({
-                url: "/api/geomap/locations",
-                params: searchParams,
-                callbackKey: 'jsonp',
-                format: new OpenLayers.Format.ModGeoMap()
-            }),
-            styleMap: new OpenLayers.StyleMap({
-                "default": style,
-                "select": {
-                    fillColor: "#8aeeef",
-                    strokeColor: "#32a8a9"
+            });
+
+        var layers = [];
+        for (i=0; i<list.length; i++)
+        {
+            var c = list[i];
+            c.query.count = c.query.count || 1000;
+            var layer = new OpenLayers.Layer.Vector(c.name, {
+                projection: "EPSG:4326",
+                strategies: [
+                    new OpenLayers.Strategy.Fixed(),
+                    new OpenLayers.Strategy.Cluster()
+                ],
+                protocol: new OpenLayers.Protocol.Script({
+                    url: "/api/geomap/locations",
+                    params: c.query,
+                    callbackKey: 'jsonp',
+                    format: new OpenLayers.Format.ModGeoMap()
+                }),
+                styleMap: new OpenLayers.StyleMap({
+                    "default": default_style,
+                    "select": {
+                        fillColor: "#8aeeef",
+                        strokeColor: "#32a8a9"
+                    }
+                }),
+                eventListeners: {
+                    "featureselected": function(evt) { self.featureSelect(evt); },
+                    "featureunselected": function(evt) { self.featureUnselect(evt); }
                 }
-            })
-        });
-        this._map.addLayers([searchLayer]);
+            });
+            layers.push(layer)
+        }
+        var selectFeatures = new OpenLayers.Control.SelectFeature(layers, { click: true });
+        this._map.addLayers(layers);
+        this._map.addControl(selectFeatures);
+        this._selectControl = selectFeatures;
+        selectFeatures.activate();
+    };
+    
+    // Needed only for interaction, not for the display.
+    GeoMap.featureSelect = function(evt) {
+        this._selectedFeature = evt.feature;
+        var self = this;
+        var cluster = this._selectedFeature.cluster;
+        var t = "";
+        
+        for (var i=0, len=cluster.length; i<Math.min(len,3); i++) {
+            var d = cluster[i].data;
+            t += "<h3><a href='"+d.page_url+"'>"+d.title+"</a></h3>"
+                 + "<p>"+d.address_country
+                 + ", " + d.address_city
+                 + ", " + d.address_street_1
+                 + "</p>";
+        }
+        if (cluster.length > 3) {
+            t = t+"<p class='geomap-other'>And <span>"+(cluster.length-3)+"</span> other.</p>";
+        }
+        this._popup = new OpenLayers.Popup.FramedCloud("featurePopup",
+                                 this._selectedFeature.geometry.getBounds().getCenterLonLat(),
+                                 new OpenLayers.Size(100,100),
+                                 t,
+                                 null, true, 
+                                 function(_closeEvt) {
+                                     // 'this' is the popup.
+                                     if (this.feature.layer) {
+                                         self._selectControl.unselect(this.feature);
+                                     } else { 
+                                         this.destroy();
+                                     }
+                                 });
+        this._selectedFeature.popup = this._popup;
+        this._popup.feature = this._selectedFeature;
+        GeoMap.map().addPopup(this._popup, true);
+    }
+    GeoMap.featureUnselect = function(evt) {
+        this._selectedFeature = evt.feature;
+        if (this._selectedFeature.popup) {
+            this._popup.feature = null;
+            this._map.removePopup(this._selectedFeature.popup);
+            this._selectedFeature.popup.destroy();
+            this._selectedFeature.popup = null;
+        }
     };
 
     window.GeoMap = GeoMap;
 
-/*
-    var.style = new OpenLayers.Style({
-        pointRadius: "${radius}",
-        fillColor: "#ffcc66",
-        fillOpacity: 0.8,
-        strokeColor: "#cc6633",
-        strokeWidth: 2,
-        strokeOpacity: 0.8
-    }, {
-        context: {
-            radius: function(feature) {
-                return Math.min(feature.attributes.count, 7) + 3;
-            }
-        }
-    });
-
-    var people = new OpenLayers.Layer.Vector("People", {
-        projection: "EPSG:4326",
-        strategies: [
-            new OpenLayers.Strategy.Fixed(),
-            new OpenLayers.Strategy.Cluster()
-        ],
-        protocol: new OpenLayers.Protocol.Script({
-            url: "/api/geomap/locations",
-            params: {
-                cat: 'person',
-                count: 1000
-            },
-            callbackKey: 'jsonp',
-            format: new OpenLayers.Format.ModGeoMap()
-        }),
-        styleMap: new OpenLayers.StyleMap({
-            "default": style,
-            "select": {
-                fillColor: "#8aeeef",
-                strokeColor: "#32a8a9"
-            }
-        })
-    });
-
-    var select = new OpenLayers.Control.SelectFeature(people, {click: true});
-
-    map.addControl(select);
-    select.activate();
-    people.events.on({"featureselected": display});
-
-    map.addLayers([base, people]);
-
-    // template setup
-    // template = new jugl.Template("template");
-*/
 }( window.jQuery );
 
