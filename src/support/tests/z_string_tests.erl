607407d96ee7ade36f5f1cf8312321de7acee1b2
Motiejus Jakštys <desired.mta@gmail.com>
2012-10-05 17:45:38 +0100

    FIX z_string:s_utf8/2
    Current checker does not validate that codepoints are within allowed
    range. For instance, this encodes U-00000, but passes the current
    validator: <<2#110:3, 0:5, 2#10:2, 0:6>>. (<<16#C080>>).
    
    New checker takes into account both utf8 syntax and allowed values that
    certain length codepoints can encode.

--- src/support/tests/z_string_tests.erl
+++ src/support/tests/z_string_tests.erl
@@ -41,3 +41,31 @@ ends_with_test() ->
     ?assert(z_string:ends_with(["is ", <<"text.">>], "This is text.")),
     ?assertNot(z_string:ends_with(["is ", <<"jpeg.">>], "This is text.")),
     ok.
+
+valid_utf8_test_() ->
+    [
+        ?_assert(v_utf8(<<>>)),
+        ?_assert(v_utf8(<<127>>)),
+        ?_assert(v_utf8(<<2#11001111, 2#10000000>>)),
+        ?_assert(v_utf8(<<2#11011111, 2#10111111>>)),
+
+        ?_assert(v_utf8(<<2#11101000, 2#10000000, 2#10000000>>)),
+        ?_assert(v_utf8(<<2#11101111, 2#10111111, 2#10111101>>)),
+
+        ?_assert(v_utf8(<<2#11110100, 2#10000000, 2#10000000, 2#10000000>>)),
+        ?_assert(v_utf8(<<2#11110000, 2#10111111, 2#10111111, 2#10111111>>)),
+
+        ?_assertNot(v_utf8(<<128>>)),
+
+        ?_assertNot(v_utf8(<<2#11100000, 2#10000000>>)),
+        ?_assertNot(v_utf8(<<2#11000000, 2#11000000>>)),
+
+        ?_assertNot(v_utf8(<<2#11110000, 2#10000000, 2#10000000>>)),
+        ?_assertNot(v_utf8(<<2#11100000, 2#11000000, 2#10000000>>)),
+        ?_assertNot(v_utf8(<<2#11100000, 2#10000000, 2#11000000>>)),
+
+        ?_assertNot(v_utf8(<<2#11111000, 2#10000000, 2#10000000, 2#10000000>>))
+    ].
+
+v_utf8(Bin) ->
+    z_string:sanitize_utf8(Bin) =:= Bin.
