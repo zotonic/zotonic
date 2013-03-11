91106b44a026d7ecafeacce55659dfe042ca1f31
Arjan Scherpenisse <arjan@scherpenisse.net>
2013-03-11 14:50:33 +0100

    mod_base: Revert commit 020fc6eb733435c0c586dff6a37dd154a306db30
    The validator is attached to the "jquery data" of the element, which
    is be destroyed when the element is removed from the DOM tree. And the
    validations are only added when the form is added.

--- modules/mod_base/lib/js/apps/zotonic-1.0.js
+++ modules/mod_base/lib/js/apps/zotonic-1.0.js
@@ -1230,6 +1230,12 @@ function z_set_validator_postback(id, postback)
 {
 	if (postback)
 	{
+		var pb = $('#'+id).data("z_postback_validation");
+		if (pb)
+		{
+			$.misc.error("Element #"+id+" had already a validation postback, add all validations as one batch.", $('#' +id));
+		}
+
 		$('#'+id).data("z_postback_validation", postback);
 	}
 }
