ae29260bf0fc95c1b39b337eaaeee6c463988b09
Marc Worrell <marc@worrell.nl>
2013-03-28 11:33:28 +0100

    mod_base: added 'only_text' option to alert. Corrected documentation.

--- modules/mod_base/actions/action_base_alert.erl
+++ modules/mod_base/actions/action_base_alert.erl
@@ -32,15 +32,11 @@ render_action(TriggerId, TargetId, Args, Context) ->
 %% @doc Fill the dialog with the delete confirmation template. The next step will ask to delete the resource
 %% @spec event(Event, Context1) -> Context2
 event(#postback{message={alert, Args}}, Context) ->
-    Text = proplists:get_value(text, Args, ""),
     Title = proplists:get_value(title, Args, ?__(<<"Alert">>, Context)),
-    Button = proplists:get_value(button, Args),
-    Action = proplists:get_all_values(action, Args),
     Vars = [
         {title, Title},
-        {text, Text},
-        {button, Button},
-        {action, Action}
+        {action, proplists:get_all_values(action, Args)}
+        | Args
     ],
     z_render:dialog(Title, "_action_dialog_alert.tpl", Vars, Context).
 
--- modules/mod_base/templates/_action_dialog_alert.tpl
+++ modules/mod_base/templates/_action_dialog_alert.tpl
@@ -1,5 +1,7 @@
 <p class="text-warning">{{ text }}</p>
 
+{% if not only_text %}
 <div class="modal-footer">
 	{% button class="btn btn-primary" text=button|default:_"OK" action={dialog_close} action=action postback=postback delegate=delegate %}
 </div>
+{% endif %}
