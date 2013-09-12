68a85dc889005071a6623159eb6520be0a0c425b
Marc Worrell <marc@worrell.nl>
2013-09-12 14:30:59 +0200

    mod_import_csv: improved parsing of CSV files, now handles quoted fields and performs automatic translation from latin-1 to utf-8 (cherry picked from commit 94e0a591ccd6f09d2a18f759c1a4458dcf12fd79)

--- modules/mod_import_csv/support/parse_csv.erl
+++ modules/mod_import_csv/support/parse_csv.erl
@@ -1,7 +1,8 @@
 %% @doc Import a csv file according to the derived file/record definitions.
 %% @author Arjan Scherpenisse <arjan@scherpenisse.net>
+%% Date: 2010-06-26
 
-%% Copyright 2010-2013 Arjan Scherpenisse
+%% Copyright 2010-2011 Arjan Scherpenisse
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -15,37 +16,43 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(parse_csv).
+-module(parse_csv_).
 -author("Arjan Scherpenisse <arjan@scherpenisse.net>").
 
 -export([
+    scan_lines/1,
     scan_lines/2,
-    trim_field/1
+    cleanup_field/1
 ]).
-
--include("zotonic.hrl").
-
-%% @doc Scan all lines, split in columns.
-%% @todo Handle quoted fields (with escapes).
-scan_lines(Device, FieldSep) ->
-    scan_lines(Device, FieldSep, <<>>, 0, [[]], <<>>).
-
 
 -define(CHUNK_SIZE, 4096).
 
-scan_lines(Device, Fs, Chunk, Index, Acc, Remainder) ->
-    case Chunk of
-        <<>> ->
-            %%
-            %% Chunk is empty. Get the next chunk from the file.
-            %%
+
+scan_lines(Device) ->
+    scan_lines(Device, $,).
+
+scan_lines(Device, FieldSep) ->
+    scan_lines(Device, FieldSep, <<>>, 0, [[]], <<>>, false).
+
+scan_lines(Device, Fs, Chunk, Index, Acc, Remainder, Quoted) ->
+    case {Chunk, Quoted} of
+        % Chunk is empty. Get the next chunk from the file.
+        {EmptyChunk, _}
+            when 
+                EmptyChunk =:= <<>>;
+                EmptyChunk =:= <<$\\>>;
+                (EmptyChunk =:= <<$">> andalso Quoted);
+                EmptyChunk =:= <<13>> ->
             case io:get_chars(Device, "", ?CHUNK_SIZE) of
                 eof ->
                     All = case Remainder of 
-                              <<>> ->
-                                  Acc;
+                              <<>> -> 
+                                Acc;
                               _ ->
-                                  append_field(<<>>, Remainder, Acc)
+                                case EmptyChunk of
+                                    <<$">> -> append_field(<<$">>, Remainder, Acc);
+                                    _ -> append_field(<<>>, Remainder, Acc)
+                                end
                           end,
                     %% Remove lastly added empty line
                     All2 = case All of
@@ -56,52 +63,113 @@ scan_lines(Device, Fs, Chunk, Index, Acc, Remainder) ->
                 {error, E} ->
                     throw({error, E});
                 NextChunk ->
-                    scan_lines(Device, Fs, NextChunk, 0, Acc, Remainder)
+                    NewChunk = case EmptyChunk of
+                                    <<>> -> NextChunk;
+                                    _ -> <<EmptyChunk/binary, NextChunk/binary>>
+                               end,
+                    scan_lines(Device, Fs, NewChunk, 0, Acc, Remainder, Quoted)
             end;
 
-        <<Field:Index/binary, C, Rest/binary>> ->
-            case C of
-                13 -> scan_lines(Device, Fs, Rest, 0, [ [] | append_last_field(Remainder, Field, Acc)], <<>>);
-                10 -> scan_lines(Device, Fs, Rest, 0, [ [] | append_last_field(Remainder, Field, Acc)], <<>>);
-                Fs -> scan_lines(Device, Fs, Rest, 0, append_field(Remainder, Field, Acc), <<>>);
-                _ -> scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder)
-            end;
-        LongLine ->
-            %% Long line; add to remainder.
-            scan_lines(Device, Fs, <<>>, 0, Acc, <<Remainder/binary, LongLine/binary>>)
+        % Escaped characters
+
+        {<<_Field:Index/binary, $\\, 13, 10, _Rest/binary>>, _} ->
+            lager:warning("Escaped NL"),
+            scan_lines(Device, Fs, Chunk, Index + 3, Acc, Remainder, Quoted);
+
+        {<<_Field:Index/binary, $\\, _, _Rest/binary>>, _} ->
+            lager:warning("Escaped Char"),
+            scan_lines(Device, Fs, Chunk, Index + 2, Acc, Remainder, Quoted);
+
+        % Quoted ----
+
+        {<<_Field:Index/binary, $", $", _Rest/binary>>, true} ->
+            scan_lines(Device, Fs, Chunk, Index + 2, Acc, Remainder, true);
+
+        {<<_Field:Index/binary, $", _Rest/binary>>, true} ->
+            scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder, false);
+
+        {<<_Field:Index/binary, 13, 10, _Rest/binary>>, true} ->
+            scan_lines(Device, Fs, Chunk, Index + 2, Acc, Remainder, true);
+
+        {<<_Field:Index/binary, 10, _Rest/binary>>, true} ->
+            scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder, true);
+
+        {<<_Field:Index/binary, _, _Rest/binary>>, true} ->
+            scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder, true);
+
+        % Unquoted ----
+
+        {<<_Field:Index/binary, $", _Rest/binary>>, false} when Index =:= 0 andalso Remainder =:= <<>> ->
+            scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder, true);
+
+        {<<Field:Index/binary, 13, 10, Rest/binary>>, false} ->
+            scan_lines(Device, Fs, Rest, 0, [ [] | append_last_field(Remainder, Field, Acc)], <<>>, false);
+
+        {<<Field:Index/binary, 10, Rest/binary>>, false} ->
+            scan_lines(Device, Fs, Rest, 0, [ [] | append_last_field(Remainder, Field, Acc)], <<>>, false);
+
+        {<<Field:Index/binary, Fs, Rest/binary>>, false} ->
+            scan_lines(Device, Fs, Rest, 0, append_field(Remainder, Field, Acc), <<>>, false);
+
+        {<<_Field:Index/binary, _, _Rest/binary>>, false} ->
+            scan_lines(Device, Fs, Chunk, Index + 1, Acc, Remainder, false);
+ 
+        % Long line; add to remainder.
+        {LongLine, _} ->
+            scan_lines(Device, Fs, <<>>, 0, Acc, <<Remainder/binary, LongLine/binary>>, Quoted)
     end.
 
 append_field(<<>>, Field, [Row|Rows]) ->
-    [[clean_field(Field)|Row]|Rows];
+    [[cleanup_field(Field)|Row]|Rows];
 append_field(Prefix, Field, [Row|Rows]) ->
     NewField = <<Prefix/binary, Field/binary>>,
-    [[clean_field(NewField)|Row]|Rows].
+    [[cleanup_field(NewField)|Row]|Rows].
 append_last_field(Prefix, Field, Acc) ->
     [R|RS] = append_field(Prefix, Field, Acc),
     [lists:reverse(R)|RS].
 
 
-clean_field(S) ->
-    unescape(trim_field(S)).
-
 %% Remove any quotes and whitespace around the fields.
-trim_field(S) ->
-    z_string:trim(z_string:unquote(S)).
+cleanup_field(L) when is_list(L) ->
+    cleanup_field(z_convert:to_binary(L));
+cleanup_field(<<>>) ->
+    <<>>;
+cleanup_field(<<$", _/binary>> = S) ->
+    utf8(unescape(z_convert:to_binary(z_string:trim(z_string:unquote(S))), true));
+cleanup_field(S) ->
+    utf8(unescape(z_convert:to_binary(z_string:trim(S)), false)).
 
-unescape(S) ->
-    unescape(S, <<>>).
+utf8(S) ->
+    case mochiutf8:valid_utf8_bytes(S) of
+        S ->
+            S;
+        Stripped -> 
+            case eiconv:convert("Windows-1250", S) of
+                {ok, Utf8} -> Utf8;
+                {error, _} -> Stripped
+            end
+    end.
 
-unescape(<<>>, Acc) ->
+unescape(S, IsQuoted) ->
+    unescape(S, <<>>, IsQuoted).
+
+unescape(<<>>, Acc, _IsQuoted) ->
     Acc;
-unescape(<<$\\, $n, Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, 10>>);
-unescape(<<$\\, $r, Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, 13>>);
-unescape(<<$\\, $t, Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, 9>>);
-unescape(<<$\\, $', Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, $'>>);
-unescape(<<$\\, $", Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, $">>);
-unescape(<<C, Rest/binary>>, Acc) ->
-    unescape(Rest, <<Acc/binary, C>>).
+unescape(<<$\\, $\\, Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, $\\>>, IsQuoted);
+unescape(<<$\\, $n, Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, 10>>, IsQuoted);
+unescape(<<$\\, $r, Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, 13>>, IsQuoted);
+unescape(<<$\\, $t, Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, 9>>, IsQuoted);
+unescape(<<$\\, $', Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, $'>>, IsQuoted);
+unescape(<<$\\, $", Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, $">>, IsQuoted);
+unescape(<<$", $", Rest/binary>>, Acc, true) ->
+    unescape(Rest, <<Acc/binary, $">>, true);
+unescape(<<C, Rest/binary>>, Acc, IsQuoted) ->
+    unescape(Rest, <<Acc/binary, C>>, IsQuoted).
+
+
