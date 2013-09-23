93dd34a46941c7547d9723b2007b30b64751cfc3
Marc Worrell <marc@worrell.nl>
2013-09-23 23:20:42 +0200

    mod_base: let split filter handle binaries and undefined in a 'sane' way.

--- modules/mod_base/filters/filter_split.erl
+++ modules/mod_base/filters/filter_split.erl
@@ -1,8 +1,8 @@
 %% @author Dmitrii Dimandt <dmitrii@dmitriid.com>
-%% @copyright 2010 Marc Worrell
+%% @copyright 2010 Dmitrii Dimandt, Marc Worrell
 %% @doc 'unjoin' filter, "reverse" of join, split a string into a list.
 
-%% Copyright 2010 Marc Worrell
+%% Copyright 2010 Dmitrii Dimandt, Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -20,6 +20,14 @@
 -export([split/3]).
 
 
+split(undefined, _Sep, _Context) ->
+	undefined;
+split(<<>>, _Sep, _Context) ->
+	[];
+split([], _Sep, _Context) ->
+	[];
+split(String, Sep, _Context) when is_binary(String), is_binary(Sep) ->
+	binary:split(String, Sep, [global]); 
 split(String, Sep, _Context) ->
     z_string:split(z_convert:to_list(String), z_convert:to_list(Sep)).
 
