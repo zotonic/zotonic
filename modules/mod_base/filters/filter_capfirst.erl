30aebfd7772795f142bfd018c50958d997f8c801
Marc Worrell <marc@worrell.nl>
2014-01-21 23:57:13 +0100

    mod_base: simple fix capfirst on empty input. Still todo: make utf-8 safe.

--- modules/mod_base/filters/filter_capfirst.erl
+++ modules/mod_base/filters/filter_capfirst.erl
@@ -27,4 +27,6 @@ capfirst([H|T], _Context) when H >= $a andalso H =< $z ->
 capfirst(<<Byte:8/integer, Binary/binary>>, _Context) when Byte >= $a andalso Byte =< $z ->
     [<<(Byte + $A - $a)>>, Binary];
 capfirst({trans, _} = In, Context) ->
-    capfirst(erlydtl_runtime:to_list(In, Context), Context).
+    capfirst(z_trans:lookup_fallback(In, Context), Context);
+capfirst(A, _Context) ->
+	A.
