5a93bea9a557cace275bb5b69d4d960c9badcc7d
Marc Worrell <marc@worrell.nl>
2014-01-21 23:56:24 +0100

    mod_admin: fix mixed tabs/spaces (now spaces)

--- modules/mod_admin/lib/js/apps/admin-common.js
+++ modules/mod_admin/lib/js/apps/admin-common.js
@@ -1,8 +1,8 @@
 /* Admin Common js
 ----------------------------------------------------------
 
-@package:	Zotonic 2009
-@Author:	Tim Benniks <tim@timbenniks.nl>
+@package:   Zotonic 2009
+@Author:    Tim Benniks <tim@timbenniks.nl>
 
 Copyright 2009 Tim Benniks
 Copyright 2012 Arjan Scherpenisse
@@ -71,56 +71,56 @@ limitations under the License.
 
 
 window.zAdminConnectDone = function(v) {
-	if (v.is_new) {
-		var target_id = "links-"+v.subject_id+"-"+v.predicate;
-		var $elt = $("#"+target_id);
-		$elt.mask("", 10);
-		z_notify("update", {
-			z_delegate: "mod_admin",
-			z_target_id: target_id,
-			z_trigger_id: target_id,
-			id: v.subject_id,
-			predicate: v.predicate,
-			template: $elt.data("reload-template")
-		});
-	}
+    if (v.is_new) {
+        var target_id = "links-"+v.subject_id+"-"+v.predicate;
+        var $elt = $("#"+target_id);
+        $elt.mask("", 10);
+        z_notify("update", {
+            z_delegate: "mod_admin",
+            z_target_id: target_id,
+            z_trigger_id: target_id,
+            id: v.subject_id,
+            predicate: v.predicate,
+            template: $elt.data("reload-template")
+        });
+    }
 };
 
 window.zAdminLinkDone = function(v) {
-	window.z_zlink(v.url_language, v.title_language);
-	window.zAdminConnectDone(v);
+    window.z_zlink(v.url_language, v.title_language);
+    window.zAdminConnectDone(v);
 };
 
 window.zAdminMediaDone = function(v) {
-	window.z_choose_zmedia(v.object_id);
-	window.zAdminConnectDone(v);
+    window.z_choose_zmedia(v.object_id);
+    window.zAdminConnectDone(v);
 };
 
 window.zEditLanguage = function() {
-	return $('.language-tabs li.active').attr('lang');
+    return $('.language-tabs li.active').attr('lang');
 };
 
 function z_admin_ensure_block_names() {
     var names = [];
-    $('.blocks input.block-name').each(function() { 
-    	var name = $(this).val();
-    	if (name != '') {
-    		names.push(name);
-    	}
+    $('input.block-name').each(function() {
+        var name = $(this).val();
+        if (name !== '') {
+            names.push(name);
+        }
     });
 
-    $('.blocks input.block-name').each(function() { 
-    	var name = $(this).val();
-    	if (name == '')
-    	{
-    		var $block = $(this).closest(".block");
-    		name = $("input.block-type", $block).val().split("_")[0];
-    		var ct = 1;
-		    while (names.indexOf(name+ct) != -1) {
-		        ct++;
-		    }
-		    $("input.block-name", $block).val(name+ct);
-	        names.push(name+ct);
-    	}
+    $('input.block-name').each(function() {
+        var name = $(this).val();
+        if (name === '')
+        {
+            var $block = $(this).closest(".block");
+            name = $("input.block-type", $block).val().split("_").pop();
+            var ct = 1;
+            while (names.indexOf(name+ct) != -1) {
+                ct++;
+            }
+            $(this).val(name+ct);
+            names.push(name+ct);
+        }
     });
-};
+}
