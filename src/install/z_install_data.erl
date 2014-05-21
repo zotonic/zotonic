088573a6b9bd7a510938476d220c89d8bae7e6c9
Arjan Scherpenisse <arjan@scherpenisse.net>
2014-05-21 18:23:45 +0200

    core: Install mod_editor_tinymce by default in new sites

--- src/install/z_install_data.erl
+++ src/install/z_install_data.erl
@@ -86,6 +86,7 @@ get_skeleton_modules(empty) ->
 
      mod_authentication,
      mod_acl_adminonly,
+     mod_editor_tinymce,
 
      mod_admin,
      mod_admin_category,
@@ -113,6 +114,7 @@ get_skeleton_modules(blog) ->
 
      mod_authentication,
      mod_acl_adminonly,
+     mod_editor_tinymce,
 
      mod_admin,
      mod_admin_category,
