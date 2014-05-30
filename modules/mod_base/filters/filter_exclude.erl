48018551b3be4ec0783d5b6bbe08d04946e50959
Mawuli Adzaku <mawuli@mawuli.me>
2014-05-30 18:01:21 +0000

    mod_base: rename 'reject' filter to 'exclude'

--- modules/mod_base/filters/filter_exclude.erl
+++ modules/mod_base/filters/filter_exclude.erl
@@ -1,6 +1,6 @@
 %% @author Mawuli Adzaku <mawuli@mawuli.me>
 %% @copyright 2014 Mawuli Adzaku
-%% @doc 'filter_reject' filter_reject, filters a list to only display elements without a certain property
+%% @doc 'filter_exclude' filter_exclude, filters a list to only display elements without a certain property
 
 %% Copyright 2014 Mawuli Adzaku
 %%
@@ -16,24 +16,24 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(filter_reject).
+-module(filter_exclude).
 -export([
-	reject/3,
-	reject/4
+	exclude/3,
+	exclude/4
 	]).
 
 
-reject(_, undefined, _Context) -> [];
-reject(undefined, _, _Context) -> [];
-reject(In, Prop, Context) ->
+exclude(_, undefined, _Context) -> [];
+exclude(undefined, _, _Context) -> [];
+exclude(In, Prop, Context) ->
     lists:filter(fun(Elt) -> 
                          z_convert:to_bool(find_value(Prop, Elt, Context)) =:= false
                  end,
                  erlydtl_runtime:to_list(In, Context)).
 
-reject(_, undefined, _, _Context) -> [];
-reject(undefined, _, _, _Context) -> [];
-reject(In, Prop, Value, Context) ->
+exclude(_, undefined, _, _Context) -> [];
+exclude(undefined, _, _, _Context) -> [];
+exclude(In, Prop, Value, Context) ->
     lists:filter(fun(Elt) -> 
                          erlydtl_operators:ne(find_value(Prop, Elt, Context), Value, Context)
                  end,
