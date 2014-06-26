04e8bf78884b119af178e25447a174839adb6b0c
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2014-06-26 15:53:17 +0200

    core: fix for postback controller

--- modules/mod_base/controllers/controller_postback.erl
+++ modules/mod_base/controllers/controller_postback.erl
@@ -55,9 +55,11 @@ malformed_request(ReqData, Context) ->
 
 forbidden(ReqData, Context) ->
     Context1 = ?WM_REQ(ReqData, Context),
-    Context2 = z_context:continue_session(Context1),
-    z_context:lager_md(Context2),
-    ?WM_REPLY(not z_context:has_session(Context2), Context2).
+    %% Ensure all, but don't start a new session
+    Context2 = z_context:set(no_session, true, Context1),
+    Context3 = z_context:ensure_all(Context2),
+    z_context:lager_md(Context3),
+    ?WM_REPLY(not z_context:has_session(Context3), Context3).
 
 allowed_methods(ReqData, Context) ->
     {['POST'], ReqData, Context}.
