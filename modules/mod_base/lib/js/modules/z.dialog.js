05dea1a02314c80ddfcdc642146d4f71d4e099ea
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2014-10-28 16:22:09 +0100

    mod_base: Removed commented out code.

--- modules/mod_base/lib/js/modules/z.dialog.js
+++ modules/mod_base/lib/js/modules/z.dialog.js
@@ -26,9 +26,6 @@
         // center the dialog vertically again
         dialogReposition: function() {
             var $dialog = $("#zmodal");
-            if ($(window).width() >= 768) {
-                //$dialog.css({marginTop: -Math.floor($dialog.height()/2)+'px'});
-            }
         },
 
         dialogAdd: function(options) {
@@ -104,8 +101,6 @@
             if ($(window).width() >= 768) {
                 if ($dialog.height() > 0.8 * $(window).height()) {
                     $dialog.addClass('high');
-                } else {
-                    //$dialog.css({marginTop: -Math.floor($dialog.height()/2)+'px'});
                 }
             }
 
