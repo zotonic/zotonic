9d15254a8864e398bc3f129d311109e1b9090e9b
Marc Worrell <marc@worrell.nl>
2014-10-29 11:22:16 +0100

    mod_email_receive: when adding recipiens, catch references to non existing rsc

--- modules/mod_email_receive/models/m_email_receive_recipient.erl
+++ modules/mod_email_receive/models/m_email_receive_recipient.erl
@@ -91,11 +91,19 @@ insert(Notification, UserId, ResourceId, Context) ->
 		1 ->
 			insert(Notification, UserId, ResourceId, Context);
 		0 ->
-			1 = z_db:q("insert into email_receive_recipient (recipient, notification, user_id, rsc_id)
-						values ($1, $2, $3, $4)",
-					   [Recipient, Notification, UserId, ResourceId],
-					   Context),
-			{ok, Recipient}
+			try
+				1 = z_db:q("insert into email_receive_recipient (recipient, notification, user_id, rsc_id)
+							values ($1, $2, $3, $4)",
+						   [Recipient, Notification, UserId, ResourceId],
+						   Context),
+				{ok, Recipient}
+			catch
+				throw:{error, {error,error,<<"23503">>,_Msg,_Detail} = Error} ->
+					lager:warning(z_context:lager_md(Context),
+								  "[~p] Error on creating a new e-mail address ~p",
+								  [z_context:site(Context), Error]),
+					{error, notfound}
+			end
 	end.
 
 
