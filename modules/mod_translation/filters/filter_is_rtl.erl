0b0977475e81bc640d4fcd4b7ad321bb083df73c
Marc Worrell <marc@worrell.nl>
2014-12-18 16:28:53 +0100

    mod_translation: add more rtl languages.
    (cherry picked from commit aa5dcb585ebe5ed4f21c1bc6af746be898da0332)

--- modules/mod_translation/filters/filter_is_rtl.erl
+++ modules/mod_translation/filters/filter_is_rtl.erl
@@ -1,8 +1,11 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2011 Marc Worrell
+%% @copyright 2011-2014 Marc Worrell
 %% @doc Check if the given language is a rtl or ltr language
+%% From: http://en.wikipedia.org/wiki/Right-to-left
+%% Only iso639-1 codes are used here.
+%% Missing in this overview are: arc, bcc, bqi, ckb, glk, mzn, pnb
 
-%% Copyright 2011 Marc Worrell
+%% Copyright 2011-2014 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -27,13 +30,38 @@ is_rtl(Id, Context) when is_integer(Id) ->
     is_rtl(filter_language:language(Id, Context), Context);
 is_rtl([Lang|_] = Langs, Context) when is_atom(Lang) ->
     is_rtl(filter_language:language(Langs, Context), Context);
+
 is_rtl(ar, _Context) -> true;
+is_rtl(dv, _Context) -> true;
 is_rtl(fa, _Context) -> true;
 is_rtl(he, _Context) -> true;
+is_rtl(ku, _Context) -> true;
+is_rtl(ps, _Context) -> true;
+is_rtl(sd, _Context) -> true;
+is_rtl(ug, _Context) -> true;
+is_rtl(ur, _Context) -> true;
+is_rtl(yi, _Context) -> true;
+
 is_rtl("ar", _Context) -> true;
+is_rtl("dv", _Context) -> true;
 is_rtl("fa", _Context) -> true;
 is_rtl("he", _Context) -> true;
+is_rtl("ku", _Context) -> true;
+is_rtl("ps", _Context) -> true;
+is_rtl("sd", _Context) -> true;
+is_rtl("ug", _Context) -> true;
+is_rtl("ur", _Context) -> true;
+is_rtl("yi", _Context) -> true;
+
 is_rtl(<<"ar">>, _Context) -> true;
+is_rtl(<<"dv">>, _Context) -> true;
 is_rtl(<<"fa">>, _Context) -> true;
 is_rtl(<<"he">>, _Context) -> true;
+is_rtl(<<"ku">>, _Context) -> true;
+is_rtl(<<"ps">>, _Context) -> true;
+is_rtl(<<"sd">>, _Context) -> true;
+is_rtl(<<"ug">>, _Context) -> true;
+is_rtl(<<"ur">>, _Context) -> true;
+is_rtl(<<"yi">>, _Context) -> true;
+
 is_rtl(_, _Context) -> false.
