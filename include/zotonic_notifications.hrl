830eb8589d7e8bc900ef2d6c9506b7d64b5e8556
ghosthamlet <ghosthamlet@gmail.com>
2015-02-01 03:04:50 +0800

    Update zotonic_notifications.hrl

--- include/zotonic_notifications.hrl
+++ include/zotonic_notifications.hrl
@@ -352,6 +352,7 @@
 %% otherwise, return a #search_sql{} or #search_result{} record.
 -record(search_query, {search, offsetlimit}).
 
+-record(backend_rsc_select, {where_sql}).
 
 %% @doc An edge has been inserted. (notify)
 %% The predicate is an atom.
