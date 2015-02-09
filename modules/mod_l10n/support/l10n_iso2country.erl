d544f3fd5d3f6f49747217adb0e87e45444fd064
Marc Worrell <marc@worrell.nl>
2015-02-09 21:15:50 +0100

    mod_l10n: add utf-8 encoding hints to source file
    (cherry picked from commit 0047baada9202a831a977513111e7c155e8b4f82)

--- modules/mod_l10n/support/l10n_iso2country.erl
+++ modules/mod_l10n/support/l10n_iso2country.erl
@@ -1,3 +1,7 @@
+% -*- coding: utf-8; Mode: erlang; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+% ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8:
+%% coding: utf-8
+
 %% @author Marc Worrell <marc@worrell.nl>
 %% @doc Mapping of iso code to country name in english
 %% @copyright 2011 Marc Worrell
