9ca37297572b21b2932f8883aedf10f2f996d8ed
Marc Worrell <marc@worrell.nl>
2015-02-11 14:35:09 +0100

    mod_base: added the filter 'trans_filter_filled'
    (cherry picked from commit 4213383238ed076b3cd9609d9cfc512b04702ac9)

---
+++ modules/mod_base/filters/filter_trans_filter_filled.erl
@@ -0,0 +1,42 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2015 Marc Worrell
+%% @doc Filter a {trans, [...]} record, only pass the filled fields.
+
+%% Copyright 2015 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(filter_trans_filter_filled).
+
+-export([
+    trans_filter_filled/3
+]).
+
+trans_filter_filled(Var, Key, Context) ->
+    filled_in(erlydtl_runtime:find_value(Key, Var, Context)).
+
+filled_in({trans, Tr}) ->
+    Tr1 = lists:filter(fun({_,T}) -> is_filled_in(T) end, Tr),
+    {trans, Tr1};
+filled_in(A) ->
+    io:format("~p~n", [A]),
+    A.
+
+is_filled_in(<<>>) ->
+    false;
+is_filled_in(<<C, T/binary>>) when C =:= 32, C=:= 9 ->
+    is_filled_in(T);
+is_filled_in(<<"<p></p>">>) ->
+    false;
+is_filled_in(_) ->
+    true.
