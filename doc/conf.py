0f470cc74e325021385fe3c9e391cf1204330707
Marc Worrell <marc@worrell.nl>
2015-02-12 23:10:13 +0100

    docs: added xelatex target to generate PDF.
    This fixes a problem with utf8 characters and plain latex.

--- doc/conf.py
+++ doc/conf.py
@@ -198,13 +198,13 @@ htmlhelp_basename = 'Zotonicdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+ 'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+# 'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+# 'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
@@ -223,7 +223,7 @@ latex_documents = [
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
