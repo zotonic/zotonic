7160c959fbd44305ba3dbb4d67459f9ee25cfbd4
Marc Worrell <marc@worrell.nl>
2015-05-15 15:47:59 +0200

    mod_search: add cat_exact query argument.

--- include/zotonic.hrl
+++ include/zotonic.hrl
@@ -140,7 +140,8 @@
 %% Used for search results
 -record(search_result, {result=[], page=1, pagelen, total, all, pages, next, prev}).
 -record(m_search_result, {search_name, search_props, result, page, pagelen, total, pages, next, prev}).
--record(search_sql, {select, from, where="", order="", group_by="", limit, tables=[], args=[], cats=[], cats_exclude=[], run_func, extra=[], assoc=false}).
+-record(search_sql, {select, from, where="", order="", group_by="", limit, tables=[], args=[], 
+                     cats=[], cats_exclude=[], cats_exact=[], run_func, extra=[], assoc=false}).
 
 %% For z_supervisor, process definitions.
 -record(child_spec, {name, mfa, status, pid, crashes=5, period=60, 
