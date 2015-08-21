f739e959b63a9a699a77647c09574d5359830348
Marc Worrell <marc@worrell.nl>
2015-08-21 14:44:02 +0200

    core: force reload of client if page_id in z_msg is undefined.
    (cherry picked from commit 3d1f3090f777280466805d5ef8c77b2daf6ca0eb)

--- src/support/z_transport.erl
+++ src/support/z_transport.erl
@@ -171,6 +171,8 @@ incoming_msgs(L, Context) when is_list(L) ->
         end,
         {ok, [], Context},
         L);
+incoming_msgs(#z_msg_v1{page_id=undefined}, Context) ->
+    {ok, [msg(undefined, session, <<"page_invalid">>, [])], Context};
 incoming_msgs(#z_msg_v1{page_id=PageId, session_id=SessionId, data=Data, ua_class=UA, content_type=CT} = Msg, Context) ->
     Context1 = maybe_logon(maybe_set_sessions(SessionId, PageId, Context)),
     Context2 = maybe_set_uaclass(UA, Context1),
