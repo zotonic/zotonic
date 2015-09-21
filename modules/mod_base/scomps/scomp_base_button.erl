3730210e0770d6aaa659ed8409ed70ef0a95292f
Arthur Clemens <arthurclemens@gmail.com>
2015-09-21 16:04:06 +0200

    mod_base: safer defaults
    Default type: “button”
    Default class: “btn btn-default”

--- modules/mod_base/scomps/scomp_base_button.erl
+++ modules/mod_base/scomps/scomp_base_button.erl
@@ -34,12 +34,17 @@ render(Params, _Vars, Context) ->
     Icon      = proplists:get_all_values(icon, Params),
     Style     = proplists:get_value(style, Params),
     TabIndex  = proplists:get_value(tabindex, Params),
-    Type      = proplists:get_value(type, Params),
+    Type      = proplists:get_value(type, Params, "button"),
     Title     = proplists:get_value(title, Params),
     Disabled  = proplists:get_value(disabled, Params, false),
     Actions   = proplists:get_all_values(action, Params),
     Tag       = proplists:get_value(tag, Params, <<"button">>),
 
+    Class1 = case Class of
+        [] -> "btn btn-default";
+        _ -> Class
+    end,
+    
     Options   = [{action,X} || X <- Actions],
     Options1  = case Postback of
                 	undefined -> Options;
@@ -65,9 +70,9 @@ render(Params, _Vars, Context) ->
         {<<"tabindex">>, TabIndex}
     ],
     
-    {Class1, Attrs1} = case z_convert:to_bool(Disabled) of
-        false -> {Class, Attrs};
-        true -> { ["disabled"|Class], [ {<<"disabled">>,"disabled"}|Attrs] }
+    {Class2, Attrs1} = case z_convert:to_bool(Disabled) of
+        false -> {Class1, Attrs};
+        true -> { ["disabled"|Class1], [ {<<"disabled">>,"disabled"}|Attrs] }
     end,
     
     Attrs2 = case Type of
@@ -83,7 +88,7 @@ render(Params, _Vars, Context) ->
             end,
     Context2 = z_tags:render_tag(
                         Tag,
-                        [{<<"class">>,Class1}|Attrs2],
+                        [{<<"class">>,Class2}|Attrs2],
                     	Text1,
                     	Context1),
     {ok, Context2}.
