6c10e8754e6efed5d200f8f36fb5607f17c8269d
Marc Worrell <marc@worrell.nl>
2015-11-06 11:49:15 +0100

    mod_base: add convenience arg to lazy scomp. If 'template' argument is defined then all arguments are wrapped in an update action.
    (cherry picked from commit a953d52529594da600aa90199980385845279b80)

--- modules/mod_base/scomps/scomp_base_lazy.erl
+++ modules/mod_base/scomps/scomp_base_lazy.erl
@@ -26,23 +26,40 @@
 vary(_Params, _Context) -> nocache.
 
 render(Params, Vars, Context) ->
-    Id = case proplists:get_value(id, Params) of
-             undefined -> z_ids:identifier();
-             FixedId -> FixedId
-         end,
-    Class  = proplists:get_value(class, Params, "z-lazy"),
-    Image = proplists:get_value(image, Params, <<"/lib/images/spinner.gif">>),
-    Params1 = [{id, Id}, {type, "visible"} | proplists:delete(id, Params)],
-    Params2 = [ ensure_moreresults_visible(Param) || Param <- Params1 ],
-    Html = [<<"<div id='">>, Id, <<"' class='">>, Class, <<"'><img src='">>, Image, <<"' alt='' /></div>">>],
-    case scomp_base_wire:render(Params2, Vars, Context) of
-        {ok, Result} -> {ok, [Html, z_context:prune_for_template(Result)]};
-        {error, _Reason} = Error -> Error
+    case proplists:get_value(template, Params) of
+        undefined ->
+            Id = case proplists:get_value(id, Params) of
+                     undefined -> z_ids:identifier();
+                     FixedId -> FixedId
+                 end,
+            Class  = proplists:get_value(class, Params, "z-lazy"),
+            Image = proplists:get_value(image, Params, <<"/lib/images/spinner.gif">>),
+            Params1 = [{id, Id}, {type, "visible"} | proplists:delete(id, Params)],
+            Params2 = [ ensure_moreresults_visible(Param) || Param <- Params1 ],
+            Html = [<<"<div id='">>, Id, <<"' class='">>, Class, <<"'><img src='">>, Image, <<"' alt='' /></div>">>],
+            case scomp_base_wire:render(Params2, Vars, Context) of
+                {ok, Result} -> {ok, [Html, z_context:prune_for_template(Result)]};
+                {error, _Reason} = Error -> Error
+            end;
+        _Template ->
+            {TargetId, Html} = case proplists:get_value(target, Params) of
+                undefined ->
+                    Target = z_ids:identifier(),
+                    Div = [<<"<div id='">>, Target, <<"' class='z-lazy'><img src='/lib/images/spinner.gif' alt='' /></div>">>],
+                    {Target, Div};
+                Target ->
+                    {Target, <<>>}
+            end,
+            Action = {update, [{target, TargetId}|Params]},
+            case scomp_base_wire:render([{id, TargetId}, {type, "visible"}, {action, Action}], Vars, Context) of
+                {ok, Result} -> {ok, [Html, z_context:prune_for_template(Result)]};
+                {error, _Reason} = Error -> Error
+            end
     end.
 
 %% @doc Convenience function: the action "moreresults" is often used with the lazy scomp.
 %%      A mistake that happens is to not add the 'visible' argument, which stops the
-%%      moreresults from rearming after the first time it is triggered.
+%%      moreresults from re-arming after the first time it is triggered.
 ensure_moreresults_visible({action, {moreresults, _} = Action}) ->
     {action, ensure_moreresults_visible_1(Action)};
 ensure_moreresults_visible({action, Actions}) when is_list(Actions) ->
