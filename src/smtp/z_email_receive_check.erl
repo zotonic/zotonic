ed74764fdc02cc5137219590939847225d607578
Marc Worrell <marc@worrell.nl>
2015-11-23 17:56:16 +0100

    smtp: mark emails from the mailer-daemon as automatic emails.
    Novell Groupwise out ot office messages don't have any special headers, the only way to recognize
    an out of office message is by the envelop from, which is set to Mailer-Daemon@...
    
    (cherry picked from commit fd6b496fa42384930b8d22555e55a68766328de1)

--- src/smtp/z_email_receive_check.erl
+++ src/smtp/z_email_receive_check.erl
@@ -48,13 +48,14 @@ is_bulk_precedence({_, S}) ->
 %% @doc Check if an e-mail message is an automatic reply or another kind of message.
 %%      This uses heuristics on the precedence and the subject of the e-mail.
 -spec is_auto(#email_received{}) -> boolean().
-is_auto(#email_received{headers=Headers, email=Email}) ->
+is_auto(#email_received{headers=Headers, email=Email, from=EnvelopFrom}) ->
            lists:keymember(<<"x-auto-response-suppress">>, 1, Headers)
     orelse lists:keymember(<<"x-autorespond">>, 1, Headers)
     orelse is_auto_precedence(lists:keyfind(<<"x-precedence">>, 1, Headers))
     orelse is_auto_precedence(lists:keyfind(<<"precedence">>, 1, Headers))
     orelse is_auto_submitted(lists:keyfind(<<"auto-submitted">>, 1, Headers))
-    orelse is_auto_subject(Email#email.subject).
+    orelse is_auto_subject(Email#email.subject)
+    orelse is_auto_from(EnvelopFrom).
 
 is_auto_precedence(false) -> 
     false;
@@ -66,12 +67,18 @@ is_auto_submitted(false) ->
 is_auto_submitted({_, S}) -> 
     z_convert:to_binary(z_string:to_lower(S)) =/= <<"no">>.
 
+is_auto_from(undefined) -> false;
+is_auto_from(EnvelopFrom) -> is_auto_from_1(z_convert:to_binary(z_string:to_lower(EnvelopFrom))).
+
+is_auto_from_1(<<"mailer-daemon@", _/binary>>) -> true;
+is_auto_from_1(_) -> false.
+
 is_auto_subject(undefined) -> false;
 is_auto_subject(Subject) -> is_auto_subject_1(z_convert:to_binary(z_string:to_lower(Subject))).
 
 -ifdef(coding_utf8).
 
-is_auto_subject_1(<<"auto:"/utf8>>) -> true;
+is_auto_subject_1(<<"auto:"/utf8, _/binary>>) -> true;
 is_auto_subject_1(<<"automatic reply"/utf8>>) -> true;
 is_auto_subject_1(<<"autosvar"/utf8>>) -> true;
 is_auto_subject_1(<<"automatisk svar"/utf8>>) -> true;
@@ -88,7 +95,7 @@ is_auto_subject_1(_) -> false.
 
 -else.
 
-is_auto_subject_1(<<"auto:">>) -> true;
+is_auto_subject_1(<<"auto:", _/binary>>) -> true;
 is_auto_subject_1(<<"automatic reply">>) -> true;
 is_auto_subject_1(<<"autosvar">>) -> true;
 is_auto_subject_1(<<"automatisk svar">>) -> true;
