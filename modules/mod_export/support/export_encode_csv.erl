3897f346dacf37245aa0f65aadeffd7afe4692cc
Marc Worrell <marc@worrell.nl>
2016-01-28 15:32:00 +0100

    mod_export: suppress ST_JUTTEMIS and year 9999 dates in the output.

--- modules/mod_export/support/export_encode_csv.erl
+++ modules/mod_export/support/export_encode_csv.erl
@@ -23,6 +23,7 @@
 	encode/2
 	]).
 
+-include_lib("zotonic.hrl").
 
 encode([], _Context) ->
 	<<"\r\n">>;
@@ -51,6 +52,12 @@ encode_value(B, _Context) when is_binary(B) ->
 encode_value({Y,M,D} = Date, Context)
 	when is_integer(Y), is_integer(M), is_integer(D) ->
 	quote(erlydtl_dateformat:format_utc({Date, {0,0,0}}, "Y-m-d", Context));
+encode_value(?ST_JUTTEMIS, _Context) ->
+	<<>>;
+encode_value({{9999,M,D}, {H,I,S}}, _Context) 
+	when is_integer(M), is_integer(D),
+		 is_integer(H), is_integer(I), is_integer(S) ->
+	<<>>;
 encode_value({{Y,M,D}, {H,I,S}} = Date, Context) 
 	when is_integer(Y), is_integer(M), is_integer(D),
 		 is_integer(H), is_integer(I), is_integer(S) ->
