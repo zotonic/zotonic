3f52a34dd47c1fb98109072d8557387cf50ac422
Marc Worrell <marc@worrell.nl>
2016-02-09 10:40:53 +0100

    core: add more information and better docs for the m.req model.
    (cherry picked from commit 5f106ada664dd8070e3c20fe9a3026e1c40db536)

--- src/models/m_req.erl
+++ src/models/m_req.erl
@@ -43,43 +43,47 @@ m_find_value(Key, #m{value=undefined}, Context) ->
 %% @doc Transform a m_config value to a list, used for template loops
 %% @spec m_to_list(Source, Context) -> List
 m_to_list(#m{value=undefined}, Context) ->
-    values(z_context:get_reqdata(Context)).
+    values(Context).
 
 %% @doc Transform a model value so that it can be formatted or piped through filters
 %% @spec m_value(Source, Context) -> term()
 m_value(#m{value=undefined}, Context) ->
-    values(z_context:get_reqdata(Context)).
+    values(Context).
 
 
 %% @doc Fetch the field from the wrq interface.
-get(ua_class, #context{} = Context) ->
-    z_user_agent:get_class(Context);
-get(ua_props, #context{} = Context) ->
-    z_user_agent:get_props(Context);
+-spec get(atom(), #context{} | #wm_reqdata{} | undefined) -> any(). 
+get(undefined, _) -> undefined;
+get(_, undefined) -> undefined;
+get(ua_class, #context{} = Context) -> z_user_agent:get_class(Context);
+get(ua_props, #context{} = Context) -> z_user_agent:get_props(Context);
+get(timezone, #context{} = Context) -> z_context:tz(Context);
+get(language, #context{} = Context) -> z_context:language(Context);
+get(What, #context{} = Context) -> get_req(What, z_context:get_reqdata(Context));
+get(What, #wm_reqdata{} = RD) -> get_req(What, RD).
 
-get(timezone, #context{} = Context) ->
-    z_context:tz(Context);
-
-get(What, #context{} = Context) -> get(What, z_context:get_reqdata(Context));
-get(_What, undefined) -> undefined;
-get(method, RD) -> wrq:method(RD);
-get(version, RD) -> wrq:version(RD);
-get(peer, RD) -> wrq:peer(RD);
-get(is_ssl, RD) -> wrq:is_ssl(RD);
-get(host, RD) -> wrq:get_req_header_lc("host", RD);
-get(raw_path, RD) -> wrq:raw_path(RD);
-get(path, RD) -> wrq:path(RD);
-get(qs, RD) -> wrq:req_qs(RD);
-get(headers, RD) -> wrq:req_headers(RD);
-get(user_agent, RD) -> wrq:get_req_header_lc("user-agent", RD);
-get(req_id, #wm_reqdata{log_data=#wm_log_data{req_id=ReqId}}) -> ReqId;
-
-get(_Key, _RD) -> undefined.
+get_req(_What, undefined) -> undefined;
+get_req(method, RD) -> wrq:method(RD);
+get_req(version, RD) -> wrq:version(RD);
+get_req(peer, RD) -> wrq:peer(RD);
+get_req(is_ssl, RD) -> wrq:is_ssl(RD);
+get_req(host, RD) -> wrq:get_req_header_lc("host", RD);
+get_req(raw_path, RD) -> wrq:raw_path(RD);
+get_req(path, RD) -> wrq:path(RD);
+get_req(qs, RD) -> wrq:req_qs(RD);
+get_req(headers, RD) -> wrq:req_headers(RD);
+get_req(user_agent, RD) -> wrq:get_req_header_lc("user-agent", RD);
+get_req(referer, RD) -> wrq:get_req_header_lc("referer", RD);
+get_req(referrer, RD) -> wrq:get_req_header_lc("referer", RD);
+get_req(req_id, #wm_reqdata{log_data=#wm_log_data{req_id=ReqId}}) -> ReqId;
+get_req(_Key, _RD) -> undefined.
 
 
-values(RD) ->
-    [ {K, get(K,RD)} || K <- [
-            method, version, peer, is_ssl, host, raw_path, path, qs, headers
+-spec values(#context{}) -> list({atom(), any()}).
+values(Context) ->
+    [ {K, get(K, Context)} || K <- [
+            method, version, peer, is_ssl, host, raw_path, path, qs, referrer, user_agent, req_id, headers,
+            ua_class, ua_props, timezone, language
         ]
     ].
 
