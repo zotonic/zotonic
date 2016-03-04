ff201364ba6262034683838de58d1eb17dc32f7c
Marc Worrell <marc@worrell.nl>
2016-03-04 14:14:43 +0100

    mod_base: add 'is_number' filter.
    (cherry picked from commit 5ada76c081fcb2062ede6a8dadcf545ea8e30588)

---
+++ modules/mod_base/filters/filter_is_number.erl
@@ -0,0 +1,25 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2016 Marc Worrell
+%% @doc is_number filter, test if a value is a number
+
+%% Copyright 2016 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(filter_is_number).
+-export([is_number/2]).
+
+is_number(V, _Context) when is_integer(V) -> true;
+is_number(V, _Context) when is_float(V) -> true;
+is_number(_, _Context) -> false.
+
