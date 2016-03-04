6783aa60c0d821e7679c86daadb3d71dbdd4b203
Marc Worrell <marc@worrell.nl>
2016-03-04 14:25:47 +0100

    mod_backup: move the list of known resource properties to m_rsc.
    (cherry picked from commit 3ce3506be14a54e0cdb92f5cce79f5f980b608a7)
    
    Conflicts:
    	modules/mod_backup/support/backup_format.erl
    	src/models/m_rsc.erl

--- src/models/m_rsc.erl
+++ src/models/m_rsc.erl
@@ -69,7 +69,9 @@
 
     name_lookup/2,
     uri_lookup/2,
-    ensure_name/2
+    ensure_name/2,
+
+    common_properties/1
 ]).
 
 -export_type([resource/0, resource_id/0, resource_name/0]).
@@ -814,7 +816,7 @@ predicates_edit(Id, Context) ->
     ByCategory = m_predicate:for_subject(Id, Context),
     Present = m_edge:object_predicate_ids(Id, Context),
     ByCategory ++ Present.
-    
+   
 
 %% @doc Ensure that a resource has a name, caller must have update rights.
 -spec ensure_name(integer(), #context{}) -> ok.
@@ -859,3 +861,77 @@ ensure_name_unique(BaseName, N, Context) ->
 
 postfix(0) -> <<>>;
 postfix(N) -> integer_to_list(N).
+
+
+%% @doc Common properties, these are used by exporter and backup routines.
+common_properties(_Context) ->
+    [
+        title,
+
+        category_id, 
+        creator_id,
+        modifier_id,
+
+        created,
+        modified,
+
+        publication_start,
+        publication_end,
+
+        is_published,
+        is_featured,
+        is_protected,
+
+        chapeau,
+        subtitle,
+        short_title,
+        summary,
+
+        name_prefix,
+        name_first,
+        name_surname_prefix,
+        name_surname,
+
+        phone,
+        phone_mobile,
+        phone_alt,
+        phone_emergency,
+
+        email,
+        website,
+
+        date_start,
+        date_end,
+        date_remarks,
+
+        address_street_1,
+        address_street_2,
+        address_city,
+        address_state,
+        address_postcode,
+        address_country,
+
+        mail_street_1,
+        mail_street_2,
+        mail_city,
+        mail_state,
+        mail_postcode,
+        mail_country,
+
+        location_lng,
+        location_lat,
+
+        body,
+        body_extra,
+        blocks,
+
+        page_path,
+        name,
+
+        seo_noindex,
+        seo_title,
+        slug,
+        custom_slug,
+        seo_keywords,
+        seo_desc
+    ].  
