f915387085c0b0861b9688df6d67df6afa9ad554
Marc Worrell <marc@worrell.nl>
2016-03-15 15:01:46 +0100

    mod_editor_tinymce: remove version 4.0.26 and 4.1.6
    (cherry picked from commit 7303a583208e3c4b54537d8adc1540b3344393d7)

--- modules/mod_editor_tinymce/lib/js/tinymce-4.0.26/tinymce/plugins/zlink/langs/nl.js
+++
@@ -1,4 +0,0 @@
-tinymce.addI18n('nl',{
-	'Insert an internal link': 'Voeg een interne link in',
-	'Insert internal link': 'Voeg interne link in'
-});
--- modules/mod_editor_tinymce/lib/js/tinymce-4.1.6/tinymce/plugins/codemirror/langs/fr_FR.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('fr_FR',{
-	'HTML source code': 'Code source HTML',
-	'Start search': 'Rechercher',
-	'Find next': 'Chercher suiv.',
-	'Find previous': 'Chercher préc.',
-	'Replace': 'Replace',
-	'Replace all': 'Replace all'
-});
