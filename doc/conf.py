21cbe8221400edb3c75c840bb6f4c4b0fe37ffac
David de Boer <david@ddeboer.nl>
2016-04-04 12:28:28 +0200

    doc: Add Sphinx extlinks extension

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.extlinks'
+]
 
 todo_include_todos = True
 
