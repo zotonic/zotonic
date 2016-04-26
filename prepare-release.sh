84ffaeff60856e09d3966c9fbc13e15354821d8d
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-04-26 15:19:21 +0200

    scripts: Fix prepare-release.sh
    add usage; also change minor version in docs/conf.py
    
    (cherry picked from commit f279af9d58a6a4b25cefe8823ba88996066a1bea)

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if [ "$1" = "" ]; then
+    echo "Usage example: $0 0.14.0"
+    exit 1
+fi
+
+VERSION="$1"
+MINOR=$(echo $VERSION | sed -e 's/.[^.]*$//')
+
 # Increments version numbers where needed
 # Usage: ./prepare-release.sh 0.13.7
 
-sed -i '' "s/release = .*/release = '$1'/" doc/conf.py
-sed -i '' "s/{vsn, .*/{vsn, \"$1\"},/" src/zotonic.app.src
-sed -i '' "s/ZOTONIC_VERSION, .*)./ZOTONIC_VERSION, \"$1\")./" include/zotonic_release.hrl
+sed -e "s/^version = .*/version = '$MINOR'/" -i doc/conf.py
+sed -e "s/^release = .*/release = '$VERSION'/" -i doc/conf.py
+sed -e "s/{vsn, .*/{vsn, \"$VERSION\"},/" -i src/zotonic.app.src
+sed -e "s/ZOTONIC_VERSION, .*)./ZOTONIC_VERSION, \"$VERSION\")./" -i include/zotonic_release.hrl
+
+git status
