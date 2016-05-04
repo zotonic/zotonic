0128ca15688539add64882b7b8d123a5f59b88a3
Marc Worrell <marc@worrell.nl>
2016-05-04 12:50:26 +0200

    core: add sanitization on the contents of uploaded SVG files. Issue #1265

--- src/tests/z_sanitize_test.erl
+++ src/tests/z_sanitize_test.erl
@@ -14,3 +14,33 @@ youtube_object_test() ->
 	Out = <<"<iframe width=\"560\" height=\"315\" allowfullscreen=\"1\" frameborder=\"0\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\"></iframe>">>,
 	?assertEqual(Out, z_sanitize:html(In, Context)).
 
+
+svg_imagetragick_test() ->
+    ?assertEqual(false, z_media_sanitize:is_acceptable_svg(<<"
+<?xml version=\"1.0\" standalone=\"no\"?>
+<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">
+<svg width=\"800\" height=\"600\"
+    xmlns:svg=\"http://www.w3.org/2000/svg\" version=\"1.2\"
+    xmlns:xlink=\"http://www.w3.org/1999/xlink\">
+    <image width=\"800\" height=\"600\" xlink:href=\"https://upload.wikimedia.org/wikipedia/commons/c/ca/Triple-Spiral-4turns_green_transparent.png\"></image>
+</svg>
+">>)),
+    ?assertEqual(false, z_media_sanitize:is_acceptable_svg(<<"
+<?xml version=\"1.0\" standalone=\"no\"?>
+<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">
+<svg width=\"800\" height=\"600\"
+    xmlns:svg=\"http://www.w3.org/2000/svg\" version=\"1.2\"
+    xmlns:xlink=\"http://www.w3.org/1999/xlink\">
+    <image width=\"800\" height=\"600\" clip-path=\"url(http://example.com)\"</image>
+</svg>
+">>)),
+    ?assertEqual(true, z_media_sanitize:is_acceptable_svg(<<"
+<?xml version=\"1.0\" standalone=\"no\"?>
+<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">
+<svg width=\"800\" height=\"600\"
+    xmlns:svg=\"http://www.w3.org/2000/svg\" version=\"1.2\"
+    xmlns:xlink=\"http://www.w3.org/1999/xlink\">
+    <image width=\"800\" height=\"600\" clip-path=\"url(#foobar)\"</image>
+</svg>
+">>)),
+    ok.
