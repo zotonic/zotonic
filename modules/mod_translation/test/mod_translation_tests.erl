d334cf7844ad5c928e79f2d39aa2337525a9fe08
Arthur Clemens <arthurclemens@gmail.com>
2016-08-16 22:08:00 +0200

    Split z_language tests

--- modules/mod_translation/test/mod_translation_tests.erl
+++ modules/mod_translation/test/mod_translation_tests.erl
@@ -27,26 +27,3 @@ test_url_strip_language3() ->
     Expected = "/admin/translation",
     ?assertEqual(Expected, Result).
 
-filter_is_rtl_test() ->
-    Context = z_context:new(testsandbox),
-    test_filter_is_rtl1(Context),
-    test_filter_is_rtl2(Context),
-    test_filter_is_rtl3(Context).
-
-test_filter_is_rtl1(Context) ->
-    Code = "en",
-    Result = filter_is_rtl:is_rtl(Code, Context),
-    Expected = false,
-    ?assertEqual(Expected, Result).
-
-test_filter_is_rtl2(Context) ->
-    Code = <<"ar">>,
-    Result = filter_is_rtl:is_rtl(Code, Context),
-    Expected = true,
-    ?assertEqual(Expected, Result).
-
-test_filter_is_rtl3(Context) ->
-    Code = <<"ar-ae">>,
-    Result = filter_is_rtl:is_rtl(Code, Context),
-    Expected = true,
-    ?assertEqual(Expected, Result).
