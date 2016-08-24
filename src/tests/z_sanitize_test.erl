d8fb68d17a93b4d641cebea5faf038f7b64115ab
David de Boer <david@ddeboer.nl>
2016-08-24 18:30:56 +0200

    Try to run sanitize tests against testsandbox

--- src/tests/z_sanitize_test.erl
+++ src/tests/z_sanitize_test.erl
@@ -3,13 +3,13 @@
 -include_lib("eunit/include/eunit.hrl").
 
 youtube_test() ->
-	Context = z_context:new(zotonic_status),
+	Context = z_context:new(testsandbox),
 	In  = <<"<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/2RXp3r2gb3A\" frameborder=\"0\" allowfullscreen></iframe>">>,
 	Out = <<"<iframe src=\"//www.youtube.com/embed/2RXp3r2gb3A\" width=\"560\" height=\"315\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe>">>,
 	?assertEqual(Out, z_sanitize:html(In, Context)).
 
 youtube_object_test() ->
-	Context = z_context:new(zotonic_status),
+	Context = z_context:new(testsandbox),
 	In  = <<"<object data=\"http://www.youtube.com/embed/dQw4w9WgXcQ\" width=\"560\" height=\"315\"></object>">>,
 	Out = <<"<iframe width=\"560\" height=\"315\" allowfullscreen=\"1\" frameborder=\"0\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\"></iframe>">>,
 	?assertEqual(Out, z_sanitize:html(In, Context)).
