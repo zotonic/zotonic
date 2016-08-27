9bb448d6af44eec126603f9a50c231b553b757d0
David de Boer <david@ddeboer.nl>
2016-08-27 10:47:15 +0200

    Don't start mod_admin for testsandbox as it requires authentication

--- priv/sites/testsandbox/tests/testsandbox_basic_tests.erl
+++ priv/sites/testsandbox/tests/testsandbox_basic_tests.erl
@@ -12,7 +12,7 @@ testsandbox_enabled_test() ->
 default_modules_test() ->
     C = z_context:new(testsandbox),
     ?assertEqual(z_module_manager:active(mod_base, C), true),
-    ?assertEqual(z_module_manager:active(mod_admin, C), true),
+    ?assertEqual(z_module_manager:active(mod_mqtt, C), true),
     ok.
 
 %% Test if the webserver is running, by looking at the home tpl.
