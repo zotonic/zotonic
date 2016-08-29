716d6ab4c00ff6b1ae0ed75882e232f0a5659f6f
David de Boer <david@ddeboer.nl>
2016-08-29 21:29:59 +0200

    tests: Include db_tests in testrun (#1393)
    * Make sure all tests are run with runtests, including db_tests.
    * Fix previously excluded tests that now failed.
    * Start minimum number of modules on testsandbox and remove mod_acl_adminonly
       from modules list.

--- src/tests/z_acl_tests.erl
+++ src/tests/z_acl_tests.erl
@@ -1,8 +1,7 @@
 -module(z_acl_tests).
--author("david").
 
 -include_lib("eunit/include/eunit.hrl").
 
 name_for_object_test() ->
-    Context = z_context:new(testsandboxdb),
+    Context = z_context:new(testsandbox),
     false = z_acl:is_allowed(update, administrator, Context).
