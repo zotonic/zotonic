1526ec34f44d97ee91a0dc5c0cfdd67a893a5a0d
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-09-23 14:57:10 +0200

    doc: Tweak Sphinx RTD theme

--- doc/conf.py
+++ doc/conf.py
@@ -107,6 +107,13 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme_options = {
+    'collapse_navigation': True,
+    'display_version': True,
+    'logo_only': True
+}
+
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
