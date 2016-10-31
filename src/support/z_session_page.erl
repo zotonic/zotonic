3ce1b8afdda82f4e25fbde05dbc406bbdc1ccc14
Marc Worrell <marc@worrell.nl>
2016-10-31 17:08:12 +0100

    core: remove transport log messages about unknown page sessions.

--- src/support/z_session_page.erl
+++ src/support/z_session_page.erl
@@ -180,19 +180,21 @@ comet_detach(Pid) ->
     z_utils:flush_message(script_queued).
 
 %% @doc Attach the websocket request process to the page session, enabling sending scripts to the user agent
+-spec websocket_attach(pid(), binary()|undefined, #context{}) -> ok.
 websocket_attach(WsPid, PageId, Context) when is_binary(PageId) ->
     websocket_attach(WsPid, whereis(PageId, Context));
-websocket_attach(_WsPid, undefined, Context) ->
-    lager:info(z_context:lager_md(Context),
-               "Websocket attach to non-existing page ~p", [Context#context.page_id]).
+websocket_attach(_WsPid, undefined, _Context) ->
+    % No page id, ignore attach.
+    ok.
 
-websocket_attach(WsPid, #context{page_pid=Pid}) when is_pid(Pid) ->
+-spec websocket_attach(pid(), #context{}|pid()|undefined) -> ok.
+websocket_attach(WsPid, #context{page_pid=Pid}) ->
     websocket_attach(WsPid, Pid);
 websocket_attach(WsPid, Pid) when is_pid(Pid) ->
     gen_server:cast(Pid, {websocket_attach, WsPid});
-websocket_attach(_WsPid, #context{} = Context) ->
-    lager:info(z_context:lager_md(Context),
-               "Websocket attach to non-existing page ~p", [Context#context.page_id]).
+websocket_attach(_WsPid, undefined) ->
+    % No page process, ignore attach
+    ok.
 
 %% @doc Called by the comet process or the page request to fetch any queued transport messages
 get_transport_data(Pid) when is_pid(Pid) ->
