zotonic/zotonic

commit b835ee708e9b346c7833fde07179208f2e051fc3

admin: Fix access checks (#1514)
* admin: Fix access checks

* admin: Fix access checks

* In dispatch rules and admin menu items, only check module use
  permissions.
* On category and predicate admin screens, check for insert, update and
   delete permissions to decide which buttons to show.
* Add ACL checks where there weren’t any.

modules/mod_admin_category/templates/_admin_category_sorter.tpl
@@ -1,9 +1,9 @@
-{% with m.acl.is_admin as editable %}
+{% with m.acl.is_allowed.update.category as editable %}
 
 <ul id="category" class="tree-list categories {% if editable %}do_menuedit{% endif %}">
     {% for mid, path, action in m.category.menu|menu_flat %}
     {% with forloop.counter as c %}
-        {% include "_menu_edit_item.tpl" c=forloop.counter id=mid %}
+        {% include "_menu_edit_item.tpl" c=forloop.counter id=mid editable=editable %}
     {% endwith %}
     {% endfor %}
 </ul>

modules/mod_menu/templates/_menu_edit_item.tpl
@@ -10,21 +10,23 @@
 
 		<i class="warning glyphicon glyphicon-eye-close" {% if id.is_published %}style="display: none"{% endif %}></i>
 
-	    <span class="btns">
-		    <span class="btn-group">
-		        <a href="#" class="btn btn-default btn-xs menu-edit">{_ Edit _}</a>
+        {% if editable %}
+            <span class="btns">
+                <span class="btn-group">
+                    <a href="#" class="btn btn-default btn-xs menu-edit">{_ Edit _}</a>
 
-		        <a href="#" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-cog"></i> <span class="caret"></span></a>
-				<ul class="dropdown-menu dropdown-menu-right">
-				    <li><a href="#" data-where="before">&uarr; {_ Add before _}</a></li>
-				    <li><a href="#" data-where="below">&rarr; {_ Add below _}</a></li>
-				    <li><a href="#" data-where="after">&darr; {_ Add after _}</a></li>
-				    <li class="divider"></li>
-				    <li><a href="#" data-where="copy">{_ Copy _}</a></li>
-				    <li><a href="#" data-where="remove">{_ Remove _}</a></li>
-				</ul>
-		    </span>
-		  </span>
+                    <a href="#" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-cog"></i> <span class="caret"></span></a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="#" data-where="before">&uarr; {_ Add before _}</a></li>
+                        <li><a href="#" data-where="below">&rarr; {_ Add below _}</a></li>
+                        <li><a href="#" data-where="after">&darr; {_ Add after _}</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#" data-where="copy">{_ Copy _}</a></li>
+                        <li><a href="#" data-where="remove">{_ Remove _}</a></li>
+                    </ul>
+                </span>
+            </span>
+        {% endif %}
 	</div>
 
 	{% if action == `down` %}

modules/mod_admin_category/mod_admin_category.erl
@@ -184,7 +184,7 @@ observe_admin_menu(admin_menu, Acc, Context) ->
                 parent=admin_structure,
                 label=?__("Categories", Context),
                 url={admin_category_sorter},
-                visiblecheck={acl, insert, category}}
+                visiblecheck={acl, use, mod_admin_category}}
 
      |Acc].
 

modules/mod_admin_category/templates/admin_category_sorter.tpl
@@ -3,7 +3,6 @@
 {% block title %}{_ Category Hierarchy _}{% endblock %}
 
 {% block content %}
-{% with m.acl.is_admin as editable %}
 <div class="admin-header">
     <h2>{_ Categories _}</h2>
 
@@ -29,5 +28,4 @@
     </div>
 </div>
 </div>
-{% endwith %}
 {% endblock %}

modules/mod_admin_predicate/dispatch/dispatch
@@ -1,7 +1,7 @@
 %% -*- mode: erlang -*-
 %% Dispatch rule for a predicate list, edit and connections overview
 [
- {admin_predicate,      ["admin", "predicate"],     controller_admin,                [{template, "admin_predicate.tpl"}, {selected, "predicate"}]},
- {admin_predicate_edit, ["admin", "predicate", id], controller_admin_predicate_edit, []},
+ {admin_predicate,      ["admin", "predicate"],     controller_admin,                [{acl_module, mod_admin_predicate}, {template, "admin_predicate.tpl"}, {selected, "predicate"}]},
+ {admin_predicate_edit, ["admin", "predicate", id], controller_admin_predicate_edit, [{acl_module, mod_admin_predicate}]},
  {admin_edges,  		["admin", "connections"],   controller_admin,                [{template, "admin_edges.tpl"}, {selected, "edges"}]}
 ].

modules/mod_admin_predicate/mod_admin_predicate.erl
@@ -187,7 +187,7 @@ observe_admin_menu(admin_menu, Acc, Context) ->
                 parent=admin_structure,
                 label=?__("Predicates", Context),
                 url={admin_predicate},
-                visiblecheck={acl, insert, predicate}},
+                visiblecheck={acl, use, mod_admin_predicate}},
      #menu_item{id=admin_edges,
                 parent=admin_content,
                 label=?__("Page connections", Context),

modules/mod_admin_predicate/templates/admin_predicate.tpl
@@ -12,7 +12,7 @@
     The relation is always directed, from the subject to the object.<br/>Predicates are defined in ontologies like <a href="http://sioc-project.org/">SIOC</a>.  On this page you can define the predicates known to Zotonic. _}</p>
 </div>
 
-{% if editable %}
+{% if m.acl.insert.predicate %}
 <div class="well">
     {% button class="btn btn-primary" text=_"Make a new predicate" action={dialog_predicate_new title=""} %}
 </div>
@@ -37,8 +37,12 @@
                 <td>{{ p.uri|default:"&nbsp;" }}</td>
                 <td>
                     <div class="pull-right buttons">
-                        {% button class="btn btn-default btn-xs" disabled=p.is_protected text=_"delete" action={dialog_delete_rsc id=p.id} %}
-                        <a href="{% url admin_edit_rsc id=p.id %}" class="btn btn-default btn-xs">{_ edit _}</a>
+                        {% if m.acl.is_allowed.delete[p.id] %}
+                            {% button class="btn btn-default btn-xs" disabled=p.is_protected text=_"delete" action={dialog_delete_rsc id=p.id} %}
+                        {% endif %}
+                        {% if m.acl.is_allowed.update[p.id] %}
+                            <a href="{% url admin_edit_rsc id=p.id %}" class="btn btn-default btn-xs">{_ edit _}</a>
+                        {% endif %}
                     </div>
                     {{ p.reversed|yesno:"reversed,&nbsp;" }}
                 </td>

commit 3d8ab6c29706a9810e65227a5dfb947cbaba2955

mod_menu: Fix menu and user groups not editable
Broken in b835ee708e9b346c7833fde07179208f2e051fc3.

modules/mod_admin_category/templates/_admin_category_sorter.tpl
@@ -1,5 +1,4 @@
-{% with m.acl.is_allowed.update.category as editable %}
-
+{% with m.acl.is_allowed.insert.category as editable %}
 <ul id="category" class="tree-list categories {% if editable %}do_menuedit{% endif %}">
     {% for mid, path, action in m.category.menu|menu_flat %}
     {% with forloop.counter as c %}

modules/mod_menu/mod_menu.erl
@@ -92,7 +92,7 @@ handle_cmd(<<"copy">>, Data, Context) ->
             NewTitle = make_copy_title(m_rsc:p(FromId, title, Context), Context),
             case m_rsc:duplicate(FromId, [{title, NewTitle}], Context) of
                 {ok, NewId} ->
-                    {Html, Context1} = z_template:render_to_iolist({cat, "_menu_edit_item.tpl"}, [{id,NewId}], Context),
+                    {Html, Context1} = z_template:render_to_iolist({cat, "_menu_edit_item.tpl"}, [{id, NewId}, {editable, true}], Context),
                     z_render:wire({script, [{script, [
                                     <<"window.zMenuInsertAfter(\"">>,
                                             integer_to_list(FromId), $",$,,
@@ -108,7 +108,7 @@ handle_cmd(<<"copy">>, Data, Context) ->
 handle_cmd(<<"menu-item-render">>, Data, Context) ->
     Id = z_convert:to_integer(proplists:get_value(<<"id">>, Data)),
     Callback = proplists:get_value(<<"callback">>, Data),
-    {Html, Context2} = z_template:render_to_iolist({cat, "_menu_edit_item.tpl"}, [{id,Id}], Context),
+    {Html, Context2} = z_template:render_to_iolist({cat, "_menu_edit_item.tpl"}, [{id, Id}, {editable, true}], Context),
     z_render:wire({script, [{script, [
                     Callback, $(,
                         $", integer_to_list(Id), $",$,,

modules/mod_menu/templates/_admin_menu_hierarchy_sorter.tpl
@@ -1,7 +1,7 @@
-<ul id="hierarchy-{{ id.name }}" class="tree-list menu_hierarchy {% if is_editable %}do_menuedit{% endif %}">
+<ul id="hierarchy-{{ id.name }}" class="tree-list menu_hierarchy {% if editable %}do_menuedit{% endif %}">
 {% for mid, path, action in m.hierarchy[id.name].menu_ensured|menu_flat %}
     {% with forloop.counter as c %}
-        {% include "_menu_edit_item.tpl" c=forloop.counter id=mid %}
+        {% include "_menu_edit_item.tpl" c=forloop.counter id=mid editable=editable %}
     {% endwith %}
 {% endfor %}
 </ul>

modules/mod_menu/templates/_admin_menu_menu_view.tpl
@@ -2,30 +2,34 @@
 	<h3 class="widget-header">
 		{_ Menu _}
 	</h3>
-	<div class="widget-content">
 
-        <span class="btn-group pull-right">
-            <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">{_ Add menu item _} <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                <!-- dropdown menu links -->
-                <li><a href="#" data-where="top">&uarr; {_ Add top _}</a></li>
-                <li><a href="#" data-where="bottom">&darr; {_ Add bottom _}</a></li>
+    {% with id.is_editable as editable %}
+        <div class="widget-content">
+            {% if id.is_editable %}
+                <span class="btn-group pull-right">
+                    <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">{_ Add menu item _} <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <!-- dropdown menu links -->
+                        <li><a href="#" data-where="top">&uarr; {_ Add top _}</a></li>
+                        <li><a href="#" data-where="bottom">&darr; {_ Add bottom _}</a></li>
+                    </ul>
+                </span>
+            {% endif %}
+
+            <p>
+                {_ Click on <strong>Add menu item</strong> or <strong>Menu item</strong> to add pages. _}
+                <br/>{_ Drag menu items in the menu up, down, left or right to structure the menu. _}
+            </p>
+
+            {% block menu_tree %}
+            <ul class="tree-list tree-list-menu{% if editable %} do_menuedit{% endif %}" id="menu-{{ id }}" data-menuedit="item_template: '_menu_edit_item.tpl'">
+                {% for mid, path, action in id.menu|menu_flat %}
+                    {% include "_menu_edit_item.tpl" c=forloop.counter id=mid editable=editable %}
+                {% endfor %}
             </ul>
-        </span>
-
-        <p>
-            {_ Click on <strong>Add menu item</strong> or <strong>Menu item</strong> to add pages. _}
-            <br/>{_ Drag menu items in the menu up, down, left or right to structure the menu. _}
-        </p>
-
-        {% block menu_tree %}
-		<ul class="tree-list tree-list-menu do_menuedit" id="menu-{{ id }}" data-menuedit="item_template: '_menu_edit_item.tpl'">
-			{% for mid, path, action in id.menu|menu_flat %}
-				{% include "_menu_edit_item.tpl" c=forloop.counter id=mid %}
-			{% endfor %}
-		</ul>
-		{% endblock %}
-	</div>
+            {% endblock %}
+        </div>
+    {% endwith %}
 </div>
 
 {% include "_menu_edit_scripts.tpl" menu_id=#menu in_sorter="menu-"++id %}

modules/mod_menu/templates/_menu_edit_item.tpl
@@ -10,23 +10,25 @@
 
 		<i class="warning glyphicon glyphicon-eye-close" {% if id.is_published %}style="display: none"{% endif %}></i>
 
-        {% if editable %}
-            <span class="btns">
-                <span class="btn-group">
-                    <a href="#" class="btn btn-default btn-xs menu-edit">{_ Edit _}</a>
+        <span class="btns">
+            <span class="btn-group">
+                <a href="#" class="btn btn-default btn-xs menu-edit">{_ Edit _}</a>
 
+                {% if editable %}
                     <a href="#" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-cog"></i> <span class="caret"></span></a>
                     <ul class="dropdown-menu dropdown-menu-right">
                         <li><a href="#" data-where="before">&uarr; {_ Add before _}</a></li>
                         <li><a href="#" data-where="below">&rarr; {_ Add below _}</a></li>
                         <li><a href="#" data-where="after">&darr; {_ Add after _}</a></li>
                         <li class="divider"></li>
-                        <li><a href="#" data-where="copy">{_ Copy _}</a></li>
+                        {% if m.acl.is_allowed.insert[id.category.name] %}
+                            <li><a href="#" data-where="copy">{_ Copy _}</a></li>
+                        {% endif %}
                         <li><a href="#" data-where="remove">{_ Remove _}</a></li>
                     </ul>
-                </span>
+                {% endif %}
             </span>
-        {% endif %}
+        </span>
 	</div>
 
 	{% if action == `down` %}

modules/mod_menu/templates/admin_menu_hierarchy.tpl
@@ -11,38 +11,43 @@
 			<div class="row">
 				{# For now demand mod_admin_config rights #}
 				{% if m.acl.use.mod_admin_config %}
-					<div id="{{ #sorter }}" class="col-lg-8 col-md-8">
+                    {% with m.acl.is_allowed.insert[id.name] as editable %}
+                        <div id="{{ #sorter }}" class="col-lg-8 col-md-8">
 
-						<div class="clearfix panel">
-							<div class="btn-group pull-right">
-								<a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">{_ Add _} {{ id.title }} <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<!-- dropdown menu links -->
-									<li><a href="#" data-where="top">&uarr; {_ Add top _}</a></li>
-									<li><a href="#" data-where="bottom">&darr; {_ Add bottom _}</a></li>
-								</ul>
-							</div>
-						</div>
+                                <div class="clearfix panel">
+                                    {% if editable %}
+                                    <div class="btn-group pull-right">
+                                        <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">{_ Add _} {{ id.title }} <span class="caret"></span></a>
+                                        <ul class="dropdown-menu">
+                                            <!-- dropdown menu links -->
+                                            <li><a href="#" data-where="top">&uarr; {_ Add top _}</a></li>
+                                            <li><a href="#" data-where="bottom">&darr; {_ Add bottom _}</a></li>
+                                        </ul>
+                                    </div>
+                                    {% endif %}
+                                </div>
 
-						{% include "_admin_menu_hierarchy_sorter.tpl"
-									sorter=#sorter name=id.name is_editable
-									cat_id=id is_unique_ids
-						%}
-					</div>
+                            {% include "_admin_menu_hierarchy_sorter.tpl"
+                                        sorter=#sorter name=id.name
+                                        editable=editable
+                                        cat_id=id is_unique_ids
+                            %}
+                        </div>
 
-					<div id="sidebar" class="col-lg-4 col-md-4">
-						<div class="widget">
-							<h3 class="widget-header">{_ How does this work? _}</h3>
-							<div class="widget-content">
-								<p>
-									{_ Create an (optionally) nested navigation menu for this site. _}
-								</p>
-								<p>
-									{_ Drag pages to the place where you want them in the hierarchy. _}
-								</p>
-							</div>
-						</div>
-					</div>
+                        <div id="sidebar" class="col-lg-4 col-md-4">
+                            <div class="widget">
+                                <h3 class="widget-header">{_ How does this work? _}</h3>
+                                <div class="widget-content">
+                                    <p>
+                                        {_ Create an (optionally) nested navigation menu for this site. _}
+                                    </p>
+                                    <p>
+                                        {_ Drag pages to the place where you want them in the hierarchy. _}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    {% endwith %}
 				{% else %}
 					<ul class="list-group">
 						{% for cg in m.hierarchy[id.name].tree_flat %}