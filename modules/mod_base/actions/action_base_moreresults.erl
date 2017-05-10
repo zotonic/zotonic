33027dc554f683be67a5fb0d9158eb93f6529150
Marc Worrell <marc@worrell.nl>
2017-05-10 09:25:26 +0200

    mod_base: remove map-support test in moreresults. Issue #1689

--- modules/mod_base/actions/action_base_moreresults.erl
+++ modules/mod_base/actions/action_base_moreresults.erl
@@ -113,18 +113,13 @@ event(#postback{message={moreresults, SearchName, SearchProps, Page, PageLen, Mo
     z_render:appear_bottom(TargetId, Html, Context1).
 
 
-    to_id(Id) when is_integer(Id) -> Id;
-    to_id({Id,_}) when is_integer(Id) -> Id;
-    to_id({_,Id}) when is_integer(Id) -> Id;
-    to_id(T) when is_tuple(T) -> element(1, T);
-    to_id([{_,_}|_] = L) -> proplists:get_value(id, L);
-    to_id(M) ->
-        case erlang:is_builtin(erlang, is_map, 1) andalso erlang:is_map(M) of
-            true ->
-                maps:get(id, M, undefined);
-            false ->
-                undefined
-        end.
+to_id(Id) when is_integer(Id) -> Id;
+to_id({Id,_}) when is_integer(Id) -> Id;
+to_id({_,Id}) when is_integer(Id) -> Id;
+to_id(T) when is_tuple(T) -> element(1, T);
+to_id([{_,_}|_] = L) -> proplists:get_value(id, L);
+to_id(M) when is_map(M) -> maps:get(id, M, undefined).
+
 
 make_postback(SearchName, SearchProps, Page, PageLen, MorePageLen, Args, TriggerId, TargetId, Context) ->
     Postback = {moreresults, SearchName, SearchProps, Page, PageLen, MorePageLen, Args},
