4d649044c866e600b4fa0c86135dcc601ef0cb53
Blaise Fringel <ffringel9@gmail.com>
2017-06-01 20:39:38 +0100

    Header file for escripts

--- src/scripts/escripts/helpers/zotonic_escript_helper.hrl
+++ src/scripts/escripts/helpers/zotonic_escript_helper.hrl
@@ -17,10 +17,27 @@
 %% usage zotonic generate-edoc
 %%%-------------------------------------------------------------------
 
--define(ZOTONIC_BIN, "ZOTONIC/bin").
--define(MAKE, "make").
+-define(ZOTONIC_BIN, "$ZOTONIC/bin").
 -define(ERL, "erl").
+-define(MAKE, "make").
 -define(MAX_PORTS, "ulimit -n|sed 's/unlimited/100000/'").
 -define(MAX_PROCESSES, 10000000).
 -define(KERNEL_POLL, true).
+-define(USER_EBIN_DIR, "").
+-define(PA, "$ZOTONIC/_checkouts/*/ebin $ZOTONIC/_build/default/lib/*/ebin $USER_EBIN_DIR $ZOTONIC/modules/*/deps/*/ebin $ZOTONIC/priv/modules/*/deps/*/ebin $ZOTONIC/priv/sites/*/deps/*/ebin $ZOTONIC/priv/sites/*/modules/*/deps/*/ebin").
 -define(NODENAME, "zotonic001").
+-define(ZOTONIC_DISTRIBUTED, "").
+
+-ifdef(ZOTONIC_DISTRIBUTED).
+-define(NAME_ARG, "-name").
+-define(NODEHOST, "").
+-else.
+-define(NAME_ARG, "-sname").
+-define(NODEHOST, "").
+-endif.
+
+-define(ZOTONIC_PIDFILE, "$ZOTONIC/zotonic.pid").
+-define(ERL_CALL, "").
+-define(ZOTONIC_CALL, "").
+-define(MODULES, "").
+-define(SITES, "").
