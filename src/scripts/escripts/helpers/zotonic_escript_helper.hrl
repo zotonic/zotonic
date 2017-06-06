5c11670cf0df568c2ecb2d5d13d3c8659c8f01da
Melki <tantemelki@gmail.com>
2017-06-06 13:19:13 +0100

    zotonic-configfile

--- src/scripts/escripts/helpers/zotonic_escript_helper.hrl
+++ src/scripts/escripts/helpers/zotonic_escript_helper.hrl
@@ -24,9 +24,7 @@
 -define(MAX_PROCESSES, 10000000).
 -define(KERNEL_POLL, true).
 -define(USER_EBIN_DIR, "").
--define(PA, "$ZOTONIC/_checkouts/*/ebin $ZOTONIC/_build/default/lib/*/ebin $USER_EBIN_DIR $ZOTONIC/modules/*/deps/*/ebin $ZOTONIC/priv/modules/*/deps/*/ebin $ZOTONIC/priv/sites/*/deps/*/ebin $ZOTONIC/priv/sites/*/modules/*/deps/*/ebin").
 -define(NODENAME, "zotonic001").
--define(ZOTONIC_DISTRIBUTED, "").
 
 -ifdef(ZOTONIC_DISTRIBUTED).
 -define(NAME_ARG, "-name").
@@ -37,5 +35,5 @@
 -endif.
 
 -define(ZOTONIC_PIDFILE, "$ZOTONIC/zotonic.pid").
--define(MODULES, "").
--define(SITES, "").
+-define(Modules, zotonic_setup:get_user_modules_dir()).
+-define(Sites, zotonic_setup:get_user_sites_dir()).
