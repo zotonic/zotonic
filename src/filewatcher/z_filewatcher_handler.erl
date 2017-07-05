ec80377a3d44036ee6a3b8defc8bab1e0f7100bc
Marc Worrell <marc@worrell.nl>
2017-07-05 10:58:27 +0200

    filewatcher: fix blacklist re for log files.

--- src/filewatcher/z_filewatcher_handler.erl
+++ src/filewatcher/z_filewatcher_handler.erl
@@ -32,7 +32,7 @@
 
 %% Which files do we not consider at all in the file_changed handler
 -define(FILENAME_BLACKLIST_RE,
-        "_flymake|\\.#|/sites/[^/]+/files/|/\\.git/|/\\.gitignore|\\.hg/|/log/\\.log$|/mnesia/").
+        "_flymake|\\.#|/sites/[^/]+/files/|/\\.git/|/\\.gitignore|\\.hg/|/log/|\\.log$|/mnesia/").
 
 
 %% @doc Called when a file is changed on disk. Decides what to do.
