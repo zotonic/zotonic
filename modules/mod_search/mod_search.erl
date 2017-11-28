57e7c671e5192243e5f1d02d70d6d982ee128365
Marc Worrell <marc@worrell.nl>
2017-11-28 14:29:33 +0100

    mod_search: add  option to match on given object-ids instead of only a subject.

--- modules/mod_search/mod_search.erl
+++ modules/mod_search/mod_search.erl
@@ -262,28 +262,43 @@ search({archive_year_month, [{cat,Cat}]}, OffsetLimit, Context) ->
 
 
 %% @doc Return the rsc records that have similar objects
-search({match_objects, [{id,Id}]}, _OffsetLimit, Context) ->
-	ObjectIds = m_edge:objects(Id, Context),
-	MatchTerms = [ ["zpo",integer_to_list(ObjId)] || ObjId <- ObjectIds ],
-	TsQuery = lists:flatten(z_utils:combine("|", MatchTerms)),
-	case TsQuery of
-		[] ->
-			#search_result{};
-		_ ->
-		    #search_sql{
-		        select="r.id, ts_rank(pivot_rtsv, query) AS rank",
-		        from="rsc r, to_tsquery($1) query",
-		        where=" query @@ pivot_rtsv and id <> $2",
-		        order="rank desc, r.publication_start desc",
-		        args=[TsQuery, z_convert:to_integer(Id)],
-		        tables=[{rsc,"r"}]
-		    }
-	end;
+search({match_objects, [{id, Id}]}, OffsetLimit, Context) ->
+    ObjectIds = m_edge:objects(Id, Context),
+    search({match_objects, [{id_exclude, Id}, {ids, ObjectIds}]}, OffsetLimit, Context);
+search({match_objects, [{ids, ObjectIds}]}, OffsetLimit, Context) ->
+    search({match_objects, [{id_exclude, undefined}, {ids, ObjectIds}]}, OffsetLimit, Context);
+search({match_objects, [{id_exclude, Exclude}, {ids, ObjectIds}]}, _OffsetLimit, Context) ->
+    ExcludeId = m_rsc:rid(Exclude, Context),
+    ObjectIds1 = [ m_rsc:rid(OId, Context) || OId <- ObjectIds ],
+    MatchTerms = [ ["zpo",integer_to_list(ObjId)] || ObjId <- ObjectIds1, ObjId =/= undefined ],
+    TsQuery = lists:flatten(z_utils:combine("|", MatchTerms)),
+    case TsQuery of
+        [] ->
+            #search_result{};
+        _ when is_integer(ExcludeId) ->
+            #search_sql{
+                select="r.id, ts_rank(pivot_rtsv, query) AS rank",
+                from="rsc r, to_tsquery($1) query",
+                where=" query @@ pivot_rtsv and id <> $2",
+                order="rank desc, r.publication_start desc",
+                args=[TsQuery, z_convert:to_integer(ExcludeId)],
+                tables=[{rsc,"r"}]
+            };
+        _ ->
+            #search_sql{
+                select="r.id, ts_rank(pivot_rtsv, query) AS rank",
+                from="rsc r, to_tsquery($1) query",
+                where=" query @@ pivot_rtsv",
+                order="rank desc, r.publication_start desc",
+                args=[TsQuery],
+                tables=[{rsc,"r"}]
+            }
+    end;
 search({match_objects, [{cat,Cat},{id,Id}]}, OffsetLimit, Context) ->
-	case search({match_objects, [{id,Id}]}, OffsetLimit, Context) of
-		#search_sql{} = Search -> Search#search_sql{cats=[{"r", Cat}]};
-		Result -> Result
-	end;
+    case search({match_objects, [{id,Id}]}, OffsetLimit, Context) of
+        #search_sql{} = Search -> Search#search_sql{cats=[{"r", Cat}]};
+        Result -> Result
+    end;
 
 %% @doc Return the rsc records that have similar objects
 search({match_objects_cats, [{id,Id}]}, _OffsetLimit, Context) ->
