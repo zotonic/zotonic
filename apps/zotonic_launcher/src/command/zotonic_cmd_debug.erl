d5ed7dbcbfd885ff9df51012b594e8d61941e98d
Blaise Fringel <ffringel9@gmail.com>
2017-12-18 11:00:52 -0500

    Zotonic command framework

---
+++ apps/zotonic_launcher/src/command/zotonic_cmd_debug.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author Blaise
+%%%Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%	 http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Dec 2017 7:55 PM
+%%%-------------------------------------------------------------------
+-module(zotonic_cmd_debug).
+-author("Blaise").
+
+%% API
+-export([run/1]).
+
+-include("zotonic_command.hrl").
+
+run(_) ->
+    erlang:error(not_implemented).
