45200f0ed222e14dee214d87852e23d73e29021f
David de Boer <david@ddeboer.nl>
2018-04-02 12:20:26 +0200

    mod_search: Fix #1895 by also splitting non-binaries (#1896)
    From query pages, `hassubject=123` yields `{hassubject, <<"123">>}`.
    From templates, however, `hassubject=123` yields `{hassubject, 123}`.
    Consequently, we have to split both binaries and integers.
    
    Ref #1894
    (cherry picked from commit 801ca4b73fae4df3afb9e5289c93e0c7ce331d3f)

--- apps/zotonic_mod_search/src/support/search_query.erl
+++ apps/zotonic_mod_search/src/support/search_query.erl
@@ -533,7 +533,7 @@ parse_query([Term|_], _Context, _Result) ->
 
 %% @doc Parse hassubject and hasobject edges.
 -spec parse_edges(hassubject | hasobject, binary() | list(), #search_sql{}, z:context()) -> #search_sql{}.
-parse_edges(Term, Edges, Result, Context) when is_binary(Edges) ->
+parse_edges(Term, Edges, Result, Context) when not is_list(Edges) ->
     parse_edges(Term, maybe_split_list(Edges), Result, Context);
 parse_edges(Term, [[Id, Predicate]], Result, Context) ->
     parse_edges(Term, [[Id, Predicate, "rsc"]], Result, Context);
@@ -865,8 +865,6 @@ map_filter_operator(Op) -> throw({error, {unknown_filter_operator, Op}}).
 
 
 % Convert an expression like [123,hasdocument]
-maybe_split_list(Id) when is_integer(Id) ->
-    [Id];
 maybe_split_list(<<"[", Rest/binary>>) ->
     split_list(Rest);
 maybe_split_list([$[|Rest]) ->
