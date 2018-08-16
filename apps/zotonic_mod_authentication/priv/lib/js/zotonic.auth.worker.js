600070364f8bfd7b91d819bb4fa6789092d95a19
Marc Worrell <marc@worrell.nl>
2018-08-16 21:02:35 +0200

    WIP: add auth worker and dispatch for service worker.

--- apps/zotonic_mod_authentication/priv/lib/js/zotonic.auth.worker.js
+++ apps/zotonic_mod_authentication/priv/lib/js/zotonic.auth.worker.js
@@ -23,46 +23,75 @@ console.log("zotonic-auth-worker here");
 //
 
 var model = {
-        status: 'stopped'
+        status: 'start'
     } ;
 
 model.present = function(data) {
-    if (data.status) {
-        model.status = data.status;
-    }
-
-    if (data.start) {
-        if (model.status !== 'started') {
-            model.status = 'starting';
-            $.post("http://cloudsentinel.com:3099/server/start",
-            {},
-            function(data, status){
-                console.log("Data: " + data + "\nStatus: " + status);
-            });
+    if (state.start(model)) {
+        if (data.check_session_state) {
+            model.status = "check_sessionstate";
+            self.call("model/sessionStorage/get/auth")
+                .then(function(msg) {
+                    actions.newSessionState({ auth: msg.payload });
+                });
         }
     }
 
-    if (data.stop) {
-        if (model.status !== 'stopped') {
-            model.status = 'stopping';
-            $.post("http://cloudsentinel.com:3099/server/stop",
-            {},
-            function(data, status){
-                console.log("Data: " + data + "\nStatus: " + status);
-            });
+    if (state.checkSessionState(model)) {
+        if (data.received_session_state) {
+            if (!data.auth) {
+                // Unknown - check 'remember-me' cookie with server
+                model.status = "check_autologon";
+            } else if (data.auth.user_id) {
+                model.status = "authenticated";
+                model.user_id = data.auth.user_id;
+            } else {
+                model.status = "anonymous";
+                model.user_id = undefined;
+            }
         }
     }
 
-    if (data.abort) {
-        if (model.status !== 'starting') {
-            $.post("http://cloudsentinel.com:3099/server/stop",
-            {},
-            function(data, status){
-                console.log("Data: " + data + "\nStatus: " + status);
-            });
+    if (state.checkAutoLogon(model)) {
+        if (data.check_autologon) {
+            // TODO: Check auth-uri on server for autologon token
+            //       Uri should return JSON for new connection details
+            //       Similar to authentication with username+password
+            //
+            // Autologin cookie:  <userid>:<series>
+            // In localStorage: <random>
+            // After every login, change the random token.
+            // If presented with illegal random token, remove the series.
+            // (Reuse might happen when cookie+token is stolen).
+            model.status = "authenticating";
+            self.call("model/localStorage/get/autologon")
+                .then((msg) => {
+                    if (msg.payload) {
+                        return fetch( self.abs_url("/zotonic-auth/check"), {
+                            method: "POST",
+                            cache: "no-cache",
+                            headers: {
+                                "Accept": "application/json; charset=utf-8"
+                            },
+                            body: ""
+                        } )
+                    } else {
+                        throw new Error("No autologon in localStorage");
+                    }
+                })
+                .then((resp) => {
+                    console.log("jaaa", resp);
+                })
+                .catch((e) => {
+                    // Error - assume anonymous
+                    model.status = "anonymous";
+                });
         }
     }
 
+    console.log("AUTH state", model);
+
+    // .. change state
     state.render(model) ;
 }
 
@@ -79,43 +108,12 @@ view.init = function(model) {
 
 // State representation of the ready state
 view.ready = function(model) {
-    return (
-            "<p>Status:"+model.status+"</p>\n\
-            <form onSubmit=\"return actions.start({});\">\n\
-                <input type=\"submit\" value=\"Start\">\n\
-            </form>"
-        ) ;
-
+    return "";
 }
 
-// State representation of the counting state
-view.starting = function(model) {
-
-    return (
-            "<p>Status:"+model.status+"</p>\n\
-             <form onSubmit=\"return actions.abort({});\">\n\
-                <input type=\"submit\" value=\"Abort\">\n\
-            </form>"
-        ) ;
-
-}
-
-// State representation of the aborted state
-view.started = function(model) {
-
-    return (
-            "<p>Status:"+model.status+"</p>\n\
-             <form onSubmit=\"return actions.stop({});\">\n\
-                <input type=\"submit\" value=\"Stop\">\n\
-            </form>"
-        ) ;
-
-}
 
 //display the state representation
 view.display = function(representation) {
-    // var stateRepresentation = document.getElementById("representation");
-    // stateRepresentation.innerHTML = representation;
 }
 
 // Display initial state
@@ -130,50 +128,60 @@ var state =  { view: view} ;
 
 model.state = state ;
 
-// Derive the state representation as a function of the systen
-// control state
+// Derive the state representation as a function of the systen control state
 state.representation = function(model) {
-    var representation = 'oops... something went wrong, the system is in an invalid state' ;
-
-    if (state.ready(model)) {
-        representation = state.view.ready(model) ;
-    }
-
-    if (state.starting(model)) {
-        representation = state.view.starting(model) ;
-    }
-
-    if (state.started(model)) {
-        representation = state.view.started(model) ;
-    }
-
-    state.view.display(representation) ;
+    // var representation = 'oops... something went wrong, the system is in an invalid state' ;
+    // if (state.ready(model)) {
+    //     representation = state.view.ready(model) ;
+    // }
+    // ...
+    // state.view.display(representation) ;
 }
 
 // Derive the current state of the system
-state.ready = function(model) {
-    return ((model.status === 'stopped') || (model.status === 'stopping')) ;
+state.start = function(model) {
+    return (model.status === 'start');
 }
 
-state.starting = function(model) {
-    return (model.status === 'starting') ;
+state.checkSessionState = function(model) {
+    return model.status === 'check_sessionstate';
 }
 
-state.started = function(model) {
-    return (model.status === 'started') ;
+state.anonymous = function(model) {
+    return (model.status === 'anonymous');
 }
 
-state.changingState = function(model) {
-    return ((model.status === 'starting') || (model.status === 'stopping')) ;
+state.authenticating = function(model) {
+    return (model.status === 'authenticating');
 }
+
+state.exchanging = function(model) {
+    return (model.status === 'exchanging');
+}
+
+state.checkAutoLogon = function(model) {
+    return (model.status === 'check_autologon');
+}
+
+state.authenticated = function(model) {
+    return (model.status === 'authenticated');
+}
+
+state.cleanup = function(model) {
+    return (model.status === 'cleanup');
+}
+
 
 // Next action predicate, derives whether
 // the system is in a (control) state where
 // an action needs to be invoked
 
 state.nextAction = function (model) {
-    if (state.changingState(model)) {
-        actions.getStatus({},model.present) ;
+    if (state.checkAutoLogon(model)) {
+        actions.checkAutoLogon();
+    }
+    if (state.cleanup(model)) {
+        // cleanup - and transfer to anonymous
     }
 }
 
@@ -189,59 +197,43 @@ state.render = function(model) {
 
 var actions = {} ;
 
-actions.start = function(data, present) {
-    present = present || model.present ;
-    data.start = true ;
-    present(data) ;
-    return false ;
+actions.start = function(data) {
+    data = data || {};
+    data.check_session_state = true ;
+    model.present(data);
 }
 
-actions.stop = function(data, present) {
-    present = present || model.present ;
-    data.stop = true ;
-    present(data) ;
-    return false ; 
+actions.newSessionState = function(data) {
+    data = data || {};
+    if ("auth" in data) {
+        data.received_session_state = true;
+        model.present(data);
+    }
 }
 
-actions.abort = function(data, present) {
-    present = present || model.present ;
-    data.abort = true ;
-    present(data) ;
-    return false ;
+actions.checkAutoLogon = function(data) {
+    data = data || {};
+    data.check_autologon = true;
+    model.present(data);
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Worker Startup
 //
 
 self.on_connect = function() {
-    console.log("connected");
-
-    self.call("model/sessionStorage/get/a")
-        .then(function(msg) {
-            console.log(msg.payload);
-        });
-    self.publish("model/sessionStorage/post/a", "From the worker");
-    self.call("model/sessionStorage/get/a")
-        .then(function(msg) {
-            console.log(msg.payload);
-        });
-
-
-    // let date = new Date();
-    // self.publish("ui/insert", {id: "second", inner: true, snippet: second_hand(date),  priority: 10});
-    // self.publish("ui/insert", {id: "minute", inner: true, snippet: minute_hand(date), priority: 10});
-    // self.publish("ui/insert", {id: "hour", inner: true, snippet: hour_hand(date), priority: 10});
-    // self.publish("ui/render");
-
-    // setInterval(function() {
-    //     let date = new Date();
-    //     self.publish("ui/update", {id: "second", snippet: second_hand(date)});
-    //     self.publish("ui/update", {id: "minute", snippet: minute_hand(date)});
-    //     self.publish("ui/update", {id: "hour", snippet: hour_hand(date)});
-    //     self.publish("ui/render");
-    // }, 1000);
+    // self.call("model/sessionStorage/get/a")
+    //     .then(function(msg) {
+    //         console.log("1", msg.payload);
+    //     });
+    // self.publish("model/sessionStorage/post/a", "From the worker");
+    // self.call("model/sessionStorage/get/a")
+    //     .then(function(msg) {
+    //         console.log("2", msg.payload);
+    //     });
+    setTimeout(function() {
+        actions.start();
+    }, 0);
 };
 
 self.connect();
