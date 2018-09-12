zotonic/zotonic

commit c0a89926678de3786ee946f0ec2b99c5e11b2038

core: Call z_datamodel:manage inside a transaction to prevent inconsistencies. See #1927

src/support/z_datamodel.erl
@@ -61,14 +61,19 @@ manage(Module, Datamodel, Context) ->
 %% @doc Install / update a set of named, predefined resources, categories, predicates, media and edges.
 -spec manage(atom(), #datamodel{}, datamodel_options(), #context{}) -> ok.
 manage(Module, Datamodel, Options, Context) ->
-    AdminContext = z_acl:sudo(Context),
-    [manage_category(Module, Cat, Options, AdminContext) || Cat <- Datamodel#datamodel.categories],
-    [manage_predicate(Module, Pred, Options, AdminContext) || Pred <- Datamodel#datamodel.predicates],
-    [manage_resource(Module, R, Options, AdminContext) || R <- Datamodel#datamodel.resources],
-    [manage_medium(Module, Medium, Options, AdminContext) || Medium <- Datamodel#datamodel.media],
-    [manage_edge(Module, Edge, Options, AdminContext) || Edge <- Datamodel#datamodel.edges],
-    [manage_data(Module, Data, AdminContext) || Data <- Datamodel#datamodel.data],
-    ok.
+    F = fun(Ctx) ->
+		[manage_category(Module, Cat, Options, Ctx) || Cat <- Datamodel#datamodel.categories],
+		[manage_predicate(Module, Pred, Options, Ctx) || Pred <- Datamodel#datamodel.predicates],
+		[manage_resource(Module, R, Options, Ctx) || R <- Datamodel#datamodel.resources],
+		[manage_medium(Module, Medium, Options, Ctx) || Medium <- Datamodel#datamodel.media],
+		[manage_edge(Module, Edge, Options, Ctx) || Edge <- Datamodel#datamodel.edges],
+		[manage_data(Module, Data, Ctx) || Data <- Datamodel#datamodel.data],
+
+		%% And flush the cache to make sure everybody is going to read fresh data.
+		z_depcache:flush(Ctx),
+		ok
+	end,
+    ok = z_db:transaction(F, z_acl:sudo(Context)).
 
 manage_medium(Module, {Name, Props}, Options, Context) ->
     manage_resource(Module, {Name, media, Props}, Options, Context);

commit efee46463f8fd5c552c174ed9db0e4731984bfcb

core: Fix pivot failure during manage_data
c0a89926678de3786ee946f0ec2b99c5e11b2038 introduced a bug where a site
initialization from an empty database (with edges defined) throws:

z_db error {error,error,<<"23505">>,
<<"duplicate key value violates unique constraint \"rsc_pivot_queue_pkey\"">>,
[{detail,<<"Key (rsc_id)=(382) already exists.">>}]} in query
"insert into rsc_pivot_queue (rsc_id, due, is_update) values ($1, current_timestamp, true)" with [382]

This change fixes that by disabling the automatic object pivot when
m_edge:insert/5 is called from z_datamodel:manage_edge/4.

src/support/z_datamodel.erl
@@ -287,7 +287,7 @@ map_prop(Value, _Context) ->
     Value.
 
 manage_edge(_Module, {SubjectName, PredicateName, ObjectName}, _Options, Context) ->
-    manage_edge(_Module, {SubjectName, PredicateName, ObjectName, []}, _Options, Context);
+    manage_edge(_Module, {SubjectName, PredicateName, ObjectName, [{no_touch, true}]}, _Options, Context);
 manage_edge(_Module, {SubjectName, PredicateName, ObjectName, EdgeOptions}, _Options, Context) ->
     Subject = m_rsc:name_to_id(SubjectName, Context),
     Predicate = m_predicate:name_to_id(PredicateName, Context),