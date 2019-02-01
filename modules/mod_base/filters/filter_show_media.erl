311bbd97a58b1b03630b4a41145acaa4f0f2aee7
Marc Worrell <marc@worrell.nl>
2019-02-01 16:08:02 +0100

    show_media: support link_url

--- modules/mod_base/filters/filter_show_media.erl
+++ modules/mod_base/filters/filter_show_media.erl
@@ -101,6 +101,7 @@ filter_args(Args, Context) ->
 
 % See _tinymce_dialog_zmedia_props.tpl
 is_valid_arg({<<"link">>, _}) -> true;
+is_valid_arg({<<"link_url">>, _}) -> true;
 is_valid_arg({<<"crop">>, _}) -> true;
 is_valid_arg({<<"caption">>, _}) -> true;
 is_valid_arg({<<"size">>, _}) -> true;
@@ -130,6 +131,11 @@ filter_args([ {<<"link">>, <<"link">>} | Args ], HasSize, Acc, Context) ->
     filter_args(Args, HasSize, [ {link, true} | Acc ], Context);
 filter_args([ {<<"link">>, _} | Args ], HasSize, Acc, Context) ->
     filter_args(Args, HasSize, [ {link, undefined} | Acc ], Context);
+filter_args([ {<<"link_url">>, <<>>} | Args ], HasSize, Acc, Context) ->
+    filter_args(Args, HasSize, [ {link_url, <<>>} | Acc ], Context);
+filter_args([ {<<"link_url">>, Url} | Args ], HasSize, Acc, Context) ->
+    Url1 = z_html:sanitize_uri(Url),
+    filter_args(Args, HasSize, [ {link_url, Url1} | Acc ], Context);
 filter_args([ {<<"align">>, Align} | Args ], HasSize, Acc, Context) ->
     filter_args(Args, HasSize, [ {align, Align} | Acc ], Context);
 filter_args([ {<<"caption">>, Caption} | Args ], HasSize, Acc, Context) ->
