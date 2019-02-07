369341ff133f7b30f677e5fda0e1899cd8f19575
Marc Worrell <marc@worrell.nl>
2019-02-07 16:36:14 +0100

    Add APLv2 license text to mod_clamav

--- modules/mod_clamav/mod_clamav.erl
+++ modules/mod_clamav/mod_clamav.erl
@@ -1,3 +1,22 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2019 Marc Worrell
+
+%% @doc Scan uploaded files with clamd.
+
+%% Copyright 2019 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(mod_clamav).
 
 -mod_title("ClamAV").
