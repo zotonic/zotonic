6e96722c0b3d6f74a0b3b2dd7669851af2e53f6d
Rob van den Bogaard <robvandenbogaard@gmail.com>
2019-02-13 13:48:09 +0100

    [mod_admin] Don't let flush button in admin interface flush all sites (#2000)
    * ISSUE-1995 [mod_admin] Flush button in admin interface flushes only corresponding site
    
    * Update text of growl

--- modules/mod_admin/actions/action_admin_admin_tasks.erl
+++ modules/mod_admin/actions/action_admin_admin_tasks.erl
@@ -20,8 +20,12 @@ render_action(TriggerId, TargetId, Args, Context) ->
 	{PostbackMsgJS, Context}.
 
 %% @doc Flush the caches of all sites.
+event(#postback{message={admin_tasks, [{task, "flush_all_sites"}]}}, Context) ->
+    do(fun z:flush/0, "All sites have been flushed.", Context);
+
+%% @doc Flush the caches of the site referred to in the supplied context.
 event(#postback{message={admin_tasks, [{task, "flush"}]}}, Context) ->
-    do(fun z:flush/0, "Caches have been flushed.", Context);
+    do(fun() -> z:flush(Context) end, "Caches have been flushed.", Context);
 
 %% @doc Reset templates.
 event(#postback{message={admin_tasks, [{task, "templates_reset"}]}}, Context) ->
