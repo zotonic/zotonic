1c6afe241eacf2ba8eaa85a0ac1a4cddd0e2dc5d
Marc Worrell <marc@worrell.nl>
2019-04-16 13:40:36 +0200

    Fix a problem with mnesia-dir where the node could be appended twice. Issue #2047

--- apps/zotonic_core/src/zotonic_core.erl
+++ apps/zotonic_core/src/zotonic_core.erl
@@ -84,14 +84,17 @@ mnesia_dir_config() ->
     case application:get_env(mnesia, dir) of
         {ok, none} -> undefined;
         {ok, ""} -> undefined;
-        {ok, Dir} -> mnesia_dir_append_node(Dir);
-        undefined ->
-            PrivDir = case code:priv_dir(zotonic) of
-                {error, bad_name} -> code:priv_dir(zotonic_core);
-                ZotonicPrivDir when is_list(ZotonicPrivDir) -> ZotonicPrivDir
-            end,
-            mnesia_dir_append_node(filename:join([ PrivDir, "mnesia" ]))
+        {ok, "priv/mnesia"} -> mnesia_priv_dir();
+        {ok, Dir} -> {ok, Dir};
+        undefined -> mnesia_priv_dir()
     end.
+
+mnesia_priv_dir() ->
+    PrivDir = case code:priv_dir(zotonic) of
+        {error, bad_name} -> code:priv_dir(zotonic_core);
+        ZotonicPrivDir when is_list(ZotonicPrivDir) -> ZotonicPrivDir
+    end,
+    mnesia_dir_append_node(filename:join([ PrivDir, "mnesia" ])).
 
 mnesia_dir_append_node(Dir) ->
     MnesiaDir = filename:join([ Dir, atom_to_list(node()) ]),
