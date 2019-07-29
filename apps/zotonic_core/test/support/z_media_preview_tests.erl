7b777420a4e8e373291509dbfd71b8b61e1b5a46
Marc Worrell <marc@worrell.nl>
2019-07-29 11:59:52 +0200

    Fix media preview test for binary mimes.

--- apps/zotonic_core/test/support/z_media_preview_tests.erl
+++ apps/zotonic_core/test/support/z_media_preview_tests.erl
@@ -9,7 +9,7 @@
 cmd_args_jpeg_test() ->
     Props = [{width,100}, {height,66}, {mime,"image/jpeg"}, {orientation,1}],
     Filters = [{crop,center}, {width,80}, {height,80}],
-    {_W,_H,Args} = z_media_preview:cmd_args(Props, Filters, "image/jpeg"),
+    {_W,_H,Args} = z_media_preview:cmd_args(Props, Filters, <<"image/jpeg">>),
     CmdArgs = lists:flatten(z_utils:combine(32, Args)),
     ?assertEqual("-background \"white\" -layers \"flatten\"    -gravity NorthWest -crop 80x80+21+0 -extent 80x80 +repage -colorspace \"sRGB\" -set units PixelsPerInch -density 72   -unsharp 0.3x0.7  -quality 97", CmdArgs).
 
@@ -17,7 +17,7 @@ cmd_args_jpeg_test() ->
 cmd_args_gif_test() ->
     Props = [{width,100}, {height,66}, {mime,"image/gif"}, {orientation,1}],
     Filters = [{crop,center}, {width,80}, {height,80}],
-    {_W,_H,Args} = z_media_preview:cmd_args(Props, Filters, "image/gif"),
+    {_W,_H,Args} = z_media_preview:cmd_args(Props, Filters, <<"image/gif">>),
     CmdArgs = lists:flatten(z_utils:combine(32, Args)),
     ?assertEqual("-coalesce    -gravity NorthWest -crop 80x80+21+0 -extent 80x80 +repage -colorspace \"sRGB\" -set units PixelsPerInch -density 72  ", CmdArgs).
 
