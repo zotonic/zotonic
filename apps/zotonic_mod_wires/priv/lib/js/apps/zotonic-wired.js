64395a4f585a202ec838a8f02e4cee407880a44d
Marc Worrell <marc@worrell.nl>
2019-08-08 00:00:58 +0200

    Uncomment is_equal function.

--- apps/zotonic_mod_wires/priv/lib/js/apps/zotonic-wired.js
+++ apps/zotonic_mod_wires/priv/lib/js/apps/zotonic-wired.js
@@ -1555,22 +1555,23 @@ $.fn.selected = function(select) {
     });
 };
 
-// function is_equal(x, y) {
-//     if ( x === y ) return true;
-//     if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
-//     if ( x.constructor !== y.constructor ) return false;
-//     for ( var p in x ) {
-//         if ( ! x.hasOwnProperty( p ) ) continue;
-//         if ( ! y.hasOwnProperty( p ) ) return false;
-//         if ( x[ p ] === y[ p ] ) continue;
-//         if ( typeof( x[ p ] ) !== "object" ) return false;
-//         if ( ! is_equal( x[ p ],  y[ p ] ) ) return false;
-//     }
-//     for ( p in y ) {
-//         if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
-//     }
-//     return true;
-// }
+// Used in z.feedback.js
+function is_equal(x, y) {
+    if ( x === y ) return true;
+    if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+    if ( x.constructor !== y.constructor ) return false;
+    for ( var p in x ) {
+        if ( ! x.hasOwnProperty( p ) ) continue;
+        if ( ! y.hasOwnProperty( p ) ) return false;
+        if ( x[ p ] === y[ p ] ) continue;
+        if ( typeof( x[ p ] ) !== "object" ) return false;
+        if ( ! is_equal( x[ p ],  y[ p ] ) ) return false;
+    }
+    for ( p in y ) {
+        if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+    }
+    return true;
+}
 
 $.extend({
     keys: function(obj){
