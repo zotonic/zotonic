7128586a7f74dc7100f4eb2a830ca7673d0799f0
Marc Worrell <marc@worrell.nl>
2019-08-27 18:22:08 +0200

    Cowboy needs headers to be lowercase

--- apps/zotonic_core/src/support/z_context.erl
+++ apps/zotonic_core/src/support/z_context.erl
@@ -999,7 +999,7 @@ set_security_headers(Context) ->
                 {<<"x-content-type-options">>, <<"nosniff">>},
                 {<<"x-permitted-cross-domain-policies">>, <<"none">>},
                 {<<"referrer-policy">>, <<"origin-when-cross-origin">>},
-                {<<"X-DTAP-Environment">>, atom_to_binary( m_site:environment(Context), utf8 )} ],
+                {<<"x-dtap-environment">>, atom_to_binary( m_site:environment(Context), utf8 )} ],
     Default1 = case z_context:get(allow_frame, Context, false) of
         true -> Default;
         false -> [ {<<"x-frame-options">>, <<"sameorigin">>} | Default ]
