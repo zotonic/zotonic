fc68972abebb9f899a2073974d8e00098c9d7c8c
Marc Worrell <marc@worrell.nl>
2019-10-15 17:57:18 +0200

    Add support for site config dir in zotonic-config dir.

---
+++ apps/zotonic_core/src/support/z_sites_config.erl
@@ -0,0 +1,132 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2019 Marc Worrell
+%% @doc Load and manage site configuration files.
+
+%% Copyright 2019 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(z_sites_config).
+
+-export([
+    app_is_site/1,
+    config_files/1,
+    config_files/2,
+    read_configs/1
+    ]).
+
+-define(CONFIG_FILE, "zotonic_site.*").
+
+-spec app_is_site( atom() ) -> boolean().
+app_is_site( App ) ->
+    file:is_regular( site_config_file( App ) ).
+
+%% @doc Return the main configuration file for a site
+-spec site_config_file( atom() ) -> file:filename_all() | {error, bad_name}.
+site_config_file(Site) ->
+    case z_path:site_dir(Site) of
+        {error, _} = Error ->
+            Error;
+        SiteDir ->
+            Files = filelib:wildcard( filename:join([ SiteDir, "priv", ?CONFIG_FILE ]) ),
+            Files1 = lists:filter(
+                fun(F) ->
+                    case filename:extension(F) of
+                        ".config" -> true;
+                        ".yaml" -> true;
+                        ".yml" -> true;
+                        ".json" -> true;
+                        _ -> false
+                    end
+                end,
+                Files),
+            case Files1 of
+                [] -> {error, bad_name};
+                [ File | _ ] -> File
+            end
+    end.
+
+-spec config_files( atom() ) -> list( file:filename_all() ).
+config_files( Site ) ->
+    config_files( node(), Site ).
+
+-spec config_files( node(), atom() ) -> list( file:filename_all() ).
+config_files(Node, Site) ->
+    case site_config_file(Site) of
+        {error, _} ->
+            [];
+        ConfigFile ->
+            SitePrivDir = filename:dirname(ConfigFile),
+            case z_config_files:config_dir(Node) of
+                {ok, ConfigDir} ->
+                    [ ConfigFile ]
+                    ++ z_config_files:files( filename:join([ ConfigDir, "site_config.d", Site ]) )
+                    ++ z_config_files:files( filename:join([ SitePrivDir, "config.d" ]) );
+                {error, _} ->
+                    [ ConfigFile ]
+                    ++ z_config_files:files( filename:join([ SitePrivDir, "config.d" ]) )
+            end
+    end.
+
+
+-spec read_configs( [ file:filename_all() ] ) -> {ok, map()} | {error, term()}.
+read_configs(Fs) when is_list(Fs) ->
+    lists:foldl(
+        fun
+            (_, {error, _} = Error) ->
+                Error;
+            (F, {ok, Acc}) ->
+                case z_config_files:consult(F) of
+                    {ok, Data} ->
+                        apps_config(F, Data, Acc);
+                    {error, _} = Error ->
+                        Error
+                end
+        end,
+        {ok, #{}},
+        Fs).
+
+apps_config(File, Data, Cfgs) when is_list(Data) ->
+    lists:foldl(
+        fun
+            (AppConfig, Acc) when is_map(AppConfig) ->
+                maps:fold(
+                    fun
+                        (Key, Cfg, {ok, MAcc}) ->
+                            KeyAtom = z_convert:to_atom(Key),
+                            {ok, MAcc#{ KeyAtom => Cfg }};
+                        (_Key, _Cfg, {error, _} = Error) ->
+                            Error
+                    end,
+                    {ok, Acc},
+                    AppConfig);
+            (AppConfig, Acc) when is_list(AppConfig) ->
+                lists:foldl(
+                    fun
+                        ({Key, Cfg}, {ok, MAcc}) ->
+                            KeyAtom = z_convert:to_atom(Key),
+                            {ok, MAcc#{ KeyAtom => Cfg }};
+                        (Other, {ok, _}) ->
+                            {error, {config_file, format, File, {unknown_term, Other}}};
+                        (_, {error, _} = Error) ->
+                            Error
+                    end,
+                    {ok, Acc},
+                    AppConfig)
+        end,
+        Cfgs,
+        Data);
+apps_config(File, _Data, _Cfgs) ->
+    error_logger:error_msg("Config file '~s' does not contain a list of configurations.",
+                           [File]),
+    {error, {config_file, missing_list_map, File, undefined}}.
--- apps/zotonic_launcher/src/zotonic_launcher_config.erl
+++ apps/zotonic_launcher/src/zotonic_launcher_config.erl
@@ -30,96 +30,20 @@
     erlang_config_files/1
 ]).
 
--include_lib("zotonic_core/include/zotonic_release.hrl").
--include_lib("yamerl/include/yamerl_errors.hrl").
-
-
 -spec config_dir() -> {ok, file:filename_all()} | {error, enoent}.
 config_dir() ->
     config_dir(node()).
 
 config_dir(Node) when is_atom(Node) ->
-    case os:getenv("ZOTONIC_CONFIG_DIR") of
-        false -> config_dir_init(Node);
-        "" -> config_dir_init(Node);
-        Dir ->
-            case filelib:is_dir(Dir) of
-                true -> {ok, Dir};
-                false -> {error, enoent}
-            end
-    end.
+    maybe_initialize( z_config_files:config_dir(Node) ).
 
-config_dir_init(Node) ->
-    case proplists:get_value(zotonic_config_dir, init:get_arguments()) of
-        undefined ->
-            config_dir_fs(Node);
-        [] ->
-            config_dir_fs(Node);
-        [ Dir ] ->
-            case filelib:is_dir(Dir) of
-                true -> {ok, Dir};
-                false -> {error, enoent}
-            end
-    end.
-
-config_dir_fs(Node) ->
-    Home = os:getenv("HOME"),
-    {MajorVersion, MinorVersion} = split_version(?ZOTONIC_VERSION),
-    HomeLocs = case Home of
-        false -> [];
-        "" -> [];
-        _ ->
-            [
-                filename:join([Home, ".zotonic", atom_to_list(Node)]),
-                filename:join([Home, ".zotonic", base_nodename(Node)]),
-                filename:join([Home, ".zotonic", ?ZOTONIC_VERSION]),
-                filename:join([Home, ".zotonic", MinorVersion]),
-                filename:join([Home, ".zotonic", MajorVersion]),
-                filename:join([Home, ".zotonic"])
-            ]
-    end,
-    EtcLocs = [
-        filename:join(["/etc/zotonic", atom_to_list(Node)]),
-        filename:join(["/etc/zotonic", base_nodename(Node)]),
-        filename:join(["/etc/zotonic", ?ZOTONIC_VERSION]),
-        filename:join(["/etc/zotonic", MinorVersion]),
-        filename:join(["/etc/zotonic", MajorVersion]),
-        filename:join(["/etc/zotonic"])
-    ],
-    Locations = HomeLocs ++ EtcLocs,
-    Found = lists:dropwhile(
-        fun(D) -> files(D, "zotonic*") =:= [] end,
-        Locations),
-    case Found of
-        [] when is_list(Home), Home =/= "" ->
-            Dir = filename:join([Home, ".zotonic", MajorVersion]),
-            ZotonicDir = filename:join([Home, ".zotonic"]),
-            case file:make_dir(ZotonicDir) of
-                ok -> file:change_mode(ZotonicDir, 8#00700);
-                {error, _} -> ok
-            end,
-            case file:make_dir(Dir) of
-                ok -> file:change_mode(Dir, 8#00700);
-                {error, _} -> ok
-            end,
-            case filelib:is_dir(Dir) of
-                true ->
-                    maybe_initialize_configs(Dir),
-                    {ok, Dir};
-                false ->
-                    {error, enoent}
-            end;
-        [] ->
-            {error, enoent};
-        [ "/etc/" ++ _ = Dir | _ ] ->
-            {ok, Dir};
-        [ Dir | _ ] ->
-            maybe_initialize_configs(Dir),
-            {ok, Dir}
-    end.
-
-base_nodename(Node) ->
-    lists:takewhile(fun(C) -> C =/= $@ end, atom_to_list(Node)).
+maybe_initialize({ok, "/etc/" ++ _} = Ok) ->
+    Ok;
+maybe_initialize({ok, Dir}) ->
+    maybe_initialize_configs(Dir),
+    {ok, Dir};
+maybe_initialize({error, _} = Error) ->
+    Error.
 
 -spec maybe_initialize_configs( file:filename_all() ) -> ok.
 maybe_initialize_configs(Dir) ->
@@ -172,8 +96,8 @@ replace_placeholders(Data) ->
 zotonic_config_files(Node) ->
     case config_dir(Node) of
         {ok, Dir} ->
-            Files = lists:filter(fun(F) -> not is_erlang_config(F) end, files(Dir)),
-            SubFiles= files( filename:join([ Dir, "config.d" ]) ),
+            Files = lists:filter(fun(F) -> not is_erlang_config(F) end, z_config_files:files(Dir)),
+            SubFiles= z_config_files:files( filename:join([ Dir, "config.d" ]) ),
             Files ++ SubFiles;
         {error, _} ->
             []
@@ -185,7 +109,7 @@ zotonic_config_files(Node) ->
 erlang_config_files(Node) ->
     case config_dir(Node) of
         {ok, Dir} ->
-            Files = files(Dir),
+            Files = z_config_files:files(Dir),
             lists:filter(fun(F) -> is_erlang_config(F) end, Files);
         {error, _} ->
             []
@@ -194,26 +118,12 @@ erlang_config_files(Node) ->
 config_files(Node) ->
     case config_dir(Node) of
         {ok, Dir} ->
-            Files = files(Dir),
-            SubFiles= files( filename:join([ Dir, "config.d" ]) ),
+            Files = z_config_files:files(Dir),
+            SubFiles= z_config_files:files( filename:join([ Dir, "config.d" ]) ),
             Files ++ SubFiles;
         {error, _} ->
             []
     end.
-
-files(Dir) ->
-    files(Dir, "*").
-
-files(Dir, Wildcard) ->
-    Files = filelib:wildcard( filename:join(Dir, Wildcard) ),
-    lists:filter(
-        fun(F) ->
-            case filename:basename(F) of
-                "." ++ _ -> false;
-                _ -> not filelib:is_dir(F)
-            end
-        end,
-        Files).
 
 is_erlang_config(F) ->
     case filename:basename(F) of
@@ -244,7 +154,7 @@ read_configs(Fs) when is_list(Fs) ->
             (_, {error, _} = Error) ->
                 Error;
             (F, {ok, Acc}) ->
-                case consult_config(F) of
+                case z_config_files:consult(F) of
                     {ok, Data} ->
                         apps_config(F, Data, Acc);
                     {error, _} = Error ->
@@ -305,64 +215,6 @@ app_config(File, App, Data, Acc) when is_list(Data), is_atom(App) ->
     Map = to_map(Data),
     app_config(File, App, Map, Acc).
 
-consult_config(File) ->
-    case filename:extension(File) of
-        ".config" ->
-            consult_erlang(File);
-        ".erlang" ->
-            consult_erlang(File);
-        ".yml" ->
-            consult_yaml(File);
-        ".yaml" ->
-            consult_yaml(File);
-        ".json" ->
-            consult_json(File);
-        _Other ->
-            {error, {config_file, unknown_format, File, undefined}}
-    end.
-
-consult_erlang(File) ->
-    case file:consult(File) of
-        {ok, L} when is_list(L); is_map(L) ->
-            {ok, L};
-        {error, Reason} when is_atom(Reason) ->
-            {error, {config_file, Reason, File, undefined}};
-        {error, Reason} ->
-            {error, {config_file, consult_error, File, Reason}}
-    end.
-
-consult_yaml(File) ->
-    try
-        io:format("~p~n~n", [File]),
-        Data = yamerl_constr:file(File, [ str_node_as_binary, {map_node_format, map} ]),
-        {ok, Data}
-    catch
-        throw:#yamerl_exception{} = E ->
-            {error, {config_file, yaml_format, File, E}}
-    end.
-
-consult_json(File) ->
-    case file:read_file(File) of
-        {ok, Data} ->
-            try
-                case jsxrecord:decode(Data) of
-                    {ok, Map} when is_map(Map) ->
-                        {ok, [ Map ]};
-                    {ok, [ Map | _ ] = List} when is_map(Map) ->
-                        {ok, List};
-                    {ok, _} ->
-                        {error, {config_file, no_list_or_map, File, undefined}};
-                    {error, _} = Error ->
-                        Error
-                end
-            catch
-                _:Reason ->
-                    {error, {config_file, json_format, File, Reason}}
-            end;
-        {error, Reason} ->
-            {error, {config_file, Reason, File, undefined}}
-    end.
-
 to_map(Data) when is_list(Data) ->
     to_map(Data, #{}).
 
@@ -380,7 +232,3 @@ to_map(Data, Map) ->
         end,
         Map,
         Data).
-
-split_version(Version) ->
-    {match, [Minor, Major]} = re:run(Version, "(([0-9]+)\\.[0-9]+)", [{capture, all_but_first, list}]),
-    {Major, Minor}.
