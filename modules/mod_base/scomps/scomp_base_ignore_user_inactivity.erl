f7a5a7a1028ca5d0e3f361ae480c639a9f271228
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2020-01-13 23:08:04 +0100

    Rename scomp in order to prevent confusion

--- modules/mod_base/scomps/scomp_base_ignore_user_inactivity.erl
+++ modules/mod_base/scomps/scomp_base_ignore_user_inactivity.erl
@@ -15,7 +15,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(scomp_base_ignore_inactivity).
+-module(scomp_base_ignore_user_inactivity).
 -behaviour(gen_scomp).
 
 -export([vary/2, render/3]).
