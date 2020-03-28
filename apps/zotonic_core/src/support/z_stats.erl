0561be1a12436b8893dd2e0e70e124e7732f4544
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2020-03-28 14:00:16 +0100

    Use bulk reporting
    (cherry picked from commit 8290c7c5da3d80d198cf0d4b03f79b0bb164c2ca)

--- apps/zotonic_core/src/support/z_stats.erl
+++ apps/zotonic_core/src/support/z_stats.erl
@@ -31,8 +31,14 @@
 %% @doc Initialize the statistics collection machinery.
 %%
 init() ->
+    create_vm_metrics(),
+    setup_mqtt_reporter(),
+
+
+
     ok.
 
+% @doc Setup stats for each site.
 init_site(Host) ->
     %% Cowmachine/HTTP metrics
     exometer:re_register([zotonic, Host, http, requests], counter, []),
@@ -53,40 +59,53 @@ init_site(Host) ->
     exometer:re_register([zotonic, Host, db, connection_wait], histogram, []),
 
     %% Session metrics
-    exometer:re_register([zotonic, Host, session, sessions], gauge, []),
-    exometer:re_register([zotonic, Host, session, page_processes], counter, []),
+    %% [TODO] add mqtt sessions
+    %% exometer:re_register([zotonic, Host, session, sessions], gauge, []),
+    %% exometer:re_register([zotonic, Host, session, page_processes], counter, []),
 
     ok.
 
 
-%% @doc Collect log data from webzmachine and update webzmachine metrics
+%% @doc Collect log data from cowmachine and update cowmachine metrics
 %%
 
 log_access(MetricsData) ->
-    Site = get_site(MetricsData),
-
     try
-        count_request(Site),
-        measure_duration(Site,
-                         duration(maps:get(req_start, MetricsData, undefined),
-                                  maps:get(resp_end, MetricsData, undefined))),
-        measure_data_out(Site, maps:get(resp_body_length, MetricsData))
+        handle_stats(MetricsData)
     after
         z_access_syslog:log_access(MetricsData)
     end.
 
 
-count_request(Site) when is_atom(Site) ->
-    exometer:update([zotonic, Site, webzmachine, requests], 1).
+handle_stats(MetricsData) ->
+    Site = get_site(MetricsData),
+    DispatchRule = get_dispatch_rule(MetricsData),
+    count_request(Site, DispatchRule),
+    measure_duration(Site, DispatchRule,
+                     duration(maps:get(req_start, MetricsData, undefined),
+                              maps:get(resp_end, MetricsData, undefined))),
+    measure_data_out(Site, DispatchRule,
+                     maps:get(resp_body_length, MetricsData)).
 
-measure_duration(Site, Duration) when is_integer(Duration) ->
-    exometer:update([zotonic, Site, webzmachine, duration], Duration);
-measure_duration(_Site, _) ->
+
+count_request(Site, DispatchRule) when is_atom(Site)
+                                       andalso is_atom(DispatchRule) ->
+    ok = exometer:update_or_create([zotonic, Site, cowmachine, DispatchRule, requests], 1, spiral, []);
+count_request(_Site, _DispatchRule) ->
     ok.
 
-measure_data_out(Site, DataOut) when is_integer(DataOut) ->
-    exometer:update([zotonic, Site, webzmachine, data_out], DataOut);
-measure_data_out(_Site, _DataOut) ->
+measure_duration(Site, DispatchRule, Duration) when is_atom(Site)
+                                                    andalso is_atom(DispatchRule)
+                                                    andalso is_integer(Duration) ->
+    ok = exometer:update_or_create([zotonic, Site, cowmachine, DispatchRule, duration], Duration, histogram, []);
+measure_duration(_Site, _Dispatch, _Duration) ->
+    ok.
+
+measure_data_out(Site, Dispatch, DataOut) when is_atom(Site)
+                                               andalso is_atom(Dispatch)
+                                               andalso is_integer(DataOut) ->
+    ok = exometer:update_or_create([zotonic, Site, cowmachine, Dispatch, data_out], DataOut, spiral, []);
+measure_data_out(_Site, _Dispatch, _DataOut) ->
     ok.
 
 % log_access(#wm_log_data{finish_time=undefined}=LogData) ->
@@ -113,10 +132,81 @@ count_db_event(Event, Context) when is_atom(Event) ->
 
 % @private Return the site name from the user-data
 get_site(#{user_data := #{site := Site}}) -> Site;
-get_site(#{}) -> undefined.
+get_site(#{}) -> '$unknown'.
+
+% @private Return the selected dispatch rule
+get_dispatch_rule(#{user_data := #{dispatch_rule := DispatchRule}}) -> DispatchRule;
+get_dispatch_rule(#{}) -> '$unknown'. 
+
 
 % @private Return the duration in microseconds.
 duration(undefined, _) -> undefined;
 duration(_, undefined) -> undefined;
 duration(Start, End) ->
     erlang:convert_time_unit(End-Start, native, microsecond).
+<<<<<<< HEAD
+=======
+
+
+% @private vm_stats
+create_vm_metrics() ->
+    ok = exometer:new([erlang, memory],
+                      {function, erlang, memory, ['$dp'], value,
+                       [total, processes, system, atom, binary, ets]}),
+
+    ok = exometer:new([erlang, system],
+                      {function, erlang, system_info, ['$dp'], value,
+                       [process_count, port_count]}),
+
+    ok = exometer:new([erlang, statistics],
+                      {function, erlang, statistics, ['$dp'], value,
+                       [run_queue]}),
+
+    ok = exometer:new([erlang, gc],
+                      {function, erlang, statistics, [garbage_collection],
+                       match, {total_coll, rec_wrd, '_'}}),
+
+    ok = exometer:new([erlang, io],
+                      {function, erlang, statistics, [io], match,
+                       {{'_', input}, {'_', output}}}).
+
+
+% @private Setup mqtt reporter
+setup_mqtt_reporter() ->
+    add_mqtt_reporter(),
+
+    [ ok = exometer_report:subscribe(z_exometer_mqtt,
+                                     {select,
+                                      [{ {[zotonic | '_'], DataPoint, enabled}, [], ['$_'] }]},
+                                     default, % datapoints(DataPoint),
+                                     10000)
+      || DataPoint <- datapoints()],
+    
+    ok = exometer_report:subscribe(z_exometer_mqtt,
+                                     {select,
+                                      [{ {[erlang | '_'], '_', enabled}, [], ['$_'] }]},
+                                     default,
+                                     10000),
+
+    ok.
+
+
+%
+add_mqtt_reporter() ->
+    case exometer_report:add_reporter(
+           z_exometer_mqtt, [{status, enabled}, {report_bulk, true}]) of
+        ok -> ok;
+        {error, already_running} -> ok
+    end.
+
+
+datapoints() ->
+    [counter, spiral, gauge, histogram, meter].
+
+datapoints(counter) ->[value];
+datapoints(spiral) -> [count, one];
+datapoints(gauge) -> [value];
+datapoints(histogram) -> [mean, min, max, 50, 95, 99, 999];
+datapoints(meter) -> [count, one, five, fifteen, day, mean].
+
+>>>>>>> 8290c7c5d... Use bulk reporting
