62310a11135d316eb6adbd794c181aeb4743dd24
Marc Worrell <marc@worrell.nl>
2020-05-08 12:50:56 +0200

    More type fixes.

--- apps/zotonic_mod_filestore/src/support/filestore_uploader.erl
+++ apps/zotonic_mod_filestore/src/support/filestore_uploader.erl
@@ -69,39 +69,46 @@ handle_call(Msg, _From, State) ->
     {reply, {error, unknown_msg}, State}.
 
 handle_cast(start, #state{id=Id, path=Path, context=Context, media_info=MInfo} = State) ->
-    MaybeEntry = m_filestore:lookup(Path, Context),
-    case m_filestore:is_upload_ok(MaybeEntry) of
-        true ->
-            RscId = maps:get(<<"id">>, MInfo, undefined),
-            case z_notifier:first(#filestore_credentials_lookup{id=RscId, path=Path}, Context) of
-                {ok, #filestore_credentials{} = Cred} ->
-                    case handle_upload(Path, Cred, Context) of
-                        ok ->
+    case m_filestore:lookup(Path, Context) of
+        {ok, Entry} ->
+            case m_filestore:is_upload_ok(Entry) of
+                true ->
+                    RscId = maps:get(<<"id">>, MInfo, undefined),
+                    case z_notifier:first(#filestore_credentials_lookup{id=RscId, path=Path}, Context) of
+                        {ok, #filestore_credentials{} = Cred} ->
+                            case handle_upload(Path, Cred, Context) of
+                                ok ->
+                                    m_filestore:dequeue(Id, Context),
+                                    {stop, normal, State};
+                                fatal ->
+                                    m_filestore:dequeue(Id, Context),
+                                    {stop, normal, State};
+                                retry ->
+                                    lager:info("Filestore upload of ~p, sleeping 30m for retry.", [Path]),
+                                    timer:send_after(?RETRY_DELAY, restart),
+                                    {noreply, State, hibernate}
+                            end;
+                        undefined ->
+                            lager:info("Filestore no credentials found for ~p", [Path]),
                             m_filestore:dequeue(Id, Context),
-                            {stop, normal, State};
-                        fatal ->
-                            m_filestore:dequeue(Id, Context),
-                            {stop, normal, State};
-                        retry ->
-                            lager:info("Filestore upload of ~p, sleeping 30m for retry.", [Path]),
-                            timer:send_after(?RETRY_DELAY, restart),
-                            {noreply, State, hibernate}
+                            {stop, normal, State}
                     end;
-                undefined ->
-                    lager:info("Filestore no credentials found for ~p", [Path]),
+                false ->
+                    case m_filestore:is_download_ok(Entry) of
+                        true ->
+                            % Already uploaded - we can safely delete the file.
+                            AbsPath = z_path:abspath(Path, Context),
+                            file:delete(AbsPath);
+                        false ->
+                            ok
+                    end,
                     m_filestore:dequeue(Id, Context),
                     {stop, normal, State}
             end;
-        false ->
-            case m_filestore:is_download_ok(MaybeEntry) of
-                true ->
-                    % Already uploaded - we can safely delete the file.
-                    AbsPath = z_path:abspath(Path, Context),
-                    file:delete(AbsPath);
-                false ->
-                    ok
-            end,
-            m_filestore:dequeue(Id, Context),
+        {error, enoent} ->
+            {stop, normal, State};
+        {error, _} = Error ->
+            lager:error("Filestore upload error reading for path ~p: ~p", [ Path, Error ]),
             {stop, normal, State}
     end;
 handle_cast(stop, State) ->
