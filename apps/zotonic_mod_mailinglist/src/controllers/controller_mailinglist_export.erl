5eb81171830bae45af684ef362ee58af1f735a8a
Marc Worrell <marc@worrell.nl>
2020-06-30 09:31:20 +0200

    Add name to mailinglist export.

--- apps/zotonic_mod_mailinglist/src/controllers/controller_mailinglist_export.erl
+++ apps/zotonic_mod_mailinglist/src/controllers/controller_mailinglist_export.erl
@@ -45,8 +45,22 @@ content_types_provided(Context) ->
 process(_Method, _AcceptedCT, _ProvidedCT, Context) ->
 	Id = m_rsc:rid(z_context:get_q(<<"id">>, Context), Context),
 	%% Fetch all exported email addresses
-	Recipients = m_mailinglist:get_enabled_recipients(Id, Context),
-	Export = lists:join([13,10], Recipients),
+	{ok, Recipients} = m_mailinglist:list_recipients(Id, Context),
+    Export = lists:filtermap(
+        fun
+            (#{<<"is_enabled">> := true } = R) ->
+                Line = [
+                    maps:get(<<"email">>, R), 9,
+                    maps:get(<<"name_first">>, R, <<>>), 9,
+                    maps:get(<<"name_surname">>, R, <<>>), 9,
+                    maps:get(<<"name_surname_prefix">>, R, <<>>),
+                    13, 10
+                ],
+                {true, Line};
+            (_) ->
+                false
+        end,
+        Recipients),
 	%% Set the content disposition filename
 	Filename = <<"mailinglist-", (z_string:to_slug(m_rsc:p(Id, title, Context)))/binary, ".csv">>,
 	Context1 = z_context:set_resp_header(
