70e7542b9780e86090254b1821dd1b926cd5faf2
Marc Worrell <marc@worrell.nl>
2021-02-15 17:24:50 +0100

    Add zotonic_dispatch_file, zotonic_dispatch_module to the known dispatcher args.

--- apps/zotonic_core/src/support/z_dispatcher.erl
+++ apps/zotonic_core/src/support/z_dispatcher.erl
@@ -70,6 +70,7 @@ dispatcher_args() ->
     [
         is_permanent, dispatch, q, qargs,
         zotonic_dispatch, ssl, protocol, session_id, set_session_id,
+        zotonic_dispatch_file, zotonic_dispatch_module,
         auth_options, auth_expires
     ].
 
