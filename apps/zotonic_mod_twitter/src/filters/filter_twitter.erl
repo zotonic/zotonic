63e63156d92800a06b440aa3530296a88661f911
Marc Worrell <marc@worrell.nl>
2021-04-23 21:48:27 +0200

    Fix a problem where the twitter importer could crash on illegal URLs.

--- apps/zotonic_mod_twitter/src/filters/filter_twitter.erl
+++ apps/zotonic_mod_twitter/src/filters/filter_twitter.erl
@@ -93,9 +93,13 @@ twitter1_url_html(Pre, Url, Opts) ->
                 true ->
                     [];
                 false ->
-                    Url2 = z_url:location(<<Pre/binary,Url/binary>>),
-                    Text = z_string:truncate(z_url:remove_protocol(Url2), ?URL_TRUNCATE),
-                    ["<a href=\"", Url2, "\">", Text, "</a>"]
+                    try
+                        Url2 = z_url:location(<<Pre/binary,Url/binary>>),
+                        Text = z_string:truncate(z_url:remove_protocol(Url2), ?URL_TRUNCATE),
+                        ["<a href=\"", Url2, "\">", Text, "</a>"]
+                    catch
+                        _:_ -> []
+                    end
             end;
         false ->
             ["<a href=\"", Pre, Url, "\">", Url, "</a>"]
