16c5308c31604bfd66fd2c274318e9fdc9249b15
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2021-07-08 16:47:30 +0200

    Cleanup cotonic fetch script

--- apps/zotonic_mod_base/cotonic-fetch.sh
+++ apps/zotonic_mod_base/cotonic-fetch.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
 function install {
-    base=https://raw.githubusercontent.com/cotonic/cotonic/
+    version=$1
+    base=https://raw.githubusercontent.com/cotonic/cotonic
 
     if [ -d priv/lib-src/cotonic/$1 ]; then
-        echo " - cotonic $1 already installed"
+        echo " - cotonic ${version} already installed"
     else
-        echo " - Downloading cotonic $1 from github"
+        echo " - Downloading cotonic ${version} from github"
 
-        mkdir -p priv/lib-src/cotonic/$1;
-        cd priv/lib-src/cotonic/$1;
+        mkdir -p priv/lib-src/cotonic/${version};
+        cd priv/lib-src/cotonic/${version};
 
-        curl -#O ${base}/$1/cotonic.js
-        curl -#O ${base}/$1/cotonic-worker.js
-        curl -#O ${base}/$1/cotonic-service-worker.js
+        curl -#O ${base}/${version}/cotonic.js
+        curl -#O ${base}/${version}/cotonic-worker.js
+        curl -#O ${base}/${version}/cotonic-service-worker.js
 
         cd ../../../..
     fi
 
     mkdir -p priv/lib/cotonic;
 
-    cp priv/lib-src/cotonic/$1/cotonic.js priv/lib/cotonic/cotonic.js
-    cp priv/lib-src/cotonic/$1/cotonic-worker.js priv/lib/cotonic/cotonic-worker.js
-    cp priv/lib-src/cotonic/$1/cotonic-service-worker.js priv/lib/cotonic/cotonic-service-worker.js
+    cp priv/lib-src/cotonic/${version}/cotonic.js priv/lib/cotonic/cotonic.js
+    cp priv/lib-src/cotonic/${version}/cotonic-worker.js priv/lib/cotonic/cotonic-worker.js
+    cp priv/lib-src/cotonic/${version}/cotonic-service-worker.js priv/lib/cotonic/cotonic-service-worker.js
 }
 
 install "1.0.6" 
