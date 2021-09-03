c16664604ae295cfccea362c12de06d432362927
Marc Worrell <marc@worrell.nl>
2021-09-03 19:03:12 +0200

    Copy the zotonic_core/_build to every depending app before compilation. This greatly speeds up publishing the Hex packages.

--- bin/hex-publish.sh
+++ bin/hex-publish.sh
@@ -4,17 +4,13 @@
 # Run ./prepare-release.sh before running this script.
 # This script should be running after tagging the master branch with the new version.
 
+pushd apps;
 
 # First publish core dependencies for other apps
 
-APPS="zotonic_notifier zotonic_core \
-      zotonic_filewatcher zotonic_filehandler zotonic_fileindexer \
-      zotonic_listen_http zotonic_listen_smtp zotonic_listen_mqtt \
-      zotonic_launcher"
+APPS1="zotonic_notifier zotonic_core"
 
-pushd apps;
-
-for i in $APPS
+for i in $APPS1
 do
     pushd $i
 
@@ -25,6 +21,29 @@ do
 
     popd
 done
+
+APPS2="zotonic_filewatcher zotonic_filehandler zotonic_fileindexer \
+      zotonic_listen_http zotonic_listen_smtp zotonic_listen_mqtt \
+      zotonic_launcher"
+
+for i in $APPS2
+do
+    pushd $i
+
+    # Take zotonic_core as the basis for the build, this prevents
+    # fetching and recompiling all dependencies of zotonic_core for
+    # every module
+    rm -rf _build
+    cp -r ../zotonic_core/_build .
+
+    ../../rebar3 compile
+    ../../rebar3 edoc
+
+    # ../../rebar3 publish
+
+    popd
+done
+
 
 # Publish all remaining apps - skip core dependencies
 
@@ -52,6 +71,12 @@ do
 
         *)
             pushd $i
+
+            # Take zotonic_core as the basis for the build, this prevents
+            # fetching and recompiling all dependencies of zotonic_core for
+            # every module
+            rm -rf _build
+            cp -r ../zotonic_core/_build .
 
             ../../rebar3 compile
             ../../rebar3 edoc
