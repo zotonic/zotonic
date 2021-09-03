861b884018361fa9efd3550717d0c1d11754231b
Marc Worrell <marc@worrell.nl>
2021-09-03 14:38:01 +0200

    Start with hex publish script

---
+++ hex-publish.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Publish all apps as Hex packages.
+# Run ./prepare-release.sh before running this script.
+# This script should be running after tagging the master branch with the new version.
+
+
+# First publish core dependencies for other apps
+
+APPS="zotonic_notifier zotonic_core \
+      zotonic_filewatcher zotonic_filehandler zotonic_fileindexer \
+      zotonic_listen_http zotonic_listen_smtp zotonic_listen_mqtt \
+      zotonic_launcher"
+
+cd apps;
+
+for i in $APPS
+do
+    pushd $i
+
+    ../../rebar3 compile
+    ../../rebar3 edoc
+
+    popd
+done
+
+# Publish all remaining apps
+
+for i in *
+do
+    case $i in
+        zotonic_notifier)
+            ;;
+        zotonic_core)
+            ;;
+        zotonic_filewatcher)
+            ;;
+        zotonic_filehandler)
+            ;;
+        zotonic_fileindexer)
+            ;;
+        zotonic_listen_http)
+            ;;
+        zotonic_listen_smtp)
+            ;;
+        zotonic_listen_mqtt)
+            ;;
+        zotonic_launcher)
+            ;;
+
+        *)
+            echo $i
+            ;;
+    esac
+done
+
+# Cleanup
+rm -rf apps/*/_build
+
