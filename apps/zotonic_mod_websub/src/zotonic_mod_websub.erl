4ade37a4083f3d045a0e3d4a8461845dcf0030fd
Marc Worrell <marc@worrell.nl>
2021-10-04 14:26:57 +0200

    WIP: websub support

---
+++ apps/zotonic_mod_websub/src/zotonic_mod_websub.erl
@@ -0,0 +1,28 @@
+%% @doc Publish and subscribe to resources between sites using WebSub
+%% @copyright 2021 Marc Worrell
+%% @author Marc Worrell <marc@worrell.nl>
+
+%% Copyright 2021 Marc Worrell
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(zotonic_mod_websub).
+-author("Marc Worrell <marc@worrell.nl>").
+
+-mod_title("Resource PubSub").
+-mod_description("Publish and subscribe to resources between sites using WebSub.").
+
+-include_lib("zotonic_core/include/zotonic.hrl").
+
+-export([
+]).
