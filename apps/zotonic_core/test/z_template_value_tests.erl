80aef8c1b23b639dd69b5151f8c682238a192c46
Marc Worrell <marc@worrell.nl>
2021-10-27 16:46:55 +0200

    Fix test, name of 1 is administrator

--- apps/zotonic_core/test/z_template_value_tests.erl
+++ apps/zotonic_core/test/z_template_value_tests.erl
@@ -10,20 +10,20 @@
 value_lookup_test() ->
     Context = z_context:new(zotonic_site_testsandbox),
     ?assertEqual(
-        <<"admin">>,
+        <<"administrator">>,
         z_template_compiler_runtime:find_nested_value([ <<"a">>, <<"atlas:post">>, <<"name">> ], vars(), Context)),
     ?assertEqual(
-        <<"admin">>,
+        <<"administrator">>,
         z_template_compiler_runtime:find_nested_value([ <<"a">>, <<"atlas:post">>, 1, <<"name">> ], vars(), Context)),
     ?assertEqual(
         undefined,
         z_template_compiler_runtime:find_nested_value([ <<"a">>, <<"atlas:post">>, 2, <<"name">> ], vars(), Context)),
 
     ?assertEqual(
-        <<"admin">>,
+        <<"administrator">>,
         z_template_compiler_runtime:find_nested_value([ <<"b">>, <<"atlas:post">>, <<"name">> ], vars(), Context)),
     ?assertEqual(
-        <<"admin">>,
+        <<"administrator">>,
         z_template_compiler_runtime:find_nested_value([ <<"b">>, <<"atlas:post">>, 1, <<"name">> ], vars(), Context)),
     ?assertEqual(
         undefined,
