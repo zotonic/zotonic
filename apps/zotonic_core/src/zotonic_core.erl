f3d4dcf89998a3b9046c9e9c7563bfcc8e69c3de
Marc Worrell <marc@worrell.nl>
2022-01-31 12:28:29 +0100

    Include logstasher

--- apps/zotonic_core/src/zotonic_core.erl
+++ apps/zotonic_core/src/zotonic_core.erl
@@ -56,6 +56,7 @@ is_app_available(App) ->
 -spec setup( node() ) -> ok.
 setup(Node) ->
     io:setopts([{encoding, unicode}]),
+    maybe_start_logstasher(),
     assert_schedulers( erlang:system_info(schedulers) ),
     load_applications(),
     set_configs(),
@@ -81,26 +82,20 @@ set_configs() ->
     application:set_env(filezcache, data_dir, filename:join([ FileZCache, "data" ])),
     application:set_env(filezcache, journal_dir, filename:join([ FileZCache, "journal" ])).
 
-% set_logger_dir() ->
-%     io:format("LOGGER: ~p~n", [application:get_env(zotonic_core, logger)]),
-%     LogDir = z_config:get(log_dir),
-%     lists:map(
-%         fun(#{ config := Config, id := HandlerId }) ->
-%             case Config of
-%                 #{ file := File } when is_list(File) ->
-%                     io:format("SET(~p) to ~p~n", [ HandlerId, filename:join(LogDir, filename:basename(File)) ]),
-%                     logger:update_handler_config(
-%                         HandlerId,
-%                         file,
-%                         filename:join(LogDir, filename:basename(File)));
-%                 #{} ->
-%                     ok
-%             end
-%         end,
-%         logger:get_handler_config()),
-%     logger:i(),
-%     ok.
+maybe_start_logstasher() ->
+    IsLogstasherNeeded = lists:any(
+        fun(#{ module := Module }) ->
+            Module =:= logstasher_h
+        end,
+        logger:get_handler_config()),
+    case IsLogstasherNeeded of
+        true ->
+            application:ensure_all_started(logstasher);
+        false ->
+            ok
+    end.
 
+%% @doc Refuse to start if only 1 scheduler active. Bcrypt needs separate schedulers.
 assert_schedulers(1) ->
     io:format("FATAL: Not enough schedulers, please start with 2 or more schedulers.~nUse: ERLOPTS=\"+S 4:4\" ./bin/zotonic debug~n~n"),
     erlang:halt();
