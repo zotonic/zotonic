e2bc77d45a79ad60041166adb57237f754e312e1
williamthome <willegp88@gmail.com>
2022-05-16 12:53:20 -0300

    skel: Remove comments and format SITE_sup.erl files

--- apps/zotonic_mod_zotonic_site_management/priv/skel/nodb/src/SITE_sup.erl
+++ apps/zotonic_mod_zotonic_site_management/priv/skel/nodb/src/SITE_sup.erl
@@ -20,15 +20,12 @@
 
 -behaviour(supervisor).
 
-%% API functions
--export([ start_link/0 ]).
-
-%% Supervisor callbacks
--export([ init/1 ]).
+-export([ start_link/0, init/1 ]).
 
 -define(SERVER, ?MODULE).
 
-start_link() -> supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
     SupFlags = #{
