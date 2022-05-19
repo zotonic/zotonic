226f58012eff04644699857bae15654bbd9e66f4
williamthome <willegp88@gmail.com>
2022-05-19 01:46:05 -0300

    scripts: Improve Chrome cmd usage

--- apps/zotonic_launcher/src/command/zotonic_cmd_chrome.erl
+++ apps/zotonic_launcher/src/command/zotonic_cmd_chrome.erl
@@ -40,12 +40,19 @@ usage() ->
 
 usage(Browser) ->
     io:format("USAGE: zotonic " ++ Browser ++ " [options] [switches] <site_name> ~n"),
+    io:format("~n"),
     io:format("Options: ~n"),
     io:format("  -s <secure>   Opens site as secure and ignore certificate errors ~n"),
+    io:format("~n"),
     io:format("Switches: ~n"),
-    io:format("  See https://peter.sh/experiments/chromium-command-line-switches/ ~n"),
-    io:format("Note: ~n"),
-    io:format("  Requires 'zotonic start' ~n~n").
+    io:format("  --incognito            Launches " ++ Browser ++ " directly in Incognito private browsing mode ~n"),
+    io:format("  --purge-memory-button  Add purge memory button to " ++ Browser ++ " ~n"),
+    io:format("  --multi-profiles       Enable multiple profiles in " ++ Browser ++ " ~n"),
+    io:format("  * See a list of switches here https://peter.sh/experiments/chromium-command-line-switches/ ~n"),
+    io:format("~n"),
+    io:format("Notes: ~n"),
+    io:format("  Requires Zotonic running (use the command 'zotonic start') ~n"),
+    io:format("~n").
 
 run(Args) ->
     run(chrome, Args).
