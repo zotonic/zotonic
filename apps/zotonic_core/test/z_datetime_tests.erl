8d71f7b1c5c3123c97ab8f442a98c896f8a8d7a2
Marc Worrell <marc@worrell.nl>
2022-06-13 15:00:41 +0200

    core: add z_datetime:next_month/2 and z_datetime:prev_month/2. (#3013)
    * core: add z_datetime:next_month/2 and z_datetime:prev_month/2.
    
    This fixes an issue where adding (or substracting)  multiple months to a date could shift the day.

--- apps/zotonic_core/test/z_datetime_tests.erl
+++ apps/zotonic_core/test/z_datetime_tests.erl
@@ -4,7 +4,6 @@
 -module(z_datetime_tests).
 
 -include_lib("eunit/include/eunit.hrl").
--include_lib("zotonic.hrl").
 
 timesince_simple_test() ->
     C = z_context:new(zotonic_site_testsandbox, en),
@@ -29,3 +28,13 @@ timesince_user_specified_texts_test() ->
     ?assertEqual(<<"10 hours">>, iolist_to_binary(z_datetime:timesince({{2010,9,27},{10,13,35}}, {{2010,9,27},{20,13,45}}, " ,bar,baz", C))),
     ?assertEqual(<<"bar">>, iolist_to_binary(z_datetime:timesince({{2010,9,27},{10,13,35}}, {{2010,9,27},{10,13,35}}, " ,bar", C))),
     ?assertEqual(<<"moments foo">>, iolist_to_binary(z_datetime:timesince({{2010,9,27},{10,13,35}}, {{2010,9,27},{10,13,37}}, "foo,bar,baz", C))).
+
+time_add_month_test() ->
+    ?assertEqual({{2021,12,1},{0,0,0}}, z_datetime:next_month({{2022,1,1},{0,0,0}}, -1)),
+    ?assertEqual({{2022,2,1},{0,0,0}},  z_datetime:next_month({{2022,1,1},{0,0,0}}, 1)),
+    ?assertEqual({{2022,2,28},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 1)),
+    ?assertEqual({{2022,3,31},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 2)),
+    ?assertEqual({{2022,4,30},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 3)),
+    ?assertEqual({{2023,1,31},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 12)),
+    ?assertEqual({{2023,2,28},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 13)),
+    ?assertEqual({{2024,2,29},{0,0,0}}, z_datetime:next_month({{2022,1,31},{0,0,0}}, 25)).
