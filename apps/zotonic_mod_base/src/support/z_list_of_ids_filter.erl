6fd5a860ceb387c5ad9f9755cf6dbe403f662b05
Marc Worrell <marc@worrell.nl>
2022-07-28 17:28:33 +0200

    mod_base: fix spec of z_list_of_ids_filter:filter functions. (#3073)

--- apps/zotonic_mod_base/src/support/z_list_of_ids_filter.erl
+++ apps/zotonic_mod_base/src/support/z_list_of_ids_filter.erl
@@ -1,8 +1,8 @@
 %% @author Paul Guyot <pguyot@kallisys.net>
-%% @copyright 2011 Paul Guyot
+%% @copyright 2011-2022 Paul Guyot
 %% @doc common code for is_* filters that work on lists of ids.
 
-%% Copyright 2011 Paul Guyot
+%% Copyright 2011-2022 Paul Guyot
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -19,25 +19,31 @@
 -module(z_list_of_ids_filter).
 -export([filter/3, filter/4]).
 
--spec filter(integer() | [integer()], fun((integer()) -> boolean()), any()) -> boolean() | [integer()].
-filter(Id, Fun, _Context) when is_integer(Id) ->
-    Fun(Id);
-filter(Id, Fun, _Context) when is_binary(Id) ->
+-spec filter(IdOrIds, TestFun, Context) -> BoolOrIds when
+    IdOrIds :: m_rsc:resource() | list( m_rsc:resource() ) | term(),
+    TestFun :: fun((m_rsc:resource()) -> boolean()),
+    Context :: z:context(),
+    BoolOrIds :: boolean() | list( m_rsc:resource() ).
+filter(Id, Fun, _Context) when is_integer(Id); is_binary(Id); is_map(Id) ->
     Fun(Id);
 filter(List, Fun, Context) ->
     lists:filter(Fun, z_template_compiler_runtime:to_list(List, Context)).
 
--spec filter([integer()], fun((integer()) -> boolean()), integer(), any()) -> [integer()].
+-spec filter(Ids, TestFun, Count, Context) -> FilteredIds when
+    Ids :: list( m_rsc:resource() ) | term(),
+    TestFun :: fun((m_rsc:resource()) -> boolean()),
+    Count :: integer(),
+    Context :: z:context(),
+    FilteredIds :: list( m_rsc:resource() ).
 filter(List, Fun, N, Context) ->
     filter1(z_template_compiler_runtime:to_list(List, Context), Fun, N, []).
 
 filter1([], _Fun, _N, Acc) ->
     lists:reverse(Acc);
-filter1(_List, _Fun, 0, Acc) ->
+filter1(_List, _Fun, N, Acc) when N =< 0 ->
     lists:reverse(Acc);
 filter1([Id|Rest], Fun, N, Acc) ->
     case Fun(Id) of
         true -> filter1(Rest, Fun, N-1, [Id|Acc]);
         false -> filter1(Rest, Fun, N, Acc)
     end.
-
