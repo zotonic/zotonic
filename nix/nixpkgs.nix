ea183834ae19012949af51d0a3ad2bf0763b488f
Colin de Roos <colin@driebit.nl>
2023-01-13 09:46:38 +0100

    Update Nix package set

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 import
   (builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
-    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+    url = "https://github.com/NixOS/nixpkgs/archive/219b2fc946a59851cdfc59efdd11129e1cee2b44.zip"; # Most recent 22.11 commit as of 5 Dec 2022 16:18
+    sha256 = "sha256:05sgy7bdbqyyin07vzbx8fnc4mjw5780qf0r1ia8188ss2vxw4yr";
   })
 { }
