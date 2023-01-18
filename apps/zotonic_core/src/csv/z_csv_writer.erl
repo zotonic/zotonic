8012063c9f77ebcc94f6d7c60ceb242ad6b63f8b
Marc Worrell <marc@worrell.nl>
2023-01-18 20:29:06 +0100

    core: use type file:filename_all in z_csv_writer

--- apps/zotonic_core/src/csv/z_csv_writer.erl
+++ apps/zotonic_core/src/csv/z_csv_writer.erl
@@ -27,18 +27,18 @@
     encode_line/2
     ]).
 
--spec sanitize( file:filename(), file:filename() ) -> ok.
+-spec sanitize( file:filename_all(), file:filename() ) -> ok.
 sanitize(InFile, OutFile) ->
     {ok, _Hs, Sep} = z_csv_parser:inspect_file(InFile),
     Lines = z_csv_parser:scan_lines(InFile, Sep),
     ok = z_csv_writer:write_file(OutFile, Lines, Sep).
 
 
--spec write_file( file:filename(), z_csv_parser:lines() ) -> ok | {error, term()}.
+-spec write_file( file:filename_all(), z_csv_parser:lines() ) -> ok | {error, term()}.
 write_file(Filename, Data) ->
     write_file(Filename, Data, $,).
 
--spec write_file( file:filename(), z_csv_parser:lines(), z_csv_parser:sep() ) -> ok | {error, term()}.
+-spec write_file( file:filename_all(), z_csv_parser:lines(), z_csv_parser:sep() ) -> ok | {error, term()}.
 write_file(Filename, Data, Sep) when is_list(Data) ->
     case file:open(Filename, [write, binary]) of
         {ok, Device} ->
