0ce2229941dec7411c4e3f81e6c900cf58e17731
Marc Worrell <marc@worrell.nl>
2023-02-22 16:36:35 +0100

    mod_mailinglist: add page where subscribers can subscribe from mailinglists

--- apps/zotonic_mod_mailinglist/src/support/z_mailinglist_recipients.erl
+++ apps/zotonic_mod_mailinglist/src/support/z_mailinglist_recipients.erl
@@ -1,8 +1,8 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2020 Marc Worrell <marc@worrell.nl>
+%% @copyright 2020-2023 Marc Worrell <marc@worrell.nl>
 %% @doc Fetch all recipients for a mailinglist.
 
-%% Copyright 2020 Marc Worrell
+%% Copyright 2020-2023 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@
 -module(z_mailinglist_recipients).
 
 -export([
+    recipient_key_encode/2,
+    recipient_key_decode/2,
+
     count_recipients/2,
     list_recipients/2
     ]).
@@ -26,6 +29,66 @@
 -include_lib("zotonic_core/include/zotonic.hrl").
 
 -define(MAX_ROWS, 100000).
+-define(RECIPIENT_SECRET_LENGTH, 32).
+
+%% @doc Encode a recipient to a key used for the manage subscriptions page.
+-spec recipient_key_encode(Recipient, Context) -> {ok, Key} when
+    Recipient :: Email | RscId,
+    RscId :: m_rsc:resource_id(),
+    Email :: binary(),
+    Context :: z:context(),
+    Key :: binary().
+recipient_key_encode(Recipient, Context) ->
+    Term = {r0, Recipient, z_datetime:timestamp()},
+    {ok, termit:encode_base64(Term, recipient_secret(Context))}.
+
+%% @doc Decode a recipient to a key used for the manage subscriptions page.
+-spec recipient_key_decode(Key, Context) -> {ok, Recipient} | {error, Reason} when
+    Key :: binary(),
+    Context :: z:context(),
+    Recipient :: Email | RscId,
+    RscId :: m_rsc:resource_id(),
+    Email :: binary(),
+    Reason :: expired | term().
+recipient_key_decode(Key, Context) ->
+    case termit:decode_base64(Key, recipient_secret(Context)) of
+        {ok, {r0, Recipient, Timestamp}} when is_integer(Timestamp) ->
+            % TODO: check timestamp
+            {ok, Recipient};
+        {ok, Decoded} ->
+            ?LOG_ERROR(#{
+                in => mod_mailinglist,
+                text => <<"Mailinglist recipient key with unknown decoded value">>,
+                decoded => Decoded,
+                result => error,
+                reason => payload
+            }),
+            {error, payload};
+        {error, Reason} ->
+            ?LOG_ERROR(#{
+                in => mod_mailinglist,
+                text => <<"Mailinglist recipient key could not be decoded">>,
+                result => error,
+                reason => Reason,
+                key => Key
+            }),
+            {error, Reason}
+    end.
+
+-spec recipient_secret( z:context() ) -> binary().
+recipient_secret(Context) ->
+    case m_config:get_value(mod_mailinglist, recipient_secret, Context) of
+        <<>> -> generate_recipient_secret(Context);
+        undefined -> generate_recipient_secret(Context);
+        Secret -> Secret
+    end.
+
+-spec generate_recipient_secret( z:context() ) -> binary().
+generate_recipient_secret(Context) ->
+    Secret = z_ids:id(?RECIPIENT_SECRET_LENGTH),
+    m_config:set_value(mod_mailinglist, recipient_secret, Secret, Context),
+    Secret.
+
 
 -spec count_recipients( m_rsc:resource(), z:context() ) -> map().
 count_recipients(ListId, Context) ->
@@ -78,10 +141,10 @@ list_recipients(List, Context) ->
         fun(Id, Acc) ->
             case z_acl:rsc_visible(Id, Context)
                 andalso m_rsc:p(Id, is_published_date, Context)
-                andalso not z_convert:to_bool( m_rsc:p_no_acl(Id, is_mailing_opt_out, Context) )
+                andalso not z_convert:to_bool( m_rsc:p_no_acl(Id, <<"is_mailing_opt_out">>, Context) )
             of
                 true ->
-                    Email = m_rsc:p_no_acl(Id, email_raw, Context),
+                    Email = m_rsc:p_no_acl(Id, <<"email_raw">>, Context),
                     Acc#{
                         Email => Id
                     };
