8675455283b05a07817fb935072eb4a5d7a3cada
Marc Worrell <marc@worrell.nl>
2023-03-07 22:09:36 +0100

    mod_base: add option to listfilter for sorting the filtered list (#3317)
    * mod_base: add option to listfilter for sorting the filtered list
    
    * Add hideEmpty option

--- apps/zotonic_mod_base/priv/lib/js/modules/z.listfilter.js
+++ apps/zotonic_mod_base/priv/lib/js/modules/z.listfilter.js
@@ -1,17 +1,17 @@
 /* listfilter js
 ----------------------------------------------------------
 
-@package:	Zotonic 2009	
-@Author: 	Tim Benniks <tim@timbenniks.nl>
+@package: Zotonic 2009
+@Author:  Tim Benniks <tim@timbenniks.nl>
 
-Copyright 2009 Tim Benniks
+Copyright 2009-2023 Tim Benniks
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
- 
+
 http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,104 +28,108 @@ A port of the Quicksilver string ranking algorithm
 
 ---------------------------------------------------------- */
 
-String.prototype.score = function(abbreviation,offset) {
+String.prototype.score = function(abbreviation, offset) {
   offset = offset || 0 // TODO: I think this is unused... remove
- 
+
   if(abbreviation.length == 0) return 0.9
   if(abbreviation.length > this.length) return 0.0
 
-  for (var i = abbreviation.length; i > 0; i--) {
-	var sub_abbreviation = abbreviation.substring(0,i)
-	var index = this.indexOf(sub_abbreviation)
+  for (let i = abbreviation.length; i > 0; i--) {
+    const sub_abbreviation = abbreviation.substring(0,i)
+    const index = this.indexOf(sub_abbreviation)
 
+    if (index < 0) continue;
+    if (index + abbreviation.length > this.length + offset) continue;
 
-	if(index < 0) continue;
-	if(index + abbreviation.length > this.length + offset) continue;
+    const next_string     = this.substring(index+sub_abbreviation.length);
+    let next_abbreviation = null;
 
-	var next_string		  = this.substring(index+sub_abbreviation.length)
-	var next_abbreviation = null
+    if(i >= abbreviation.length) {
+      next_abbreviation = '';
+    } else {
+      next_abbreviation = abbreviation.substring(i);
+    }
 
-	if(i >= abbreviation.length)
-	  next_abbreviation = ''
-	else
-	  next_abbreviation = abbreviation.substring(i)
- 
-	var remaining_score	  = next_string.score(next_abbreviation,offset+index)
- 
-	if (remaining_score > 0) {
-	  var score = this.length-next_string.length;
+    const remaining_score   = next_string.score(next_abbreviation,offset+index)
 
-	  if(index != 0) {
-		var j = 0;
+    if (remaining_score > 0) {
+      let score = this.length-next_string.length;
 
-		var c = this.charCodeAt(index-1)
-		if(c==32 || c == 9) {
-		  for(var j=(index-2); j >= 0; j--) {
-			c = this.charCodeAt(j)
-			score -= ((c == 32 || c == 9) ? 1 : 0.15)
-		  }
-		} else {
-		  score -= index
-		}
-	  }
-   
-	  score += remaining_score * next_string.length
-	  score /= this.length;
-	  return score
-	}
+      if (index != 0) {
+        let c = this.charCodeAt(index-1)
+
+        if (c == 32 || c == 9) {
+          for (let j=(index-2); j >= 0; j--) {
+            c = this.charCodeAt(j)
+            score -= ((c == 32 || c == 9) ? 1 : 0.15)
+          }
+        } else {
+          score -= index
+        }
+      }
+      score += remaining_score * next_string.length
+      score /= this.length;
+      return score
+    }
   }
   return 0.0
 }
 
 jQuery.fn.listfilter = function(options)
 {
-	var list = jQuery(options.list);
+  let list = jQuery(options.list);
+  let sortlist = options.sort || false;
+  let hideEmpty = options.hideEmpty || false;
 
-	if(list.length) 
-	{
-		var rows = list.children()
-		var cache = rows.map(function()
-		{
-		    if (typeof options.text == 'string') {
-			    return $(options.text, this).text().toLowerCase();
-		    } else {
-			    return $(this).text().toLowerCase();
-			}
-		});
-			
-		this.keyup(filter).keyup().parents('form').submit(function()
-		{
-			return false;
-		});
-	}
-		
-	return this;
-		
-	function filter()
-	{
-		var term = jQuery.trim(jQuery(this).val().toLowerCase()), scores = [];
-		
-		if(!term) 
-		{
-			rows.show();
-		}
-		else
-		{
-			rows.hide();
+  if(list.length)
+  {
+    var rows = list.children()
+    var cache = rows.map(function() {
+        if (typeof options.text == 'string') {
+          return $(options.text, this).text().toLowerCase();
+        } else {
+          return $(this).text().toLowerCase();
+        }
+    });
 
-			cache.each(function(i)
-			{
-				var score = this.score(term);
-				if(score > 0)
-				{
-					scores.push([score, i]); 
-				}
-			});
+    this.keyup(filter).keyup().parents('form').submit(function() {
+      return false;
+    });
+  }
 
-			jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function()
-			{
-				jQuery(rows[ this[1] ]).show();
-			});
-		}
-	}
+  return this;
+
+  function filter()
+  {
+    var term = jQuery.trim(jQuery(this).val().toLowerCase()), scores = [];
+
+    if (!term) {
+      if (hideEmpty) {
+        rows.hide();
+      } else {
+        if (sortlist) {
+          jQuery.each(rows, function() {
+            list.append(rows[ this[1] ]);
+          });
+        }
+        rows.show();
+      }
+    } else {
+      rows.hide();
+
+      cache.each(function(i) {
+        var score = this.score(term);
+        if(score > 0) {
+          scores.push([score, i]);
+        }
+      });
+
+      jQuery.each(scores.sort(function(a, b){return a[0] - b[0];}), function() {
+          if (sortlist) {
+            list.prepend(rows[ this[1] ]);
+          }
+          jQuery(rows[ this[1] ]).show();
+      });
+    }
+  }
 };
