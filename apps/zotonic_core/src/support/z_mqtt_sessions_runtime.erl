9fa81b2d9c03af7c4212580fb74a1f3c049fcf7e
HeiHeShang <strangerfuture@mail.ru>
2023-06-18 11:34:22 +0900

    format check hostname:user@domain.com

--- apps/zotonic_core/src/support/z_mqtt_sessions_runtime.erl
+++ apps/zotonic_core/src/support/z_mqtt_sessions_runtime.erl
@@ -463,9 +463,9 @@ split_vhost_username(Username) ->
     Delimetr = [<<X>> || <<X>> <= Username, <<X>> == ?COLON orelse <<X>> == ?AT],
     match(Delimetr, Username).
 
-match([?COLON], Username) ->
+match([?COLON |_], Username) ->
     match_colon(Username);
-match([?AT], Username) ->
+match([?AT |_], Username) ->
     match_at(Username);
 match([], _) ->
     undefined.
