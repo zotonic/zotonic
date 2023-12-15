3607d42be0e5cbf00d80380e3530c5f8e7414373
Rob van den Bogaard <rob@driebit.nl>
2023-12-15 13:03:07 +0100

    [core] Accept self as binary or atom in m_rsc_update (#3641)
    * [core] Self is passed as a binary instead of an atom now
    
    * [core] Make handling of self backwards compatible
    
    * Accept self as binary and atom
    
    ---------
    
    Co-authored-by: Marc Worrell <marc@worrell.nl>

--- apps/zotonic_core/src/models/m_rsc_update.erl
+++ apps/zotonic_core/src/models/m_rsc_update.erl
@@ -802,7 +802,7 @@ update_transaction_fun_insert(#rscupd{id = insert_rsc} = RscUpd, Props, _Raw, Up
     % Create dummy resource with correct creator, category and content group.
     % This resource will be updated with the other properties.
     InsertId = case maps:get(<<"creator_id">>, UpdateProps, undefined) of
-                   self ->
+                   Self when Self =:= <<"self">>; Self =:= self ->
                         {ok, InsId} = z_db:insert(
                             rsc,
                             InsProps#{ <<"creator_id">> => undefined },
@@ -859,7 +859,7 @@ update_transaction_fun_insert(#rscupd{id = Id} = RscUpd, Props, Raw, UpdateProps
     Props2 = case z_acl:is_admin(Context) of
                  true ->
                      case maps:get(<<"creator_id">>, UpdateProps, undefined) of
-                         <<"self">> ->
+                         Self when Self =:= <<"self">>; Self =:= self ->
                              Props#{ <<"creator_id">> => Id };
                          CreatorId when is_integer(CreatorId) ->
                              Props#{ <<"creator_id">> => CreatorId };
@@ -1639,4 +1639,3 @@ update_page_path_log(RscId, OldProps, NewProps, Context) ->
             z_db:q("INSERT INTO rsc_page_path_log(id, page_path) VALUES ($1, $2)", [RscId, Old], Context),
             ok
     end.
-
