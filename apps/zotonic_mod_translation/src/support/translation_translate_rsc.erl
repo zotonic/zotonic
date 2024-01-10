c549033d0df9f8b0f621dabada999a08f012cbd6
Marc Worrell <marc@worrell.nl>
2024-01-10 09:09:46 +0100

    mod_translation: add routines to add or remove translations from resources

---
+++ apps/zotonic_mod_translation/src/support/translation_translate_rsc.erl
@@ -0,0 +1,361 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2024 Driebit BV
+%% @doc Translate a resource, adds a new language to all language properties.
+%% @end
+
+%% Copyright 2024 Driebit BV
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(translation_translate_rsc).
+-author("Marc Worrell <marc@worrell.nl>").
+
+-include_lib("zotonic_core/include/zotonic.hrl").
+
+-export([
+    translate/5,
+    remove_translation/3,
+    has_language/3
+]).
+
+
+%% @doc Add a translation to the resource. The source and destination language must be editable
+%% languages for the site. If the source language is not in the resource then an error is returned.
+-spec translate(Id, FromLanguage, ToLanguage, IsOverwrite, Context) -> ok | {error, Reason} when
+    Id :: m_rsc:resource(),
+    FromLanguage :: z_language:language_code(),
+    ToLanguage :: z_language:language_code(),
+    IsOverwrite :: boolean(),
+    Context :: z:context(),
+    Reason :: term().
+translate(Id, FromLanguage, ToLanguage, IsOverwrite, Context) ->
+    case z_acl:rsc_editable(Id, Context) of
+        true ->
+            case {z_language:is_language_editable(FromLanguage, Context),
+                  z_language:is_language_editable(ToLanguage, Context)}
+            of
+                {false, _} ->
+                    {error, src};
+                {_, false} ->
+                    {error, dst};
+                {true, true} ->
+                    case has_language(Id, FromLanguage, Context) of
+                        true ->
+                            translate_1(Id, FromLanguage, ToLanguage, IsOverwrite, Context);
+                        false ->
+                            {error, nosrc}
+                    end
+            end;
+        false ->
+            {error, eacces}
+    end.
+
+%% @doc Check if a resource has a language or one of the languages.
+-spec has_language(Id, Lang, Context) -> boolean() when
+    Id :: m_rsc:resource(),
+    Lang :: z_language:language_code(),
+    Context :: z:context().
+has_language(Id, Lang, Context) when is_atom(Lang) ->
+    has_language(Id, [ Lang ], Context);
+has_language(Id, Langs, Context) when is_list(Langs) ->
+    case m_rsc:p_no_acl(Id, <<"language">>, Context) of
+        undefined ->
+            true;
+        Language when is_list(Language) ->
+            lists:any(
+                fun(Lang) -> lists:member(Lang, Language) end,
+                Langs)
+    end.
+
+
+%% @doc Remove languages from a resource.
+-spec remove_translation(Id, Language, Context) -> ok | {error, Reason} when
+    Id :: m_rsc:resource(),
+    Language :: z_language:language_code() | [ z_language:language_code() ],
+    Context :: z:context(),
+    Reason :: term().
+remove_translation(Id, Language, Context) when is_atom(Language) ->
+    remove_translation(Id, [ Language ], Context);
+remove_translation(Id, Langs, Context) when is_list(Langs) ->
+    case z_acl:rsc_editable(Id, Context) of
+        true ->
+            case has_language(Id, Langs, Context) of
+                true ->
+                    Rsc = m_rsc:get(Id, Context),
+                    Props = remove_1(Rsc, Langs, false),
+                    TransStatus = case m_rsc:p_no_acl(Id, <<"translation_status">>, Context) of
+                        undefined -> #{};
+                        TrSt -> TrSt
+                    end,
+                    BinLangs = [ atom_to_binary(Lang) || Lang <- Langs ],
+                    Language = case m_rsc:p_no_acl(Id, <<"language">>, Context) of
+                        undefined -> [];
+                        Lng -> Lng
+                    end,
+                    Props1 = Props#{
+                        <<"translation_status">> => maps:without(BinLangs, TransStatus),
+                        <<"language">> => Language -- Langs
+                    },
+                    case m_rsc:update(Id, Props1, Context) of
+                        {ok, _} -> ok;
+                        {error, _} = Error -> Error
+                    end;
+                false ->
+                    ok
+            end;
+        false ->
+            {error, eacces}
+    end.
+
+remove_1(Map, Langs, IsCopyAll) when is_map(Map) ->
+    maps:fold(
+        fun(K, V, Acc) ->
+            case remove_1(V, Langs, true) of
+                V when not IsCopyAll -> Acc;
+                V1 -> Acc#{ K => V1 }
+            end
+        end,
+        #{},
+        Map);
+remove_1(L, Langs, _IsCopyAll) when is_list(L) ->
+    lists:map(
+        fun(V) -> remove_1(V, Langs, true) end,
+        L);
+remove_1(#trans{ tr = Tr }, Langs, _IsCopyAll) ->
+    Tr1 = lists:foldl(
+        fun(Lang, Acc) ->
+            lists:keydelete(Lang, 1, Acc)
+        end,
+        Tr,
+        Langs),
+    #trans{ tr = Tr1 };
+remove_1(V, _Language, _IsCopyAll) ->
+    V.
+
+
+translate_1(Id, FromLanguage, ToLanguage, IsOverwrite, Context) ->
+    Texts = collect_src_texts(Id, FromLanguage, ToLanguage, IsOverwrite, Context),
+    case m_translation:translate_to_lookup(FromLanguage, ToLanguage, Texts, Context) of
+        {ok, Translations} ->
+            TransMap = lists:foldl(
+                fun(#{ <<"text">> := Txt, <<"translation">> := TxtTr }, Acc) ->
+                    Acc#{
+                        Txt => TxtTr
+                    }
+                end,
+                #{},
+                Translations),
+            Props = insert_dst_texts(Id, FromLanguage, ToLanguage, TransMap, IsOverwrite, Context),
+            case maps:size(Props) of
+                0 ->
+                    ok;
+                _ ->
+                    TransStatus = case m_rsc:p_no_acl(Id, <<"translation_status">>, Context) of
+                        undefined -> #{};
+                        TrSt -> TrSt
+                    end,
+                    ToLanguageB = z_convert:to_binary(ToLanguage),
+                    TransStatus1 = TransStatus#{ ToLanguageB => <<"1">> },
+                    Language = case m_rsc:p_no_acl(Id, <<"language">>, Context) of
+                        undefined -> [];
+                        Lng -> Lng
+                    end,
+                    Language1 = case lists:member(ToLanguage, Language) of
+                        true -> Language;
+                        false -> [ ToLanguage | Language ]
+                    end,
+                    Props1 = Props#{
+                        <<"translation_status">> => TransStatus1,
+                        <<"language">> => lists:sort(Language1)
+                    },
+                    case m_rsc:update(Id, Props1, Context) of
+                        {ok, _} -> ok;
+                        {error, _} = Error -> Error
+                    end
+            end;
+        {error, _} = Error ->
+            Error
+    end.
+
+collect_src_texts(Id, FromLanguage, ToLanguage, IsOverwrite, Context) ->
+    Rsc = m_rsc:get(Id, Context),
+    collect_texts_1(Rsc, FromLanguage, ToLanguage, IsOverwrite, []).
+
+collect_texts_1(Map, FromLanguage, ToLanguage, IsOverwrite, Acc0) when is_map(Map) ->
+    maps:fold(
+        fun(K, V, Acc) ->
+            collect_texts_2(K, V, FromLanguage, ToLanguage, IsOverwrite, Acc)
+        end,
+        Acc0,
+        Map);
+collect_texts_1(L, FromLanguage, ToLanguage, IsOverwrite, Acc0) when is_list(L) ->
+    lists:foldl(
+        fun(V, Acc) ->
+           collect_texts_2(<<>>, V, FromLanguage, ToLanguage, IsOverwrite, Acc)
+        end,
+        Acc0,
+        L);
+collect_texts_1(_, _FromLanguage, _ToLanguage, _IsOverwrite, Acc) ->
+    Acc.
+
+collect_texts_2(K, V, FromLanguage, ToLanguage, IsOverwrite, Acc) when is_binary(V) ->
+    case is_text(K) of
+        true ->
+            V1 = #trans{ tr = [ {FromLanguage, V} ] },
+            collect_trans(K, V1, FromLanguage, ToLanguage, IsOverwrite, Acc);
+        false ->
+            Acc
+    end;
+collect_texts_2(K, #trans{} = V, FromLanguage, ToLanguage, IsOverwrite, Acc) ->
+    collect_trans(K, V, FromLanguage, ToLanguage, IsOverwrite, Acc);
+collect_texts_2(_K, V, FromLanguage, ToLanguage, IsOverwrite, Acc) when is_list(V) ->
+    collect_texts_1(V, FromLanguage, ToLanguage, IsOverwrite, Acc);
+collect_texts_2(_K, V, FromLanguage, ToLanguage, IsOverwrite, Acc) when is_map(V) ->
+    collect_texts_1(V, FromLanguage, ToLanguage, IsOverwrite, Acc);
+collect_texts_2(_, _, _FromLanguage, _ToLanguage, _IsOverwrite, Acc) ->
+    Acc.
+
+collect_trans(K, #trans{ tr = Tr }, FromLanguage, ToLanguage, IsOverwrite, Acc) ->
+    case is_json(K) of
+        true ->
+            Acc;
+        false ->
+            ToText = case lists:keyfind(ToLanguage, 1, Tr) of
+                false -> <<>>;
+                {_, <<>>} -> <<>>;
+                {_, T} -> T
+            end,
+            case lists:keyfind(FromLanguage, 1, Tr) of
+                {_, <<>>} ->
+                    Acc;
+                {_, FromText} when ToText =:= <<>> orelse IsOverwrite ->
+                    [ FromText | Acc ];
+                {_, _} ->
+                    Acc;
+                false ->
+                    Acc
+            end
+    end.
+
+is_json(K) ->
+    binary:longest_common_suffix([K, <<"_json">>]) =:= 5.
+
+% Fields we are sure of that they are text fields
+is_text(<<"title">>) -> true;
+is_text(<<"short_title">>) -> true;
+is_text(<<"summary">>) -> true;
+is_text(<<"body">>) -> true;
+is_text(<<"body_extra">>) -> true;
+is_text(<<"date_remarks">>) -> true;
+is_text(<<"prompt">>) -> true;
+is_text(<<"explanation">>) -> true;
+is_text(<<"matching">>) -> true;
+is_text(<<"narrative">>) -> true;
+is_text(<<"feedback">>) -> true;
+is_text(<<"seo_title">>) -> true;
+is_text(<<"seo_desc">>) -> true;
+is_text(<<"seo_keywords">>) -> true;
+is_text(K) ->
+    binary:longest_common_suffix([ K, <<"_html">> ]) =:= 5.
+
+%% @doc Add the translation to all translatable texts.
+insert_dst_texts(Id, FromLanguage, ToLanguage, Translations, IsOverwrite, Context) ->
+    Rsc = m_rsc:get(Id, Context),
+    insert_dst_texts_1(Rsc, FromLanguage, ToLanguage, Translations, IsOverwrite, true).
+
+insert_dst_texts_1(Map, FromLanguage, ToLanguage, Translations, IsOverwrite, CopyAll) when is_map(Map) ->
+    maps:fold(
+        fun
+            (K, V, Acc) when is_binary(V) ->
+                case is_text(K) of
+                    true ->
+                        V1 = #trans{ tr = [ {FromLanguage, V} ] },
+                        case dst_trans(K, V1, FromLanguage, ToLanguage, Translations, IsOverwrite) of
+                            V1 when not CopyAll ->
+                                Acc;
+                            V2 ->
+                                Acc#{ K => V2 }
+                        end;
+                    false when CopyAll ->
+                        Acc#{ K => V };
+                    false ->
+                        Acc
+                end;
+            (K, #trans{} = V, Acc) ->
+                case dst_trans(K, V, FromLanguage, ToLanguage, Translations, IsOverwrite) of
+                    V when not CopyAll ->
+                        Acc;
+                    V1 ->
+                        Acc#{ K => V1 }
+                end;
+            (K, L, Acc) when is_list(L) ->
+                L1 = lists:map(
+                    fun(V) ->
+                        insert_dst_texts_1(V, FromLanguage, ToLanguage, Translations, IsOverwrite, true)
+                    end,
+                    L),
+                case L1 of
+                    L when not CopyAll -> Acc;
+                    _ -> Acc#{ K => L1 }
+                end;
+            (K, M, Acc) when is_map(M) ->
+                case insert_dst_texts_1(M, FromLanguage, ToLanguage, Translations, IsOverwrite, true) of
+                    M when not CopyAll -> Acc;
+                    M1 -> Acc#{ K => M1 }
+                end;
+            (K, V, Acc) when CopyAll ->
+                Acc#{ K => V };
+            (_, _, Acc) ->
+                Acc
+        end,
+        #{},
+        Map);
+insert_dst_texts_1(L, FromLanguage, ToLanguage, Translations, IsOverwrite, _CopyAll) when is_list(L) ->
+    lists:map(
+        fun(V) ->
+            insert_dst_texts_1(V, FromLanguage, ToLanguage, Translations, IsOverwrite, true)
+        end,
+        L);
+insert_dst_texts_1(#trans{} = Tr, FromLanguage, ToLanguage, Translations, IsOverwrite, _CopyAll) ->
+    dst_trans(<<>>, Tr, FromLanguage, ToLanguage, Translations, IsOverwrite);
+insert_dst_texts_1(V, _FromLanguage, _ToLanguage, _Translations, _IsOverwrite, _CopyAll) ->
+    V.
+
+dst_trans(K, #trans{ tr = Tr } = V, FromLanguage, ToLanguage, Translations, IsOverwrite) ->
+    case is_json(K) of
+        false ->
+            ToText = case lists:keyfind(ToLanguage, 1, Tr) of
+                false -> <<>>;
+                {_, <<>>} -> <<>>;
+                {_, T} -> T
+            end,
+            if
+                ToText =:= <<>> orelse IsOverwrite ->
+                    Translated = case lists:keyfind(FromLanguage, 1, Tr) of
+                        {_, FromText} ->
+                            case maps:get(FromText, Translations, <<>>) of
+                                undefined -> <<>>;
+                                T1 -> T1
+                            end;
+                        false ->
+                            V
+                    end,
+                    Tr1 = lists:keydelete(ToLanguage, 1, Tr),
+                    Tr2 = [ {ToLanguage, Translated} | Tr1 ],
+                    #trans{ tr = lists:sort(Tr2) };
+                true ->
+                    V
+            end;
+        true ->
+            V
+    end.
