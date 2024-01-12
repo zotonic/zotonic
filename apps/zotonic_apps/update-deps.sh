0a7097ed2c734cdbe6556798c41e82990bd2f72c
Marc Worrell <marc@worrell.nl>
2024-01-12 15:44:32 +0100

    Ensure that core apps are build before apps_user - so that deps are resolved.

--- apps/zotonic_apps/update-deps.sh
+++ apps/zotonic_apps/update-deps.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VERSION=`cat ../../../VERSION`
+# Ensure that all Zotonic core apps are in the deps of the rebar.config file
 
-ls ../../../apps \
+ls .. \
+    | grep -v "zotonic_apps" \
     | grep -v "zotonic_mod_acl_mock" \
     | grep -v "zotonic_site_testsandbox" \
-    | sed -e "s/\(zotonic_[^ ]*\)/{\1, \"$VERSION\"},/g" \
-    | sed -E '$ s/},/}/' > tempdeps
+    | sed -e "s/\(zotonic_[^ ]*\)/\1,/g" \
+    | sed -E '$ s/,//' > tempdeps
 
 cp rebar.config.template rebar.config
 sed -i.bck -e '/ZOTONIC_APPS/ {' -e 'r tempdeps' -e 'd' -e '}' rebar.config
