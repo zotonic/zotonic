730fef38f5165d91554358ddc2cb7b2aed94205c
Marc Worrell <marc@worrell.nl>
2024-02-29 08:53:22 +0100

    Add config option smtp_plaintext_fallback

--- apps/zotonic_core/src/smtp/z_email_server.erl
+++ apps/zotonic_core/src/smtp/z_email_server.erl
@@ -1105,17 +1105,19 @@ send_blocking_smtp(MsgId, VERP, RecipientEmail, EncodedMail, SmtpOpts, Context) 
         relay => Relay,
         port => proplists:get_value(port, SmtpOpts)
     }),
+    IsFallbackPlainText = z_config:get(smtp_plaintext_fallback),
     case gen_smtp_client:send_blocking({VERP, [RecipientEmail], EncodedMail}, SmtpOpts) of
         Receipt when is_binary(Receipt) ->
             {ok, Receipt};
-        {error, no_more_hosts, {permanent_failure, _Host, <<"ign Root ", _/binary>>}} ->
+        {error, no_more_hosts, {permanent_failure, _Host, <<"ign Root ", _/binary>>}}
+            when IsFallbackPlainText ->
             % Don't ask ...
             send_blocking_no_tls(VERP, RecipientEmail, EncodedMail, SmtpOpts, Context);
         {error, retries_exceeded, {_FailureType, _Host, {error, Reason}}}
-            when Reason =:= closed; Reason =:= timeout ->
+            when IsFallbackPlainText, (Reason =:= closed orelse Reason =:= timeout) ->
             send_blocking_no_tls(VERP, RecipientEmail, EncodedMail, SmtpOpts, Context);
         {error, send, {_FailureType, _Host, {error, Reason}}}
-            when Reason =:= closed; Reason =:= timeout ->
+            when IsFallbackPlainText, (Reason =:= closed orelse Reason =:= timeout) ->
             send_blocking_no_tls(VERP, RecipientEmail, EncodedMail, SmtpOpts, Context);
         {error, _} = Error ->
             Error;
