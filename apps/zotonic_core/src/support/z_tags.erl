c570e4eede356261c01b7a58c1bd461ef572c5c8
Marc Worrell <marc@worrell.nl>
2024-12-01 17:51:26 +0100

    Fix edoc

--- apps/zotonic_core/src/support/z_tags.erl
+++ apps/zotonic_core/src/support/z_tags.erl
@@ -1,8 +1,8 @@
 %% @author Marc Worrell <marc@worrell.nl>
 %% @copyright 2009-2024 Marc Worrell <marc@worrell.nl>
-%% @doc Generate a HTML element with attributes, correctly escape all attributes.
 %% Based on code Copyright (c) 2008-2009 Rusty Klophaus
 %% Original author Tom McNulty <tom.mcnulty@cetiforge.com>
+%% @doc Generate a HTML element with attributes, correctly escape all attributes.
 %% @end
 
 %% Copyright 2009-2024 Marc Worrell
