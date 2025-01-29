96a6d89986e2c3b88711c51918735d7c2980cdd9
Marc Worrell <marc@worrell.nl>
2025-01-29 18:11:25 +0100

    Update copyright year

--- apps/zotonic_core/src/support/z_context.erl
+++ apps/zotonic_core/src/support/z_context.erl
@@ -1,9 +1,9 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2009-2024  Marc Worrell
+%% @copyright 2009-2025  Marc Worrell
 %% @doc Request context for Zotonic request evaluation.
 %% @end
 
-%% Copyright 2009-2024 Marc Worrell
+%% Copyright 2009-2025 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
--- apps/zotonic_mod_seo/src/mod_seo.erl
+++ apps/zotonic_mod_seo/src/mod_seo.erl
@@ -1,9 +1,9 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2009-2023 Marc Worrell
+%% @copyright 2009-2025 Marc Worrell
 %% @doc Search engine optimization.  Provides admin interface for the SEO modules.
 %% @end
 
-%% Copyright 2009-2023 Marc Worrell
+%% Copyright 2009-2025 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
