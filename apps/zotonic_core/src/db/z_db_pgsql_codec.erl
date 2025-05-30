8c6f9d7e3b1e7ab2fc97c4ecdc8a3491d61f3a60
MM Zeeman <mmzeeman@xs4all.nl>
2025-05-30 09:15:52 +0200

    Move datetime eterm decode to epgsql codec (#4096)
    * db: Added missing convert for group_by queries containing an array_agg column
    
    * db: Added missing convert for group_by queries containing an array_agg(row(..)) column
    
    * test: Fix typo
    
    * db: Proof of concept zotonic codec for epgsql
    
    * db: Refactor retrieving the connect options
    
    * db: Remove unneeded decode code
    
    * db: Move erlang term encoding to codec
    
    * Oops rebar.lock updated
    
    * Apply suggestions from code review
    
    Thanks for the suggestions.
    
    Co-authored-by: Viacheslav Katsuba <v.katsuba.dev@gmail.com>
    
    * test: Added json conversion test. (Test number 100 🎉)
    
    * db: Move json term encoding to the codec
    
    * Oops, pushed rebar.lock another time
    
    * db: Separate decoding bytea from datetime values in order to prevent accidental binary_to_term
    
    * db: Removed typo
    
    * db: Make sure the connection is always returned by using try after
    
    ---------
    
    Co-authored-by: Viacheslav Katsuba <v.katsuba.dev@gmail.com>

---
+++ apps/zotonic_core/src/db/z_db_pgsql_codec.erl
@@ -0,0 +1,85 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2025 Maas-Maarten Zeeman
+%% @doc Postgresql zotonic codec
+%%
+%% These are conversion routines between how z_db expects values and how epgsl expects them.
+%%
+%% Notable differences:
+%% - Input values {term, ...} (use the ?DB_PROPS(...) macro!) are term_to_binary encoded and decoded
+%% - date/datetimes have a floating-point second argument in epgsql, in Zotonic they don't.
+%% @end
+
+%% Copyright 2025 Arjan Scherpenisse, Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(z_db_pgsql_codec).
+-behaviour(epgsql_codec).
+
+-export([init/2, names/0, encode/3, decode/3, decode_text/3]).
+
+%% 4-byte magic number used as a prefix to serialized Erlang terms.
+%% Helps identify and distinguish term binaries in raw byte streams.
+-define(TERM_MAGIC_NUMBER, 16#01326A3A:1/big-unsigned-unit:32).
+
+
+init(Term, Sock) ->
+    %% The datetime codec has to check what time formatting is used
+    epgsql_codec_datetime:init(Term, Sock).
+
+names() ->
+    [jsonb, bytea, time, timetz, date, timestamp, timestamptz].
+
+% bytea
+encode({term, Term}, bytea, State) ->
+    B = term_to_binary(Term),
+    epgsql_codec_text:encode(<<?TERM_MAGIC_NUMBER, B/binary>>, bytea, State);
+encode(Cell, bytea, State) ->
+    epgsql_codec_text:encode(Cell, bytea, State);
+% jsonb
+encode({term_json, Term}, jsonb, _State) ->
+    epgsql_codec_json:encode(jsxrecord:encode(Term), jsonb, []);
+encode(Term, jsonb, _State) ->
+    epgsql_codec_json:encode(Term, jsonb, []);
+% datetime types
+encode(Cell, TypeName, State) ->
+    epgsql_codec_datetime:encode(Cell, TypeName, State).
+
+% bytea
+decode(Cell, bytea, _State) ->
+    decode_bytea_value(epgsql_codec_text:decode(Cell, bytea, []));
+% jsonb
+decode(Cell, jsonb, _State) ->
+    epgsql_codec_json:decode(Cell, jsonb, []);
+% datetime types
+decode(Cell, TypeName, State) ->
+    decode_datetime_value(epgsql_codec_datetime:decode(Cell, TypeName, State)).
+
+decode_text(Cell, bytea, _State) ->
+    epgsql_codec_text:decode_text(Cell, bytea, []);
+decode_text(Cell, jsonb, _State) ->
+    epgsql_codec_json:decode_text(Cell, jsonb, []);
+decode_text(Cell, TypeName, State) ->
+    epgsql_codec_datetime:decode_text(Cell, TypeName, State).
+
+decode_bytea_value(<<?TERM_MAGIC_NUMBER, B/binary>>) ->
+    binary_to_term(B);
+decode_bytea_value(V) ->
+    V.
+
+decode_datetime_value({H, M, S}) when is_float(S) ->
+    {H, M, trunc(S)};
+decode_datetime_value({{Y, Mm, D}, {H, M, S}}) when is_float(S) ->
+    {{Y, Mm, D}, {H, M, trunc(S)}};
+decode_datetime_value(V) ->
+    V.
--- apps/zotonic_core/test/z_db_tests.erl
+++ apps/zotonic_core/test/z_db_tests.erl
@@ -51,3 +51,40 @@ alter_table_test() ->
     ok = z_db:drop_table(dbtests, Context),
     ok.
 
+postgres_datetime_conversion_test() ->
+    Context = z_context:new(zotonic_site_testsandbox),
+
+    ?assertMatch( [{{{_,_,_},{_,_,S}}}] when is_integer(S), z_db:q("select now();", Context)),
+    ?assertMatch( [{[{{_,_,_},{_,_,S}}]}] when is_integer(S), z_db:q("select array_agg(now());", Context)),
+    ?assertMatch( [{[{{{_,_,_},{_,_, S}}, _} ]}] when is_integer(S), z_db:q("select array_agg(row(now(), 1));", Context)),
+
+    ok.
+
+postgres_null_conversion_test() ->
+    Context = z_context:new(zotonic_site_testsandbox),
+    [{undefined}] = z_db:q("select null;", Context),
+    ok.
+
+postgres_eterm_conversion_test() ->
+    Context = z_context:new(zotonic_site_testsandbox),
+
+    %% Should be encoded as null value in postgres, which is returned as undefined.
+    [{undefined}] = z_db:q("select $1::bytea;", [{term, undefined}], Context),
+
+    [{#{ test := 123 }}] = z_db:q("select $1::bytea;", [{term, #{ test => 123 }}], Context),
+    ok.
+
+postgres_json_conversion_test() ->
+    Context = z_context:new(zotonic_site_testsandbox),
+
+    %% Should be encoded as null value in postgres, which is returned as undefined.
+    [{undefined}] = z_db:q("select $1::jsonb;", [{term_json, undefined}], Context),
+
+    %% Currently the result is not decoded.
+    [{<<"{\"test\": 123}">>}] = z_db:q("select $1::jsonb;", [{term_json, #{ test => 123 }}], Context),
+
+    %% Check if jsxrecord is used to encode the terms.
+    [{<<"{\"list\": [1], \"_type\": \"rsc_list\"}">>}] =
+        z_db:q("select $1::jsonb;", [{term_json, #rsc_list{ list = [1] }}], Context),
+
+    ok.
