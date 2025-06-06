16975452a1a9ae174457fcfe15301eb94509eac8
MM Zeeman <mmzeeman@xs4all.nl>
2025-06-06 09:55:08 +0200

    mod_cron: Added erlcron to handle mod_cron ticks. Useful for other use-cases too (#4109)
    * Added erlcron to handle mod_cron ticks. Useful for other use-cases too
    
    * Use the newest version of erlcron (1.2.3)
    
    * Updated date in the header

--- apps/zotonic_mod_cron/src/mod_cron.erl
+++ apps/zotonic_mod_cron/src/mod_cron.erl
@@ -1,9 +1,9 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2017-2024 Marc Worrell
+%% @copyright 2017-2025 Marc Worrell
 %% @doc Periodic tasks and ticks for other modules.
 %% @end
 
-%% Copyright 2017-2024 Marc Worrell
+%% Copyright 2017-2025 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -37,24 +37,33 @@
     terminate/2
 ]).
 
--record(state, { site :: atom() }).
+-export([ tick_event/2 ]).
+
+-record(state, {
+          site :: atom(),
+          tick_job_refs = [] :: list(erlcron:job_ref())
+         }).
 
 %% Default timers. These are called approximately every N seconds.
 -define(TIMER_INTERVAL, [
-    {    1, tick_1s},
-    {   60, tick_1m},
-    {  600, tick_10m},
-    {  900, tick_15m},
-    { 1800, tick_30m},
-    { 3600, tick_1h},
-    { 7200, tick_2h},
-    {10800, tick_3h},
-    {14400, tick_4h},
-    {21600, tick_6h},
-    {43200, tick_12h},
-    {86400, tick_24h}
+    {{ 1, sec}, tick_1s},
+    {{ 1, min}, tick_1m},
+    {{10, min}, tick_10m},
+    {{15, min}, tick_15m},
+    {{30, min}, tick_30m},
+    {{ 1, hr},  tick_1h},
+    {{ 2, hr},  tick_2h},
+    {{ 3, hr},  tick_3h},
+    {{ 4, hr},  tick_4h},
+    {{ 6, hr},  tick_6h},
+    {{12, hr},  tick_12h},
+    {{24, hr},  tick_24h}
 ]).
 
+-include_lib("zotonic_core/include/zotonic.hrl").
+
+tick_event(Event, Site) ->
+    z_notifier:notify(Event, z_context:new(Site)).
 
 start_link(Args) when is_list(Args) ->
     {context, Context} = proplists:lookup(context, Args),
@@ -62,13 +71,10 @@ start_link(Args) when is_list(Args) ->
     gen_server:start_link(?MODULE, Site, []).
 
 init(Site) when is_atom(Site) ->
+    erlang:process_flag(trap_exit, true),
     z_context:logger_md(Site),
-    lists:foreach(
-        fun({Timeout, Event}) ->
-            erlang:send_after(Timeout * 1000, self(), {tick, Timeout, Event})
-        end,
-        ?TIMER_INTERVAL),
-    {ok, #state{ site = Site }}.
+    JobRefs = start_tick_jobs(Site),
+    {ok, #state{ site = Site, tick_job_refs = JobRefs }}.
 
 handle_call(_Msg, _From, State) ->
     {reply, {error, unknown_message}, State}.
@@ -76,13 +82,32 @@ handle_call(_Msg, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({tick, Timeout, Event}, #state{ site = Site } = State) ->
-    z_notifier:notify(Event, z_context:new(Site)),
-    erlang:send_after(Timeout * 1000, self(), {tick, Timeout, Event}),
+handle_info(_Info, State) ->
     {noreply, State}.
 
 code_change(_Vsn, State, _Extra) ->
     {ok, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, #state{ tick_job_refs = TickJobs }) ->
+    lists:foreach(fun(JobRef) -> _ = erlcron:cancel(JobRef) end, TickJobs),
     ok.
+
+%%
+%% Helpers
+%%
+
+start_tick_jobs(Site) ->
+    [ start_tick_job(Event, Timeout, Site) || {Timeout, Event} <- ?TIMER_INTERVAL].
+
+start_tick_job(Event, Timeout, Site) ->
+    JobRef = job_ref(Event, Site),
+    Job = #{ id => JobRef,
+             interval => {daily, {every, Timeout}},
+             execute => {?MODULE, tick_event, [Event, Site]} },
+    case erlcron:cron(Job) of
+        already_started -> JobRef;
+        JobRef -> JobRef
+    end.
+
+job_ref(Event, Site) ->
+    <<(z_convert:to_binary(Site))/binary, $_, (z_convert:to_binary(Event))/binary>>.
