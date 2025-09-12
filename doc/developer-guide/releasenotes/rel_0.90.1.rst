d3c126ec8436cef5a1e5dc6ad356d9be6e1ba9ae
Marc Worrell <marc@worrell.nl>
2025-09-12 08:36:49 +0200

    Set version to 0.90.1

---
+++ doc/developer-guide/releasenotes/rel_0.90.1.rst
@@ -0,0 +1,18 @@
+.. _rel-0.90.0:
+
+Release 0.90.1
+==============
+
+Welcome to Zotonic 0.90.1, released on September 12, 2025.
+
+This is a patch release to fix some issues with the newly introduced
+support for language codes like 'pap-cw'.
+
+Commits since 0.90.0
+--------------------
+
+Marc Worrell (3):
+
+ * mod_translation: gracefully handle unknown pref_language (#4196)
+ * mod_translation: Fix set_language for unknown language (0.x) (#4198)
+ * core: fix an issue with editing blocks in languages with a dash (#4199)
--- prepare-release.sh
+++ prepare-release.sh
@@ -11,9 +11,17 @@ MINOR=$(echo $VERSION | sed -e 's/.[^.]*$//')
 # Increments version numbers where needed
 # Usage: ./prepare-release.sh 0.13.7
 
-sed -e "s/^version = .*/version = '$MINOR'/" -i doc/conf.py
-sed -e "s/^release = .*/release = '$VERSION'/" -i doc/conf.py
-sed -e "s/{vsn, .*/{vsn, \"$VERSION\"},/" -i src/zotonic.app.src
-sed -e "s/ZOTONIC_VERSION, .*)./ZOTONIC_VERSION, \"$VERSION\")./" -i include/zotonic_release.hrl
+# Linux
+# sed -e "s/^version = .*/version = '$MINOR'/" -i doc/conf.py
+# sed -e "s/^release = .*/release = '$VERSION'/" -i doc/conf.py
+# sed -e "s/{vsn, .*/{vsn, \"$VERSION\"},/" -i src/zotonic.app.src
+# sed -e "s/ZOTONIC_VERSION, .*)./ZOTONIC_VERSION, \"$VERSION\")./" -i include/zotonic_release.hrl
+
+# macOS
+sed -e "s/^version = .*/version = '$MINOR'/" -i '' doc/conf.py
+sed -e "s/^release = .*/release = '$VERSION'/" -i '' doc/conf.py
+sed -e "s/{vsn, .*/{vsn, \"$VERSION\"},/" -i '' src/zotonic.app.src
+sed -e "s/ZOTONIC_VERSION, .*)./ZOTONIC_VERSION, \"$VERSION\")./" -i '' include/zotonic_release.hrl
+
 
 git status
