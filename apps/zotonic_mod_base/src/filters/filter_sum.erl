2bf5bf3f5ecc1c17d9bafe20d80162b8c0d391dc
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2026-01-20 15:20:20 +0100

    Added documentation for the sum filter

--- apps/zotonic_mod_base/src/filters/filter_sum.erl
+++ apps/zotonic_mod_base/src/filters/filter_sum.erl
@@ -18,7 +18,31 @@
 
 -module(filter_sum).
 -moduledoc("
-# TODO
+Sum a list of numbers.
+
+This filter calculates the sum of all numeric values in a list.  Non-numeric values are
+automatically filtered out.
+
+For example:
+
+```django
+    {{ [1, 2, 3, 4, 5] | sum }}
+```
+
+Results in `15`.
+
+When applied to a list with mixed types:
+
+```django
+    {{ [10, 20, \"text\", 30] | sum }}
+```
+
+Results in `60`. The string \"text\" is ignored.
+
+If the input is `undefined`, the filter returns `undefined`.
+
+This filter is useful for analytics and reporting, such as summing values
+extracted from lists of maps or resources.
 ").
 
 -export([sum/2]).
