b05bf45d084c55267a0a5488201d0da4b1b5b39b
Marc Worrell <marc@worrell.nl>
2026-06-24 13:27:36 +0200

    Fix SSO signup for ext email verified, local not verified

--- apps/zotonic_mod_authentication/priv/lib/js/zotonic.auth-ui.worker.js
+++ apps/zotonic_mod_authentication/priv/lib/js/zotonic.auth-ui.worker.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019-2021 Marc Worrell <marc@worrell.nl>
+ * Copyright 2019-2026 Marc Worrell <marc@worrell.nl>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- apps/zotonic_mod_oauth2/priv/lib/js/zotonic.oauth.worker.js
+++ apps/zotonic_mod_oauth2/priv/lib/js/zotonic.oauth.worker.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020-2022 Marc Worrell <marc@worrell.nl>
+ * Copyright 2020-2026 Marc Worrell <marc@worrell.nl>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
