7420bc8dd0872c64de031088607ffae21b30d8c4
Marc Worrell <marc@worrell.nl>
2026-07-01 13:36:38 +0200

    Re-add misspelled EXIF term

--- apps/zotonic_mod_media_exif/src/filters/filter_media_exif_value.erl
+++ apps/zotonic_mod_media_exif/src/filters/filter_media_exif_value.erl
@@ -110,6 +110,8 @@ format_value(Name, Value)
 format_value(Name, Value)
     when Name =:= <<"subsec_time">>;
          Name =:= <<"subsec_time_original">>;
+         % Keep the historical misspelling; EXIF metadata from an iPhone SE can contain this key.
+         Name =:= <<"subsec_time_orginal">>;
          Name =:= <<"subsec_time_digitized">> ->
     format_ascii(Value);
 format_value(_Name, Value) ->
