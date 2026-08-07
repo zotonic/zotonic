81fc227f2e5cdce912646bb47d4da1d43beff48e
Marc Worrell <marc@worrell.nl>
2026-08-07 14:38:41 +0200

    core: always deny anonymous users update actions (#4478)
    * core: always deny anonymous users update actions
    
    * Add acl tests
    
    * Disallow anoymous use.mod_admin_config
    
    * Also add check to is_allowed
    
    * Remove space (accidental change)
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Rob van den Bogaard <rob@driebit.nl>

--- apps/zotonic_core/test/z_acl_tests.erl
+++ apps/zotonic_core/test/z_acl_tests.erl
@@ -1,7 +1,43 @@
 -module(z_acl_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("zotonic.hrl").
+
+-export([acl_is_allowed/2]).
 
 name_for_object_test() ->
     Context = z_context:new(zotonic_site_testsandbox),
-    false = z_acl:is_allowed(update, administrator, Context).
+    false = z_acl:is_allowed(update, ?ACL_ADMIN_USER_ID, Context).
+
+anonymous_update_denied_test() ->
+    Context = z_context:new(zotonic_site_testsandbox),
+    z_notifier:observe(acl_is_allowed, {?MODULE, acl_is_allowed}, 1, Context),
+    z_notifier:observe(acl_is_allowed_prop, {?MODULE, acl_is_allowed}, 1, Context),
+    try
+        lists:foreach(
+            fun(Result) ->
+                assert_anonymous_update_denied(
+                    z_context:set(acl_is_allowed_result, Result, Context)
+                )
+            end,
+            [undefined, false, true]
+        )
+    after
+        z_notifier:detach(acl_is_allowed, Context),
+        z_notifier:detach(acl_is_allowed_prop, Context)
+    end.
+
+assert_anonymous_update_denied(Context) ->
+    lists:foreach(
+        fun(Action) ->
+            ?assertNot(z_acl:is_allowed(Action, ?ACL_ADMIN_USER_ID, Context)),
+            ?assertNot(z_acl:maybe_allowed(Action, ?ACL_ADMIN_USER_ID, Context)),
+            ?assertNot(z_acl:is_allowed_prop(Action, ?ACL_ADMIN_USER_ID, title, Context))
+        end,
+        [admin, insert, update, delete, link]
+    ).
+
+acl_is_allowed(#acl_is_allowed{}, Context) ->
+    z_context:get(acl_is_allowed_result, Context);
+acl_is_allowed(#acl_is_allowed_prop{}, Context) ->
+    z_context:get(acl_is_allowed_result, Context).
--- apps/zotonic_mod_acl_user_groups/test/mod_acl_user_groups_tests.erl
+++ apps/zotonic_mod_acl_user_groups/test/mod_acl_user_groups_tests.erl
@@ -12,6 +12,9 @@ EUnit tests for ACL user-group authorization behavior.
     is_allowed_always_true/2
 ]).
 
+-define(UG_TEST, acl_user_group_test).
+
+
 tree_expand_test() ->
     [] = acl_user_groups_rules:tree_expand([]),
     [{1,[1]}, {2,[2]}] = acl_user_groups_rules:tree_expand([{1,[]}, {2,[]}]),
@@ -177,81 +180,89 @@ restrict_collab_cats_intersects_cat_list_test() ->
 
 person_can_edit_own_resource_test() ->
     ContextAnon = context(),
+    ContextSudo = z_acl:sudo(ContextAnon),
 
     %% Person must be able to edit person category
     replace_managed(
         [
             {rsc, [
-                {acl_user_group_id, acl_user_group_anonymous},
+                {acl_user_group_id, ensure_test_group(ContextAnon)},
                 {actions, [view, update]},
                 {is_owner, true},
                 {category_id, person}
             ]}
         ],
-        ContextAnon
-    ),
+        ContextSudo),
 
-    {ok, Id1} = m_rsc:insert(
-                #{
-                    <<"category_id">> => person
-                }, z_acl:sudo(ContextAnon)),
+    {ok, UserId1} = m_rsc:insert(#{ <<"category_id">> => person }, ContextSudo),
+    {ok, UserId2} = m_rsc:insert(#{ <<"category_id">> => person, <<"creator_id">> => UserId1 }, ContextSudo),
+    {ok, UserId3} = m_rsc:insert(#{ <<"category_id">> => person, <<"creator_id">> => self }, ContextSudo),
 
-    {ok, Id2} = m_rsc:insert(
-                #{
-                    <<"category_id">> => person,
-                    <<"creator_id">> => Id1
-                }, z_acl:sudo(ContextAnon)),
+    m_edge:insert(UserId1, hasusergroup, ensure_test_group(ContextAnon), ContextSudo),
+    m_edge:insert(UserId2, hasusergroup, ensure_test_group(ContextAnon), ContextSudo),
+    m_edge:insert(UserId3, hasusergroup, ensure_test_group(ContextAnon), ContextSudo),
 
-    {ok, Id3} = m_rsc:insert(
-                #{
-                    <<"category_id">> => person,
-                    <<"creator_id">> => self
-                }, z_acl:sudo(ContextAnon)),
+    ContextUser1 = z_acl:logon(UserId1, ContextAnon),
+    ContextUser3 = z_acl:logon(UserId3, ContextAnon),
 
-    ContextUser1 = z_acl:logon(Id1, ContextAnon),
-    ContextUser3 = z_acl:logon(Id3, ContextAnon),
+    % No access for anonymous
+    ?assertEqual({error, eacces}, m_rsc:update(UserId1, [{title, <<"Test">>}], ContextAnon)),
+    % Must be owner
+    ?assertEqual({error, eacces}, m_rsc:update(UserId1, [{title, <<"Test">>}], ContextUser3)),
+    % Must be creator
+    ?assertEqual({error, eacces}, m_rsc:update(UserId2, [{title, <<"Test">>}], ContextUser3)),
 
-    %% No access for anonymous
-    ?assertEqual({error, eacces}, m_rsc:update(Id1, [{title, <<"Test">>}], ContextAnon)),
-    %% Must be owner
-    ?assertEqual({error, eacces}, m_rsc:update(Id1, [{title, <<"Test">>}], ContextUser3)),
-    %% Must be creator
-    ?assertEqual({error, eacces}, m_rsc:update(Id2, [{title, <<"Test">>}], ContextUser3)),
+    % User1 can update self, as user is self
+    {ok, _} = m_rsc:update(UserId1, [{title, "Test"}], ContextUser1),
+    % User1 can update user2, as user1 is creator (owner) if user2
+    {ok, _} = m_rsc:update(UserId2, [{title, "Test"}], ContextUser1),
+    % User3 can update self, as is owner of self (and is self)
+    {ok, _} = m_rsc:update(UserId3, [{title, "Test"}], ContextUser3),
 
-    {ok, _} = m_rsc:update(Id1, [{title, "Test"}], ContextUser1),
-    {ok, _} = m_rsc:update(Id2, [{title, "Test"}], ContextUser1),
-    {ok, _} = m_rsc:update(Id3, [{title, "Test"}], ContextUser3).
+    m_rsc:delete(UserId1, ContextSudo),
+    m_rsc:delete(UserId2, ContextSudo),
+    m_rsc:delete(UserId3, ContextSudo),
+    delete_managed(ContextSudo).
 
 
 person_can_insert_text_in_default_content_group_only_test() ->
-    Context = context(),
+    ContextAnon = context(),
+    ContextSudo = z_acl:sudo(ContextAnon),
 
     %% Person must be able to insert text into the default user group
     replace_managed(
         [
+            % Allow insert of articles into default_content_group
             {rsc, [
-                {acl_user_group_id, acl_user_group_anonymous},
+                {acl_user_group_id, ensure_test_group(ContextSudo)},
                 {content_group_id, default_content_group},
                 {actions, [insert]},
                 {is_owner, true},
                 {category_id, article}
+            ]},
+            % Allow view of everything
+            {rsc, [
+                {acl_user_group_id, ensure_test_group(ContextSudo)},
+                {actions, [view]}
             ]}
         ],
-        Context
-    ),
+        ContextSudo),
 
     % Make a new user
-    {ok, Id} = m_rsc:insert([{category, person}], z_acl:sudo(Context)),
-    UserContext = z_acl:logon(Id, Context),
+    {ok, UserId} = m_rsc:insert(#{ <<"category_id">> => person }, ContextSudo),
+    {ok, _} = m_edge:insert(UserId, hasusergroup, ensure_test_group(ContextSudo), ContextSudo),
+    UserContext = z_acl:logon(UserId, ContextAnon),
 
     %% The user is able to insert a text into the default content group
-    DefaultContentGroupId = m_rsc:p(default_content_group, id, Context),
+    DefaultContentGroupId = m_rsc:p(default_content_group, id, ContextAnon),
     {ok, _TextId} = m_rsc:insert([{category, article}, {content_group_id, DefaultContentGroupId}], UserContext),
 
     %% But not in the system content group
-    SystemContentGroupId = m_rsc:p(system_content_group, id, Context),
+    SystemContentGroupId = m_rsc:p(system_content_group, id, ContextAnon),
     ?assertEqual({error, eacces}, m_rsc:insert([{category, article}, {content_group_id, SystemContentGroupId}], UserContext)),
 
+    m_rsc:delete(UserId, ContextSudo),
+    delete_managed(ContextSudo),
     ok.
 
 
@@ -260,18 +271,39 @@ acl_is_allowed_accepts_rsc_name_object_test() ->
 
 %% @doc See https://github.com/zotonic/zotonic/issues/1306
 acl_is_allowed_override_test() ->
-    Context = context(),
+    ContextAnon = context(),
+    ContextSudo = z_acl:sudo(ContextAnon),
+
+    {ok, UserId} = m_rsc:insert([{category_id, person}], ContextSudo),
+    {ok, _} = m_edge:insert(UserId, hasusergroup, acl_user_group_anonymous, ContextSudo),
+    ContextUser = z_acl:logon(UserId, ContextAnon),
 
     %% Priority (10) must be before mod_acl_user_group's acl_is_allowed observer.
-    z_notifier:observe(acl_is_allowed, {?MODULE, is_allowed_always_true}, 10, Context),
-    {ok, Id} = m_rsc:insert([{category_id, text}], Context),
-    ?assertEqual(m_rsc:rid(default_content_group, Context), m_rsc:p_no_acl(Id, content_group_id, Context)),
-    z_notifier:detach(acl_is_allowed, Context).
+    z_notifier:observe(acl_is_allowed, {?MODULE, is_allowed_always_true}, 10, ContextAnon),
+
+    % Insert unpublished resource
+    {ok, TextId} = m_rsc:insert([ {category_id, text} ], ContextUser),
+
+    % Inserted into the default_content_group
+    ?assertEqual(m_rsc:rid(default_content_group, ContextSudo), m_rsc:p_no_acl(TextId, <<"content_group_id">>, ContextSudo)),
+
+    % Anon user can view but not update (short circuit in the ACL checks)
+    ?assertEqual(true, z_acl:rsc_visible(TextId, ContextAnon)),
+    ?assertEqual(false, z_acl:rsc_editable(TextId, ContextAnon)),
+
+    % Authenticated user can view and update (due to our observer)
+    ?assertEqual(true, z_acl:rsc_visible(TextId, ContextUser)),
+    ?assertEqual(true, z_acl:rsc_editable(TextId, ContextUser)),
+
+    z_notifier:detach(acl_is_allowed, ContextAnon),
+    m_rsc:delete(UserId, ContextSudo),
+    m_rsc:delete(TextId, ContextSudo).
 
 publish_test() ->
-    Context = context(),
+    ContextAnon = context(),
+    ContextSudo = z_acl:sudo(ContextAnon),
 
-    %% Anonymous can view everything
+    %% Anonymous can view all published content
     replace_managed(
         [
             {rsc, [
@@ -279,15 +311,27 @@ publish_test() ->
                 {actions, [view]}
             ]}
         ],
-        Context
-    ),
+        ContextSudo),
 
-    {ok, Id} = m_rsc:insert([{title, <<"Top secret!">>}, {category, text}], z_acl:sudo(Context)),
-    ?assertEqual(<<"Top secret!">>, m_rsc:p(Id, title, z_acl:sudo(Context))),
-    ?assertEqual(undefined, m_rsc:p(Id, title, Context)), %% invisible for anonymous
+    {ok, TextId} = m_rsc:insert([
+            {is_published, false},
+            {title, <<"Top secret!">>},
+            {category, text}
+        ], ContextSudo),
 
-    {ok, Id} = m_rsc:update(Id, [{is_published, true}], z_acl:sudo(Context)),
-    ?assertEqual(<<"Top secret!">>, m_rsc:p(Id, title, Context)). %% visible for anonymous when published
+    ?assertEqual(<<"Top secret!">>, m_rsc:p(TextId, <<"title">>, ContextSudo)),
+
+    %% invisible for anonymous
+    ?assertEqual(false, z_acl:rsc_visible(TextId, ContextAnon)),
+    ?assertEqual(undefined, m_rsc:p(TextId, <<"title">>, ContextAnon)),
+
+    {ok, TextId} = m_rsc:update(TextId, [ {is_published, true} ], ContextSudo),
+
+    %% visible for anonymous when published
+    ?assertEqual(<<"Top secret!">>, m_rsc:p(TextId, <<"title">>, ContextAnon)),
+
+    m_rsc:delete(TextId, ContextSudo),
+    delete_managed(ContextSudo).
 
 context() ->
     Context = z_context:new(zotonic_site_testsandbox),
@@ -305,14 +349,30 @@ is_allowed_always_true(#acl_is_allowed{}, _Context) ->
 replace_managed(Rules, Context) ->
     z_mqtt:subscribe(<<"model/acl_user_groups/event/acl-rules/publish-rebuild">>, z_acl:sudo(Context)),
 
-    m_acl_rule:replace_managed(
-        Rules,
-        ?MODULE,
-        z_acl:sudo(Context)
-    ),
-
+    m_acl_rule:replace_managed(Rules, ?MODULE, z_acl:sudo(Context)),
     receive
         {mqtt_msg, _Msg} -> ok
     end,
-
     z_mqtt:unsubscribe(<<"model/acl_user_groups/event/acl-rules/publish-rebuild">>, z_acl:sudo(Context)).
+
+delete_managed(Context) ->
+    z_mqtt:subscribe(<<"model/acl_user_groups/event/acl-rules/publish-rebuild">>, z_acl:sudo(Context)),
+    m_acl_rule:delete_managed(?MODULE, z_acl:sudo(Context)),
+    receive
+        {mqtt_msg, _Msg} -> ok
+    end,
+    z_mqtt:unsubscribe(<<"model/acl_user_groups/event/acl-rules/publish-rebuild">>, z_acl:sudo(Context)).
+
+ensure_test_group(Context) ->
+    case m_rsc:rid(?UG_TEST, Context) of
+        undefined ->
+            {ok, Id} = m_rsc:insert([
+                    {name, ?UG_TEST},
+                    {is_published, true},
+                    {category_id, acl_user_group},
+                    {title, <<"Test user group">>}
+                ], z_acl:sudo(Context)),
+            Id;
+        Id ->
+            Id
+    end.
