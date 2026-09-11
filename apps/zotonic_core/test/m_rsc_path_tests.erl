b722e5ffc350658124e02d021c609b4b13d2b7aa
Marc Worrell <marc@worrell.nl>
2026-09-11 16:32:07 +0200

    Fix test

--- apps/zotonic_core/test/m_rsc_path_tests.erl
+++ apps/zotonic_core/test/m_rsc_path_tests.erl
@@ -2,12 +2,13 @@
 -module(m_rsc_path_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("zotonic.hrl").
 
 invalid_page_path_test_() ->
     Paths = [ <<"/foo", C, "bar">> || C <- lists:seq(0, 31) ]
         ++ [<<"/", 255>>, <<"/", 16#e2, 16#82>>],
-    % An undefined context ensures that invalid paths never reach the database.
+    % A context without a site ensures that invalid paths never reach the database.
     [ ?_assertEqual(
         {error, {illegal_page_path, Path, unicode}},
-        m_rsc:page_path_to_id(Path, undefined))
+        m_rsc:page_path_to_id(Path, #context{}))
       || Path <- Paths ].
