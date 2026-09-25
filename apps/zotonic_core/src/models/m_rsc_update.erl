cad2a655cb64042754eb3a302344f5799cb33482
MM Zeeman <mmzeeman@xs4all.nl>
2026-09-25 15:17:25 +0200

    core: add location_zoom_level property to type hints (#4541)
    * core: add location_zoom_level property to type hints
    * core: add location_zoom_level type conversion to positive integer
    * Refactor location_zoom_level handling in props_filter

--- apps/zotonic_core/src/models/m_rsc_update.erl
+++ apps/zotonic_core/src/models/m_rsc_update.erl
@@ -1966,6 +1966,16 @@ props_filter(Location, P, Acc, _RscUpd, _Context)
             _:_ -> undefined
         end,
     Acc#{ Location => X };
+props_filter(<<"location_zoom_level">>, P, Acc, _RscUpd, _Context) ->
+    X1 = try
+            case z_convert:to_integer(P) of
+                X when is_integer(X), X < 0 -> 0;
+                X -> X
+            end
+        catch
+            _:_ -> undefined
+        end,
+    Acc#{ <<"location_zoom_level">> => X1 };
 props_filter(<<"pref_language">>, Lang, Acc, _RscUpd, _Context) ->
     Lang1 = case z_language:to_language_atom(Lang) of
         {ok, LangAtom} -> LangAtom;
